2ae38a1c23f39b2af2765b97cb33f3910f1f54a3
João Cunha <j.a.cunha@gmail.com>
2019-03-01 11:43:11 +0000

    Removes unecessary comment

--- ee/db/post_migrate/20190228114524_migrate_cluster_prometheus_update_worker_sidekiq_queue.rb
+++ ee/db/post_migrate/20190228114524_migrate_cluster_prometheus_update_worker_sidekiq_queue.rb
@@ -1,8 +1,5 @@
 # frozen_string_literal: true
 
-# See http://doc.gitlab.com/ce/development/migration_style_guide.html
-# for more information on how to write migrations for GitLab.
-
 class MigrateClusterPrometheusUpdateWorkerSidekiqQueue < ActiveRecord::Migration[5.0]
   include Gitlab::Database::MigrationHelpers
 
