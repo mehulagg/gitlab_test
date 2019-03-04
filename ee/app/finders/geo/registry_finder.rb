4caf7f1cfae3f0db5a2fa89c1a7683a5b0182384
Michael Kozono <mkozono@gmail.com>
2019-03-04 13:25:37 -0800

    Add back delegate

--- ee/app/finders/geo/registry_finder.rb
+++ ee/app/finders/geo/registry_finder.rb
@@ -4,6 +4,8 @@ module Geo
   class RegistryFinder
     attr_reader :current_node
 
+    delegate :selective_sync?, to: :current_node, allow_nil: true
+
     def initialize(current_node: nil)
       @current_node = current_node
     end
