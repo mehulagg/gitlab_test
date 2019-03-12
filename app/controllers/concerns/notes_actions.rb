yehaha9876/gitlab

Geo secondary writes for Projects & Issues

app/controllers/concerns/notes_actions.rb
@@ -49,7 +49,10 @@ module NotesActions
       in_reply_to_discussion_id: params[:in_reply_to_discussion_id]
     )
 
-    @note = Notes::CreateService.new(note_project, current_user, create_params).execute
+    @note = nil
+    for_database do
+      @note = Notes::CreateService.new(note_project, current_user, create_params).execute
+    end
 
     respond_to do |format|
       format.json do

mehulagg/gitlab_test

app/controllers/concerns/notes_actions.rb
@@ -43,7 +43,10 @@ module NotesActions
 
   # rubocop:disable Gitlab/ModuleWithInstanceVariables
   def create
-    @note = Notes::CreateService.new(note_project, current_user, create_note_params).execute
+    @note = nil
+    for_database do
+      @note = Notes::CreateService.new(note_project, current_user, create_note_params).execute
+    end
 
     respond_to do |format|
       format.json do