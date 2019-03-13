d912e443b3965b7e55592ad879e1594841cdfb41
Vladimir Shushlin <v.shushlin@gmail.com>
2019-03-13 18:14:13 +0300

    Create protected environments with feature flag scopes

--- ee/app/services/protected_environments/create_service.rb
+++ ee/app/services/protected_environments/create_service.rb
@@ -2,7 +2,22 @@
 module ProtectedEnvironments
   class CreateService < BaseService
     def execute
-      project.protected_environments.create(params)
+      protected_environment = project.protected_environments.new(params)
+      if protected_environment.save
+        create_feature_flag_protected_scopes(protected_environment)
+      end
+
+      protected_environment
+    end
+
+    private
+
+    def create_feature_flag_protected_scopes(protected_environment)
+      project.operations_feature_flags.each do |feature_flag|
+        next if feature_flag.scopes.find { |scope| scope.environment_scope == protected_environment.name }
+
+        feature_flag.scopes.create!(environment_scope: protected_environment.name, active: false)
+      end
     end
   end
 end
