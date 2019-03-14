f3dda61cc4ff46a8430dafb2ab82f3534092a5b1
Vladimir Shushlin <v.shushlin@gmail.com>
2019-03-14 19:28:32 +0300

    Add protected to feature flag scope entity

--- ee/app/serializers/feature_flag_scope_entity.rb
+++ ee/app/serializers/feature_flag_scope_entity.rb
@@ -14,6 +14,11 @@ class FeatureFlagScopeEntity < Grape::Entity
     project.protected_environment_accessible_to?(feature_flag_scope.environment_scope, current_user)
   end
 
+  expose :protected do |feature_flag_scope|
+    project = feature_flag_scope.feature_flag.project
+    project.protected_environment_by_name(feature_flag_scope.environment_scope).present?
+  end
+
   private
 
   def current_user
