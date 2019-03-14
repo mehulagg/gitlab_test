11c0a4f826ccfee5ea55fc6a50cbe2f16fa1ff69
Luke Bennett <lbennett@gitlab.com>
2019-03-14 20:52:01 +0000

    Improve project avatar settings
    Prioritize and simplify project settings content.

--- app/assets/javascripts/pages/projects/edit/index.js
+++ app/assets/javascripts/pages/projects/edit/index.js
@@ -4,12 +4,12 @@ import setupProjectEdit from '~/project_edit';
 import initConfirmDangerModal from '~/confirm_danger_modal';
 import mountBadgeSettings from '~/pages/shared/mount_badge_settings';
 import dirtySubmitFactory from '~/dirty_submit/dirty_submit_factory';
-import fileUpload from '~/lib/utils/file_upload';
+import initAvatarPicker from '~/avatar_picker';
 import initProjectLoadingSpinner from '../shared/save_project_loader';
 import initProjectPermissionsSettings from '../shared/permissions';
 
 document.addEventListener('DOMContentLoaded', () => {
-  fileUpload('.js-choose-project-avatar-button', '.js-project-avatar-input');
+  initAvatarPicker();
   initConfirmDangerModal();
   initSettingsPanels();
   mountBadgeSettings(PROJECT_BADGE);
