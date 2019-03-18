4b9c63910f7dfab99787bb904f5dce7c1699b10d
Matija Čupić <matteeyah@gmail.com>
2019-03-18 18:37:58 +0100

    Remove redundant index checks in migrations

--- ee/db/migrate/20190304010619_add_ci_pipeline_source_project_indexes.rb
+++ ee/db/migrate/20190304010619_add_ci_pipeline_source_project_indexes.rb
@@ -16,7 +16,7 @@ class AddCiPipelineSourceProjectIndexes < ActiveRecord::Migration[5.0]
   end
 
   def down
-    remove_concurrent_index :ci_sources_projects, :project_id if index_exists? :ci_sources_projects, :project_id
-    remove_concurrent_index :ci_sources_projects, :source_project_id if index_exists? :ci_sources_projects, :source_project_id
+    remove_concurrent_index :ci_sources_projects, :project_id
+    remove_concurrent_index :ci_sources_projects, :source_project_id
   end
 end
