3ad84bd818899926419b4cd463b8f42356fa2e26
Matija Čupić <matteeyah@gmail.com>
2019-03-21 19:18:13 +0100

    Add failure mechanism to ProcessProjectUpstreams

--- ee/lib/ee/gitlab/ci/pipeline/chain/process_project_upstreams.rb
+++ ee/lib/ee/gitlab/ci/pipeline/chain/process_project_upstreams.rb
@@ -8,26 +8,43 @@ module EE
           module ProcessProjectUpstreams
             extend ::Gitlab::Utils::Override
 
+            include ::Gitlab::Ci::Pipeline::Chain::Helpers
+            include ::Gitlab::Allowable
+
             override :perform!
             def perform!
-              if pipeline.default_branch? && cross_project_pipelines_enabled?
-                bridges = pipeline.stages.map(&:bridges).flatten
+              return unless pipeline.default_branch? && cross_project_pipelines_enabled?
 
-                project.upstream_projects = bridges.map do |bridge|
-                  ::Project.find_by_full_path(bridge.upstream_project_path) if bridge.upstream_bridge?
-                end.compact
+              pipeline.stages.map(&:bridges).flatten.each do |bridge|
+                next unless bridge.upstream_bridge?
+
+                if (upstream_project = ::Project.find_by_full_path(bridge.upstream_project_path))
+                  if can_create_cross_pipeline?(upstream_project)
+                    project.upstream_projects << upstream_project
+                  else
+                    return error('User does not have enough permissions to observe the upstream project')
+                  end
+                else
+                  bridge.status = "failed"
+                  bridge.failure_reason = :upstream_bridge_project_not_found
+                end
               end
             end
 
             override :break?
             def break?
-              false
+              pipeline.errors.any?
             end
 
             private
 
             def cross_project_pipelines_enabled?
               project.feature_available?(:cross_project_pipelines)
+            end
+
+            def can_create_cross_pipeline?(upstream_project)
+              can?(current_user, :read_project, upstream_project) &&
+                can?(current_user, :create_pipeline, project)
             end
           end
         end
--- ee/spec/lib/gitlab/ci/pipeline/chain/process_project_upstreams_spec.rb
+++ ee/spec/lib/gitlab/ci/pipeline/chain/process_project_upstreams_spec.rb
@@ -10,30 +10,73 @@ describe ::Gitlab::Ci::Pipeline::Chain::ProcessProjectUpstreams do
 
   let(:step) { described_class.new(pipeline, command) }
 
+  subject { step.perform! }
+
   context 'when cross-project pipelines are enabled' do
     before do
       stub_licensed_features(cross_project_pipelines: true)
     end
 
-    context 'when the pipeline is on the default branch' do
-      context 'when the pipeline has upstream bridge jobs' do
-        let(:upstream_project) { create(:project) }
+    context 'when the upstream project exists' do
+      let(:upstream_project) { create(:project) }
 
+      before do
+        pipeline.stages.build(name: 'test', position: 0, project: project)
+        pipeline.stages.first.bridges << build(:ci_upstream_bridge, upstream: upstream_project)
+      end
+
+      context 'when the user has permissions' do
         before do
-          pipeline.stages.build(name: 'test', position: 0, project: project)
-          pipeline.stages.first.bridges << build(:ci_upstream_bridge, upstream: upstream_project)
+          upstream_project.add_developer(user)
+          project.add_developer(user)
         end
 
         it 'populates the pipeline project upstream projects' do
-          step.perform!
+          subject
 
           expect(project.reload.upstream_projects).to be_one
           expect(project.reload.upstream_projects.first).to eq(upstream_project)
         end
 
         it 'does not break the chain' do
+          subject
+
           expect(step.break?).to be false
         end
+      end
+
+      context 'when the user does not have permissions' do
+        it 'does not add upstream projects' do
+          subject
+
+          expect(project.reload.upstream_projects).to be_empty
+        end
+
+        it 'breaks the chain' do
+          subject
+
+          expect(step.break?).to be true
+        end
+      end
+    end
+
+    context 'when the upstream project does not exist' do
+      before do
+        pipeline.stages.build(name: 'test', position: 0, project: project)
+        pipeline.stages.first.bridges << build(:ci_upstream_bridge, :invalid_upstream)
+      end
+
+      it 'drops the bridge job' do
+        subject
+
+        expect(pipeline.stages.first.bridges.first.status).to eq('failed')
+        expect(pipeline.stages.first.bridges.first.failure_reason).to eq('upstream_bridge_project_not_found')
+      end
+
+      it 'does not break the chain' do
+        subject
+
+        expect(step.break?).to be false
       end
     end
 
@@ -41,12 +84,14 @@ describe ::Gitlab::Ci::Pipeline::Chain::ProcessProjectUpstreams do
       let(:pipeline) { build(:ci_pipeline, project: project, ref: 'some_branch') }
 
       it 'does not add upstream projects' do
-        step.perform!
+        subject
 
         expect(project.upstream_projects).to be_empty
       end
 
       it 'does not break the chain' do
+        subject
+
         expect(step.break?).to be false
       end
     end
@@ -56,12 +101,14 @@ describe ::Gitlab::Ci::Pipeline::Chain::ProcessProjectUpstreams do
     let(:pipeline) { build(:ci_pipeline, project: project, ref: 'some_branch') }
 
     it 'does not add upstream projects' do
-      step.perform!
+      subject
 
       expect(project.upstream_projects).to be_empty
     end
 
     it 'does not break the chain' do
+      subject
+
       expect(step.break?).to be false
     end
   end
