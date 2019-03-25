73f36275cfd444caa01dd96b9507688ac054f66b
Shinya Maeda <shinya@gitlab.com>
2019-03-25 21:43:43 +0700

    Use merge request merge ref
    For attached merge request pipelines
    
    ok
    
    ok
    
    improve
    
    fix
    
    Add spec
    
    Revert some thing
    
    Rever file
    
    Complete the spec
    
    Add changelog ok
    
    remove feature flag

--- ee/app/services/ee/merge_requests/base_service.rb
+++ ee/app/services/ee/merge_requests/base_service.rb
@@ -42,6 +42,30 @@ module EE
           end
         end
       end
+
+      def create_pipeline_for(merge_request, user)
+        return super unless can_create_merge_request_pipeline_for?(merge_request)
+
+        ret = ::MergeRequests::MergeToRefService.new(merge_request.project, user)
+                                                .execute(merge_request)
+
+        return super unless ret[:status] == :success
+
+        ::Ci::CreatePipelineService.new(merge_request.source_project, user,
+                                        ref: merge_request.merge_ref_path,
+                                        checkout_sha: ret[:commit_id],
+                                        target_sha: ret[:target_id],
+                                        source_sha: ret[:source_id])
+          .execute(:merge_request_event, merge_request: merge_request)
+      end
+
+      def can_create_merge_request_pipeline_for?(merge_request)
+        return false if merge_request.work_in_progress?
+        return false unless project.merge_pipelines_enabled?
+        return false unless can_use_merge_request_ref?(merge_request)
+
+        can_create_pipeline_for?(merge_request)
+      end
     end
   end
 end
