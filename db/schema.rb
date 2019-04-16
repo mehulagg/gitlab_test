ac12b510fbe29e39d730b65d6c8ad77d238f56df
Shinya Maeda <shinya@gitlab.com>
2019-04-16 18:56:48 +0700

    Add project level config for merge train

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190408163745) do
+ActiveRecord::Schema.define(version: 20190416185130) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -2311,6 +2311,7 @@ ActiveRecord::Schema.define(version: 20190408163745) do
     t.integer "project_id", null: false
     t.boolean "group_runners_enabled", default: true, null: false
     t.boolean "merge_pipelines_enabled"
+    t.boolean "merge_trains_enabled"
     t.index ["project_id"], name: "index_project_ci_cd_settings_on_project_id", unique: true, using: :btree
   end
 
