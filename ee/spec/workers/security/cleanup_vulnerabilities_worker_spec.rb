5cad58fe41f0f41e6e7b3be992bda6288eec1525
Tetiana Chupryna <tchupryna@gitlab.com>
2019-04-18 16:45:47 +0300

    Add job tests

--- ee/spec/workers/security/cleanup_vulnerabilities_worker_spec.rb
+++ ee/spec/workers/security/cleanup_vulnerabilities_worker_spec.rb
@@ -3,4 +3,11 @@
 require 'spec_helper'
 
 describe Security::CleanupVulnerabilitiesWorker do
+  let(:worker) { described_class.new }
+
+  it 'triggers cleanup service' do
+    expect_any_instance_of(::Security::CleanupVulnerabilities).to receive(:execute)
+
+    worker.perform
+  end
 end
