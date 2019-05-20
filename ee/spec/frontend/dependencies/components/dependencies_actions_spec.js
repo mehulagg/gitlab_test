06436cde00b85939cffd2551a397b57f4bd3020f
Mark Florian <mflorian@gitlab.com>
2019-05-20 17:59:12 +0800

    this works with mount rather than shallow mount... but then I can't reasonably use snapshotting... hmm, conundrum

--- ee/spec/frontend/dependencies/components/dependencies_actions_spec.js
+++ ee/spec/frontend/dependencies/components/dependencies_actions_spec.js
@@ -1,18 +1,23 @@
-import { createLocalVue, shallowMount } from '@vue/test-utils';
+import { createLocalVue, mount } from '@vue/test-utils';
+import { GlDropdownItem } from '@gitlab/ui';
 import createStore from 'ee/dependencies/store';
 import DependenciesActions from 'ee/dependencies/components/dependencies_actions.vue';
 
 describe('DependenciesActions component', () => {
   test.todo('needs testing');
 
+  let store;
   let wrapper;
 
   const factory = (props = {}) => {
     const localVue = createLocalVue();
 
-    wrapper = shallowMount(localVue.extend(DependenciesActions), {
+    store = createStore();
+    jest.spyOn(store, 'dispatch');
+    wrapper = mount(localVue.extend(DependenciesActions), {
       localVue,
-      store: createStore(),
+      store,
+      sync: false,
     });
   };
 
@@ -28,7 +33,11 @@ describe('DependenciesActions component', () => {
     expect(wrapper.element).toMatchSnapshot();
   });
 
-  it.todo('dispatches the setSortField action on clicking item in dropdown');
+  it('dispatches the setSortField action on clicking item in dropdown', () => {
+    const item = wrapper.find(GlDropdownItem);
+    item.trigger('click');
+    expect(store.dispatch).toHaveBeenCalledWith('setSortField', expect.any(String));
+  });
 
   it.todo('dispatches the setSortOrder action on clicking item in dropdown');
 
