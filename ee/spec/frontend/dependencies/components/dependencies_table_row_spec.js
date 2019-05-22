8176abd631c95ae7b4d1f269742d7c5c9b497c19
Mark Florian <mflorian@gitlab.com>
2019-05-22 17:53:26 +0800

    add test for unknown type in table row

--- ee/spec/frontend/dependencies/components/dependencies_table_row_spec.js
+++ ee/spec/frontend/dependencies/components/dependencies_table_row_spec.js
@@ -53,4 +53,17 @@ describe('DependenciesTableRow component', () => {
       expect(wrapper.element).toMatchSnapshot();
     });
   });
+
+  describe('when a dependency is loaded with an unknown type', () => {
+    beforeEach(() => {
+      factory({
+        isLoading: false,
+        dependency: makeDependency({ type: 'foobar' }),
+      });
+    });
+
+    it('matches the snapshot', () => {
+      expect(wrapper.element).toMatchSnapshot();
+    });
+  });
 });
