b762b93d6d9cd86f102ad111dd1a505a786835b8
Mark Florian <mflorian@gitlab.com>
2019-05-27 15:01:06 +0800

    add TODO

--- ee/app/assets/javascripts/dependencies/store/getters.js
+++ ee/app/assets/javascripts/dependencies/store/getters.js
@@ -1,5 +1,6 @@
 import { REPORT_STATUS } from './constants';
 
+// TODO: Give these more explicit names, e.g., dependencyListJobNotSetUp
 export const jobNotSetUp = state => state.reportInfo.status === REPORT_STATUS.jobNotSetUp;
 export const jobFailed = state => state.reportInfo.status === REPORT_STATUS.jobFailed;
 export const isIncomplete = state => state.reportInfo.status === REPORT_STATUS.incomplete;
