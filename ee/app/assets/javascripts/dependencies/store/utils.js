b9c2b7fd88f6c347f0796ba71c60119cd4b5798a
Mark Florian <mflorian@gitlab.com>
2019-05-27 15:31:34 +0800

    loosen check for dep list

--- ee/app/assets/javascripts/dependencies/store/utils.js
+++ ee/app/assets/javascripts/dependencies/store/utils.js
@@ -1,5 +1,4 @@
-export const hasDependencyList = ({ dependencies }) =>
-  Array.isArray(dependencies) && dependencies.length > 0;
+export const hasDependencyList = ({ dependencies }) => Array.isArray(dependencies);
 
 export const hasReportStatus = ({ report }) => Boolean(report && typeof report.status === 'string');
 
