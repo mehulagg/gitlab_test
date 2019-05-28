6019bc2dc86298073f11109594845faeefc4c9df
Mark Florian <mflorian@gitlab.com>
2019-05-28 21:12:35 +0800

    [app spec] initial work

--- ee/spec/frontend/dependencies/components/app_spec.js
+++ ee/spec/frontend/dependencies/components/app_spec.js
@@ -1,3 +1,46 @@
+import { createLocalVue, shallowMount } from '@vue/test-utils';
+import { TEST_HOST } from 'helpers/test_constants';
+import createStore from 'ee/dependencies/store';
+import DependenciesApp from 'ee/dependencies/components/app.vue';
+
 describe('DependenciesApp component', () => {
   test.todo('needs testing');
+
+  let store;
+  let wrapper;
+
+  const basicAppProps = {
+    endpoint: '/foo',
+    emptyStateSvgPath: '/bar.svg',
+    documentationPath: TEST_HOST,
+  };
+
+  const factory = (props = basicAppProps) => {
+    const localVue = createLocalVue();
+
+    store = createStore();
+    jest.spyOn(store, 'dispatch');
+
+    wrapper = shallowMount(localVue.extend(DependenciesApp), {
+      localVue,
+      store,
+      sync: false,
+      propsData: { ...props },
+    });
+  };
+
+  afterEach(() => {
+    wrapper.destroy();
+  });
+
+  it('matches the snapshot', () => {
+    factory();
+    expect(wrapper.element).toMatchSnapshot();
+  });
+
+  test.todo('renders dependency list normally');
+  test.todo('renders empty state');
+  test.todo('renders job failure');
+  test.todo('renders incomplete job');
+  test.todo('renders error');
 });
