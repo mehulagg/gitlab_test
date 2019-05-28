8bca632424ecbad45f9e92389c6fee03685979c2
Mark Florian <mflorian@gitlab.com>
2019-05-28 18:11:34 +0800

    [job_failed] port cmp to shared alert

--- ee/app/assets/javascripts/dependencies/components/dependency_list_job_failed_alert.vue
+++ ee/app/assets/javascripts/dependencies/components/dependency_list_job_failed_alert.vue
@@ -1,13 +1,13 @@
 <script>
 import { GlButton } from '@gitlab/ui';
 import { sprintf } from '~/locale';
-import Icon from '~/vue_shared/components/icon.vue';
+import DependencyListAlert from './dependency_list_alert.vue';
 
 export default {
   name: 'DependencyListJobFailedAlert',
   components: {
+    DependencyListAlert,
     GlButton,
-    Icon,
   },
   props: {
     jobPath: {
@@ -17,7 +17,6 @@ export default {
   },
   data() {
     return {
-      closed: false,
       message: sprintf(
         'The %{jobName} job has failed and cannot generate the list. Please ensure the job is running properly and run the pipeline again.',
         { jobName: '<code>dependency_list</code>' },
@@ -25,28 +24,17 @@ export default {
       ),
     };
   },
-  methods: {
-    close() {
-      this.closed = true;
-    },
-  },
 };
 </script>
 
 <template>
-  <div v-if="!closed" class="danger_message">
-    <button
-      class="btn-blank float-right mr-1 mt-1 text-danger-900"
-      type="button"
-      :aria-label="__('Close')"
-      @click="close"
-    >
-      <icon name="close" aria-hidden="true" />
-    </button>
-    <h4 class="text-danger-900">{{ __('Job failed to generate the dependency list') }}</h4>
+  <dependency-list-alert
+    type="danger"
+    :header-text="__('Dependencies|Job failed to generate the dependency list')"
+  >
     <p v-html="message" />
     <gl-button :href="jobPath" class="btn-inverted btn-danger mb-2">
       {{ __('View job') }}
     </gl-button>
-  </div>
+  </dependency-list-alert>
 </template>
