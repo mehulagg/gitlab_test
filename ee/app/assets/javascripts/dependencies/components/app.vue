670a352f1f8567cbfd78c8c269338d2fc928adb7
Mark Florian <mflorian@gitlab.com>
2019-05-28 17:27:58 +0800

    [list incomplete] initial extraction

--- ee/app/assets/javascripts/dependencies/components/app.vue
+++ ee/app/assets/javascripts/dependencies/components/app.vue
@@ -4,6 +4,7 @@ import { GlBadge, GlEmptyState, GlLoadingIcon } from '@gitlab/ui';
 import Pagination from '~/vue_shared/components/pagination_links.vue';
 import DependenciesActions from './dependencies_actions.vue';
 import DependenciesTable from './dependencies_table.vue';
+import DependencyListIncompeteAlert from './dependency_list_incomplete_alert.vue';
 import DependencyListJobFailedAlert from './dependency_list_job_failed_alert.vue';
 import { REPORT_STATUS } from '../store/constants';
 
@@ -66,25 +67,7 @@ export default {
   />
 
   <div v-else>
-    <div v-if="isIncomplete" class="warning_message">
-      <h4>{{ __('Unsupported file(s) detected') }}</h4>
-      <p>
-        {{
-          __(
-            'One or more of your dependency files are not supported, and the dependency list may be incomplete. Below is a list of supported file types.',
-          )
-        }}
-      </p>
-      <ul>
-        <li>package-lock.json</li>
-        <li>composer.lock</li>
-        <li>Gemfile.lock</li>
-        <li>gems.locked</li>
-        <li>yarn.lock</li>
-        <li>requirements.txt</li>
-        <li>pom.xml</li>
-      </ul>
-    </div>
+    <dependency-list-incomplete-alert v-if="isIncomplete" />
 
     <dependency-list-job-failed-alert v-if="jobFailed" :job-path="reportInfo.jobPath" />
 
---
+++ ee/app/assets/javascripts/dependencies/components/dependency_list_incomplete_alert.vue
@@ -0,0 +1,47 @@
+<script>
+export default {
+  name: 'DependencyListIncompeteAlert',
+  components: {
+    GlButton,
+    Icon,
+  },
+  props: {
+    jobPath: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      closed: false,
+    };
+  },
+  methods: {
+    close() {
+      this.closed = true;
+    },
+  },
+};
+</script>
+
+<template>
+  <div v-if="!closed" class="warning_message">
+    <h4>{{ __('Unsupported file(s) detected') }}</h4>
+    <p>
+      {{
+        __(
+          'One or more of your dependency files are not supported, and the dependency list may be incomplete. Below is a list of supported file types.',
+        )
+      }}
+    </p>
+    <ul>
+      <li>package-lock.json</li>
+      <li>composer.lock</li>
+      <li>Gemfile.lock</li>
+      <li>gems.locked</li>
+      <li>yarn.lock</li>
+      <li>requirements.txt</li>
+      <li>pom.xml</li>
+    </ul>
+  </div>
+</template>
