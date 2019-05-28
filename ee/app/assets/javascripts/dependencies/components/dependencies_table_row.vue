c75b1fe92f60b8c433a500b2b863705462cd53df
Mark Florian <mflorian@gitlab.com>
2019-05-28 14:27:12 +0800

    [location.path] initial work

--- ee/app/assets/javascripts/dependencies/components/dependencies_table_row.vue
+++ ee/app/assets/javascripts/dependencies/components/dependencies_table_row.vue
@@ -47,7 +47,7 @@ export default {
       <div class="table-section flex-grow-1 section-wrap">
         <div class="table-mobile-header" role="rowheader">{{ s__('Dependencies|Location') }}</div>
         <div class="table-mobile-content">
-          <a :href="dependency.location.blob_path">{{ dependency.location.blob_path }}</a>
+          <a :href="dependency.location.blob_path">{{ dependency.location.path }}</a>
         </div>
       </div>
     </template>
