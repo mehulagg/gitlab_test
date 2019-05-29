557145dc68f2111c22ca66f5634c7a0255e4fb6e
Mark Florian <mflorian@gitlab.com>
2019-05-29 16:16:08 +0800

    [app spec] finally a full test done

--- ee/spec/frontend/dependencies/components/app_spec.js
+++ ee/spec/frontend/dependencies/components/app_spec.js
@@ -1,7 +1,10 @@
 import { createLocalVue, shallowMount } from '@vue/test-utils';
 import { TEST_HOST } from 'helpers/test_constants';
 import createStore from 'ee/dependencies/store';
+import { REPORT_STATUS } from 'ee/dependencies/store/constants';
 import DependenciesApp from 'ee/dependencies/components/app.vue';
+import DependenciesTable from 'ee/dependencies/components/dependencies_table.vue';
+import Pagination from '~/vue_shared/components/pagination_links.vue';
 
 describe('DependenciesApp component', () => {
   test.todo('needs testing');
@@ -33,20 +36,67 @@ describe('DependenciesApp component', () => {
     wrapper.destroy();
   });
 
-  it('dispatches the correct initial actions', () => {
-    factory();
-    expect(store.dispatch.mock.calls).toEqual([
-      ['setDependenciesEndpoint', basicAppProps.endpoint],
-      ['fetchDependencies'],
-    ]);
+  describe('on creation', () => {
+    beforeEach(() => {
+      factory();
+    });
+
+    it('dispatches the correct initial actions', () => {
+      expect(store.dispatch.mock.calls).toEqual([
+        ['setDependenciesEndpoint', basicAppProps.endpoint],
+        ['fetchDependencies'],
+      ]);
+    });
+
+    it('matches the snapshot', () => {
+      expect(wrapper.element).toMatchSnapshot();
+    });
+
+    describe('given a list of dependencies and ok report', () => {
+      let dependencies;
+
+      beforeEach(() => {
+        dependencies = ['foo', 'bar'];
+
+        Object.assign(store.state, {
+          initialized: true,
+          isLoading: false,
+          dependencies,
+        });
+        store.state.pageInfo.total = 100;
+        store.state.reportInfo.status = REPORT_STATUS.ok;
+
+        return wrapper.vm.$nextTick();
+      });
+
+      it('matches the snapshot', () => {
+        expect(wrapper.element).toMatchSnapshot();
+      });
+
+      it('passes the correct props to the dependencies table', () => {
+        const table = wrapper.find(DependenciesTable);
+        expect(table.isVisible()).toBe(true);
+        expect(table.props()).toEqual(
+          expect.objectContaining({
+            dependencies,
+            isLoading: false,
+          }),
+        );
+      });
+
+      it('passes the correct props to the pagination', () => {
+        const pagination = wrapper.find(Pagination);
+        expect(pagination.isVisible()).toBe(true);
+        expect(pagination.props()).toEqual(
+          expect.objectContaining({
+            pageInfo: store.state.pageInfo,
+            change: wrapper.vm.fetchPage,
+          }),
+        );
+      });
+    });
   });
 
-  // it('matches the snapshot', () => {
-  //   factory();
-  //   expect(wrapper.element).toMatchSnapshot();
-  // });
-
-  test.todo('renders dependency list normally');
   test.todo('renders empty state');
   test.todo('renders job failure');
   test.todo('renders incomplete job');
