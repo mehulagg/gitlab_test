45bb170714dfa1845115ed26a45752f5470c010e
Felipe Artur <felipefac@gmail.com>
2019-06-03 12:26:32 -0300

    add specs for user without permissions

--- spec/services/notes/create_service_spec.rb
+++ spec/services/notes/create_service_spec.rb
@@ -3,9 +3,9 @@
 require 'spec_helper'
 
 describe Notes::CreateService do
-  let(:project) { create(:project, :repository) }
-  let(:issue) { create(:issue, project: project) }
-  let(:user) { create(:user) }
+  set(:project) { create(:project, :repository) }
+  set(:issue) { create(:issue, project: project) }
+  set(:user) { create(:user) }
   let(:opts) do
     { note: 'Awesome comment', noteable_type: 'Issue', noteable_id: issue.id }
   end
@@ -197,50 +197,64 @@ describe Notes::CreateService do
     end
 
     context 'note with commands' do
-      context 'as a user who can update the target' do
-        context 'all quick actions' do
-          QuickAction = Struct.new(:action_text, :attribute, :expected_value)
-
-          let!(:milestone) { create(:milestone, project: project, title: "sprint") }
-          let!(:label) { create(:label, project: project, title: 'bug') }
-          let!(:label_1) { create(:label, project: project, title: 'to be copied') }
-          let!(:issue_2) { create(:issue, project: project, labels: [label, label_1]) }
-
-          # Quick actions shared by issues and merge requests
-          let(:issuable_quick_actions) do
-            [
-              QuickAction.new("/close", "closed?", true),
-              QuickAction.new("/reopen", "opened?", true),
-              QuickAction.new("/assign @#{user.username}", "assignees", [user]),
-              QuickAction.new("/unassign @#{user.username}", "assignees", []),
-              QuickAction.new("/title new title", "title", "new title"),
-              QuickAction.new("/todo", "todos.count", 1),
-              QuickAction.new("/done", "todos.first.done?", true),
-              QuickAction.new("/unsubscribe", "subscriptions.first.subscribed", false),
-              QuickAction.new("/subscribe", "subscriptions.first.subscribed", true),
-              QuickAction.new("/lock", "discussion_locked", true),
-              QuickAction.new("/unlock", "discussion_locked", false),
-              QuickAction.new("/milestone %\"sprint\"", "milestone.id", milestone.id),
-              QuickAction.new("/remove_milestone", "milestone", nil),
-              QuickAction.new("/label ~bug", "labels.first.id", label.id),
-              QuickAction.new("/unlabel", "labels", []),
-              QuickAction.new("/award :100:", "award_emoji.last.name", "100"),
-              QuickAction.new("/estimate 1d 2h 3m", "time_estimate", 36180),
-              QuickAction.new("/remove_estimate", "time_estimate", 0),
-              QuickAction.new("/spend 1d 2h 3m", "total_time_spent", 36180),
-              QuickAction.new("/remove_time_spent", "total_time_spent", 0),
-              QuickAction.new("/shrug oops", "notes.last.note", "HELLO\noops ¯\\＿(ツ)＿/¯\nWORLD"),
-              QuickAction.new("/tableflip oops", "notes.last.note", "HELLO\noops (╯°□°)╯︵ ┻━┻\nWORLD"),
-              QuickAction.new("/copy_metadata #{issue_2.to_reference}", "labels", issue_2.labels)
-            ]
+      context 'all quick actions' do
+        QuickAction = Struct.new(:action_text, :attribute, :expected_value) do
+          def skip_access_check
+            action_text["/todo"] ||
+              action_text["/done"] ||
+              action_text["/subscribe"] ||
+              action_text["/shrug"] ||
+              action_text["/tableflip"]
           end
+        end
 
-          shared_examples 'issuable quick actions' do
+        set(:milestone) { create(:milestone, project: project, title: "sprint") }
+        set(:label) { create(:label, project: project, title: 'bug') }
+        set(:label_1) { create(:label, project: project, title: 'to be copied') }
+        set(:issue_2) { create(:issue, project: project, labels: [label, label_1]) }
+
+        # Quick actions shared by issues and merge requests
+        let(:issuable_quick_actions) do
+          [
+            QuickAction.new("/todo", "todos.count", 1),
+            QuickAction.new("/done", "todos.last.done?", true),
+            QuickAction.new("/close", "closed?", true),
+            QuickAction.new("/reopen", "opened?", true),
+            QuickAction.new("/assign @#{user.username}", "assignees", [user]),
+            QuickAction.new("/unassign @#{user.username}", "assignees", []),
+            QuickAction.new("/title new title", "title", "new title"),
+            QuickAction.new("/unsubscribe", "subscriptions&.first&.subscribed", false),
+            QuickAction.new("/subscribe", "subscriptions&.first&.subscribed", true),
+            QuickAction.new("/lock", "discussion_locked", true),
+            QuickAction.new("/unlock", "discussion_locked", false),
+            QuickAction.new("/milestone %\"sprint\"", "milestone&.id", milestone.id),
+            QuickAction.new("/remove_milestone", "milestone", nil),
+            QuickAction.new("/label ~bug", "labels&.first&.id", label.id),
+            QuickAction.new("/unlabel", "labels", []),
+            QuickAction.new("/award :100:", "award_emoji&.last&.name", "100"),
+            QuickAction.new("/estimate 1d 2h 3m", "time_estimate", 36180),
+            QuickAction.new("/remove_estimate", "time_estimate", 0),
+            QuickAction.new("/spend 1d 2h 3m", "total_time_spent", 36180),
+            QuickAction.new("/remove_time_spent", "total_time_spent", 0),
+            QuickAction.new("/shrug oops", "notes&.last&.note", "HELLO\noops ¯\\＿(ツ)＿/¯\nWORLD"),
+            QuickAction.new("/tableflip oops", "notes&.last&.note", "HELLO\noops (╯°□°)╯︵ ┻━┻\nWORLD"),
+            QuickAction.new("/copy_metadata #{issue_2.to_reference}", "labels", issue_2.labels)
+          ]
+        end
+
+        shared_examples 'issuable quick actions' do
+          context 'when user can update issuable' do
+            set(:developer) { create(:user) }
+
+            before do
+              project.add_developer(developer)
+            end
+
             it 'saves the note and updates the issue' do
               quick_actions.each do |quick_action|
                 note_text = %(HELLO\n#{quick_action.action_text}\nWORLD)
 
-                note = described_class.new(project, user, note_params.merge(note: note_text)).execute
+                note = described_class.new(project, developer, note_params.merge(note: note_text)).execute
                 noteable = note.noteable
 
                 # shrug and tablefip quick actions modifies the note text
@@ -254,69 +268,70 @@ describe Notes::CreateService do
             end
           end
 
-          context 'for issues' do
-            let(:note_params) { opts }
-            let(:issue_quick_actions) do
-              [
-                QuickAction.new("/confidential", "confidential", true),
-                QuickAction.new("/due 2016-08-28", "due_date", Date.new(2016, 8, 28)),
-                QuickAction.new("/remove_due_date", "due_date", nil),
-                QuickAction.new("/duplicate #{issue_2.to_reference}", "closed?", true)
-              ]
+          context 'when user cannot update issuable' do
+            set(:non_member) { create(:user) }
+
+            it 'applies commands that user can execute' do
+              quick_actions.each do |quick_action|
+                note_text = %(HELLO\n#{quick_action.action_text}\nWORLD)
+                old_value = issuable.instance_eval(quick_action.attribute)
+
+                note = described_class.new(project, non_member, note_params.merge(note: note_text)).execute
+                noteable = note.noteable
+
+                if quick_action.skip_access_check
+                  expect(noteable.instance_eval(quick_action.attribute)).to eq(quick_action.expected_value)
+                else
+                  expect(noteable.instance_eval(quick_action.attribute)).to eq(old_value)
+                end
+              end
             end
-            let(:quick_actions) { issuable_quick_actions + issue_quick_actions }
-
-            it_behaves_like 'issuable quick actions'
-          end
-
-          context 'for merge requests' do
-            let(:merge_request) { create(:merge_request, source_project: project) }
-            let(:note_params) { opts.merge(noteable_type: 'MergeRequest', noteable_id: merge_request.id) }
-            let(:merge_request_quick_actions) do
-              [
-                QuickAction.new("/target_branch fix", "target_branch", "fix"),
-                QuickAction.new("/wip", "work_in_progress?", true),
-                QuickAction.new("/wip", "work_in_progress?", false)
-              ]
-            end
-            let(:quick_actions) { issuable_quick_actions + merge_request_quick_actions }
-
-            it_behaves_like 'issuable quick actions'
           end
         end
 
-        context 'when note only have commands' do
-          it 'adds commands applied message to note errors' do
-            note_text = %(/close)
-            service = double(:service)
-            allow(Issues::UpdateService).to receive(:new).and_return(service)
-            expect(service).to receive(:execute)
-
-            note = described_class.new(project, user, opts.merge(note: note_text)).execute
-
-            expect(note.errors[:commands_only]).to be_present
+        context 'for issues' do
+          let(:issuable) { issue }
+          let(:note_params) { opts }
+          let(:issue_quick_actions) do
+            [
+              QuickAction.new("/confidential", "confidential", true),
+              QuickAction.new("/due 2016-08-28", "due_date", Date.new(2016, 8, 28)),
+              QuickAction.new("/remove_due_date", "due_date", nil),
+              QuickAction.new("/duplicate #{issue_2.to_reference}", "closed?", true)
+            ]
           end
+          let(:quick_actions) { issuable_quick_actions + issue_quick_actions }
+
+          it_behaves_like 'issuable quick actions'
+        end
+
+        context 'for merge requests' do
+          set(:merge_request) { create(:merge_request, source_project: project) }
+          let(:issuable) { merge_request }
+          let(:note_params) { opts.merge(noteable_type: 'MergeRequest', noteable_id: merge_request.id) }
+          let(:merge_request_quick_actions) do
+            [
+              QuickAction.new("/target_branch fix", "target_branch", "fix"),
+              QuickAction.new("/wip", "work_in_progress?", true),
+              QuickAction.new("/wip", "work_in_progress?", false)
+            ]
+          end
+          let(:quick_actions) { issuable_quick_actions + merge_request_quick_actions }
+
+          it_behaves_like 'issuable quick actions'
         end
       end
 
-      context 'as a user who cannot update the target' do
-        let(:note_text) { "HELLO\n/todo\n/assign #{user.to_reference}\nWORLD" }
-        let(:note) { described_class.new(project, user, opts.merge(note: note_text)).execute }
+      context 'when note only have commands' do
+        it 'adds commands applied message to note errors' do
+          note_text = %(/close)
+          service = double(:service)
+          allow(Issues::UpdateService).to receive(:new).and_return(service)
+          expect(service).to receive(:execute)
 
-        before do
-          project.team.find_member(user.id).update!(access_level: Gitlab::Access::GUEST)
-        end
+          note = described_class.new(project, user, opts.merge(note: note_text)).execute
 
-        it 'applies commands the user can execute' do
-          expect { note }.to change { user.todos_pending_count }.from(0).to(1)
-        end
-
-        it 'does not apply commands the user cannot execute' do
-          expect { note }.not_to change { issue.assignees }
-        end
-
-        it 'saves the note' do
-          expect(note.note).to eq "HELLO\nWORLD"
+          expect(note.errors[:commands_only]).to be_present
         end
       end
     end
