c0e431da4b64ac7a13a35919bbb08aea415c3e83
Jason Goodman <jgoodman@gitlab.com>
2019-06-03 19:27:03 -0400

    Fix rubocop errors

--- ee/spec/models/operations/unleash_feature_flag_spec.rb
+++ ee/spec/models/operations/unleash_feature_flag_spec.rb
@@ -5,7 +5,7 @@ require 'spec_helper'
 describe Operations::UnleashFeatureFlag do
   it 'does not allow modification of the operations_feature_flags table' do
     feature_flag = create(:operations_feature_flag, name: 'myflag')
-    unleash_feature_flag = Operations::UnleashFeatureFlag.find(feature_flag.id)
+    unleash_feature_flag = described_class.find(feature_flag.id)
 
     expect { unleash_feature_flag.update(name: 'change') }.to raise_error(ActiveRecord::ReadOnlyRecord)
 
