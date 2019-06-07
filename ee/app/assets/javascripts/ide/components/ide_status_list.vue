cb9bc5babb2c65416f5039bbfa4dbbef2505a28a
Paul Slaughter <pslaughter@gitlab.com>
2019-06-06 20:50:08 -0500

    Extract ide_status_list from ide_status_bar
    **Why?**
    The ide_status_list will be used and extended in EE.

---
+++ ee/app/assets/javascripts/ide/components/ide_status_list.vue
@@ -0,0 +1,13 @@
+<script>
+import IdeStatusList from '~/ide/components/ide_status_list.vue';
+
+export default {
+  components: {
+    IdeStatusList,
+  },
+};
+</script>
+
+<template>
+  <ide-status-list />
+</template>
