71843ceab8db9abf18287d1808ef29f82222d9ad
Kamil Trzciński <ayufan@ayufan.eu>
2019-06-25 17:08:59 +0200

    Fix Prometheus metrics cleanup

--- config/initializers/7_prometheus_metrics.rb
+++ config/initializers/7_prometheus_metrics.rb
@@ -1,15 +1,24 @@
 require 'prometheus/client'
 require 'prometheus/client/support/unicorn'
 
+def prometheus_multiproc_dir
+  dir = ENV['prometheus_multiproc_dir']
+  dir ||= Rails.root.join('tmp/prometheus_multiproc_dir') if Rails.env.development? || Rails.env.test?
+  dir
+end
+
+def remove_prometheus_multiproc_dir
+  return unless prometheus_multiproc_dir
+
+  old_metrics = Dir[File.join(prometheus_multiproc_dir, '*.db')]
+  FileUtils.rm_rf(old_metrics)
+end
+
 Prometheus::Client.configure do |config|
   config.logger = Rails.logger
 
   config.initial_mmap_file_size = 4 * 1024
-  config.multiprocess_files_dir = ENV['prometheus_multiproc_dir']
-
-  if Rails.env.development? || Rails.env.test?
-    config.multiprocess_files_dir ||= Rails.root.join('tmp/prometheus_multiproc_dir')
-  end
+  config.multiprocess_files_dir = prometheus_multiproc_dir
 
   config.pid_provider = Prometheus::Client::Support::Unicorn.method(:worker_pid_provider)
 end
@@ -43,14 +52,14 @@ if !Rails.env.test? && Gitlab::Metrics.prometheus_metrics_enabled?
   end
 end
 
+Gitlab::Cluster::LifecycleEvents.on_master_start do
+  remove_prometheus_multiproc_dir
+end
+
 Gitlab::Cluster::LifecycleEvents.on_master_restart do
   # The following is necessary to ensure stale Prometheus metrics don't
   # accumulate over time. It needs to be done in this hook as opposed to
   # inside an init script to ensure metrics files aren't deleted after new
   # unicorn workers start after a SIGUSR2 is received.
-  prometheus_multiproc_dir = ENV['prometheus_multiproc_dir']
-  if prometheus_multiproc_dir
-    old_metrics = Dir[File.join(prometheus_multiproc_dir, '*.db')]
-    FileUtils.rm_rf(old_metrics)
-  end
+  remove_prometheus_multiproc_dir
 end
