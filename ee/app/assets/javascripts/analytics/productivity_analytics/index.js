10af5f561dd76346b358fce6abe0aaacdbca9792
Martin Wortschack <mwortschack@gitlab.com>
2019-06-25 10:55:42 +0200

    Remove filter for now

--- ee/app/assets/javascripts/analytics/productivity_analytics/index.js
+++ ee/app/assets/javascripts/analytics/productivity_analytics/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import ProductivityAnalyticsApp from './components/app.vue';
-import FilteredSearchProductivityAnalytics from './filtered_search_productivity_analytics';
 
 export default function(el) {
   if (!el) {
@@ -11,15 +10,6 @@ export default function(el) {
     el,
     components: {
       ProductivityAnalyticsApp,
-    },
-    mounted() {
-      /*
-      this.filterManager = new FilteredSearchProductivityAnalytics(
-        store.state.productivityAnalytics.filters,
-      );
-      */
-      this.filterManager = new FilteredSearchProductivityAnalytics([]);
-      this.filterManager.setup();
     },
     render(h) {
       return h(ProductivityAnalyticsApp, {
