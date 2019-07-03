8be209533a22c0663a4ab2e103b6663e4f4255a5
Tetiana Chupryna <tchupryna@gitlab.com>
2019-07-03 17:35:06 +0300

    Finalize api request

--- ee/lib/api/dependencies.rb
+++ ee/lib/api/dependencies.rb
@@ -2,6 +2,17 @@
 
 module API
   class Dependencies < Grape::API
+    helpers do
+      def dependencies_by(params)
+        pipeline = user_project.all_pipelines.latest_successful_for(user_project.default_branch)
+
+        return [] unless pipeline
+
+        ::Security::DependencyListService.new(pipeline: pipeline, params: {}).execute
+      end
+    end
+
+
     before do
       authenticate!
     end
@@ -16,8 +27,9 @@ module API
       end
 
       get ':id/dependencies' do
-        pipeline = user_project.all_pipelines.latest_successful_for(user_project.default_branch)
-        dependencies = ::Security::DependencyListService.new(pipeline: pipeline, params: {}).execute
+        authorize! :read_dependencies, user_project
+
+        dependencies = dependencies_by(declared_params.merge(project: user_project))
 
         present dependencies, with: ::EE::API::Entities::Dependency
       end
--- ee/spec/requests/api/dependencies_spec.rb
+++ ee/spec/requests/api/dependencies_spec.rb
@@ -3,12 +3,12 @@
 require 'spec_helper'
 
 describe API::Dependencies do
-  set(:project) { create(:project, :public) }
+  set(:project) { create(:project, :private) }
   set(:user) { create(:user) }
 
   describe "GET /projects/:id/dependencies" do
     before do
-      stub_licensed_features(dependency_list: true)
+      stub_licensed_features(dependency_list: true, security_dashboard: true)
     end
 
     context 'with an authorized user with proper permissions' do
@@ -17,23 +17,37 @@ describe API::Dependencies do
       before do
         project.add_developer(user)
 
-        get api("/projects/#{project.id}/dependencies", user)
+        get api("/projects/#{project.id}/dependencies", user) #to subject
       end
 
       it 'returns all dependencies' do
-
         expect(response).to have_gitlab_http_status(200)
         expect(response).to match_response_schema('public_api/v4/dependencies', dir: 'ee')
+
+        expect(json_response.length).to eq(21)
+      end
+
+      context 'with filter options' do
+
       end
     end
 
-    # context 'with authorized user without read permissions' do
-    # end
-    #
-    # context 'with no project access' do
-    # end
-    #
-    # context 'with unknown project' do
-    # end
+    context 'with authorized user without read permissions' do
+      it 'responds with 403 Forbidden' do
+        get api("/projects/#{project.id}/dependencies", user)
+
+        expect(response).to have_gitlab_http_status(403)
+      end
+    end
+
+    context 'with no project access' do
+      it 'responds with 404 Not Found' do
+        private_project = create(:project)
+
+        get api("/projects/#{private_project.id}/dependencies", user)
+
+        expect(response).to have_gitlab_http_status(404)
+      end
+    end
   end
 end
