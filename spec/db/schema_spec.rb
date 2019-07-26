ngen-brett/gitlab-ce

Inherit {start,end}_date from child epics or milestones
Epic would not inherit start_date or end_date from its
related issues milestone dates or from child epic depending on which one
gives the wider time spread.

https://gitlab.com/gitlab-org/gitlab-ee/issues/7332

spec/db/schema_spec.rb
@@ -28,7 +28,7 @@ describe 'Database schema' do
     draft_notes: %w[discussion_id],
     emails: %w[user_id],
     events: %w[target_id],
-    epics: %w[updated_by_id last_edited_by_id start_date_sourcing_milestone_id due_date_sourcing_milestone_id],
+    epics: %w[updated_by_id last_edited_by_id start_date_sourcing_milestone_id due_date_sourcing_milestone_id start_date_sourcing_epic_id end_date_sourcing_epic_id],
     forked_project_links: %w[forked_from_project_id],
     geo_event_log: %w[hashed_storage_attachments_event_id],
     geo_job_artifact_deleted_events: %w[job_artifact_id],

mehulagg/gitlab_test

spec/db/schema_spec.rb
@@ -32,7 +32,7 @@ describe 'Database schema' do
     draft_notes: %w[discussion_id],
     emails: %w[user_id],
     events: %w[target_id],
-    epics: %w[updated_by_id last_edited_by_id start_date_sourcing_milestone_id due_date_sourcing_milestone_id],
+    epics: %w[updated_by_id last_edited_by_id start_date_sourcing_milestone_id due_date_sourcing_milestone_id start_date_sourcing_epic_id due_date_sourcing_epic_id],
     forked_project_links: %w[forked_from_project_id],
     geo_event_log: %w[hashed_storage_attachments_event_id],
     geo_job_artifact_deleted_events: %w[job_artifact_id],