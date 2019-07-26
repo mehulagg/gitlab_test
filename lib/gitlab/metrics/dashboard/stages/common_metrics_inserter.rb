f1dce41fe44e6d2981cf63471b8aa9575d5e8b71
Peter Leitzen <pleitzen@gitlab.com>
2019-07-26 14:40:47 +0200

    Improve performance finding dashboard metrics
    Index common metrics to minimize iterations.

--- lib/gitlab/metrics/dashboard/stages/common_metrics_inserter.rb
+++ lib/gitlab/metrics/dashboard/stages/common_metrics_inserter.rb
@@ -9,10 +9,10 @@ module Gitlab
           # find a corresponding database record. If found,
           # includes the record's id in the dashboard config.
           def transform!
-            common_metrics = ::PrometheusMetric.common
+            common_metrics = ::PrometheusMetric.common.index_by(&:identifier)
 
             for_metrics do |metric|
-              metric_record = common_metrics.find { |m| m.identifier == metric[:id] }
+              metric_record = common_metrics[metric[:id]]
               metric[:metric_id] = metric_record.id if metric_record
             end
           end
