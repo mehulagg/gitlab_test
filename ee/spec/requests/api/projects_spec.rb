0c4691479f6edbcdc620aa2fa783d7afb865c6db
Ben Bodenmiller <bbodenmiller@hotmail.com>
2019-08-17 14:22:52 +0000

    Update file projects_spec.rb

--- ee/spec/requests/api/projects_spec.rb
+++ ee/spec/requests/api/projects_spec.rb
@@ -227,48 +227,6 @@ describe API::Projects do
       end
     end
 
-    context 'when updating repository storage' do
-      let(:unknown_storage) { 'new-storage' }
-      let(:new_project) { create(:project, :repository, namespace: user.namespace) }
-
-      context 'as a user' do
-        it 'returns 200 but does not change repository_storage' do
-          expect do
-            Sidekiq::Testing.fake! do
-              put(api("/projects/#{new_project.id}", user), params: { repository_storage: unknown_storage, issues_enabled: false })
-            end
-          end.not_to change(ProjectUpdateRepositoryStorageWorker.jobs, :size)
-
-          expect(response).to have_gitlab_http_status(200)
-          expect(json_response['issues_enabled']).to eq(false)
-          expect(new_project.reload.repository.storage).to eq('default')
-        end
-      end
-
-      context 'as an admin' do
-        let(:admin) { create(:admin) }
-
-        it 'returns 500 when repository storage is unknown' do
-          put(api("/projects/#{new_project.id}", admin), params: { repository_storage: unknown_storage })
-
-          expect(response).to have_gitlab_http_status(500)
-          expect(json_response['message']).to match('ArgumentError')
-        end
-
-        it 'returns 200 when repository storage has changed' do
-          stub_storage_settings('extra' => { 'path' => 'tmp/tests/extra_storage' })
-
-          expect do
-            Sidekiq::Testing.fake! do
-              put(api("/projects/#{new_project.id}", admin), params: { repository_storage: 'extra' })
-            end
-          end.to change(ProjectUpdateRepositoryStorageWorker.jobs, :size).by(1)
-
-          expect(response).to have_gitlab_http_status(200)
-        end
-      end
-    end
-
     context 'when updating mirror related attributes' do
       let(:import_url) { generate(:url) }
       let(:mirror_params) do
