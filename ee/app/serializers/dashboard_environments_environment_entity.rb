2aaa325ba6b725d04859a35da8beb666ba4bbc65
Jason Goodman <jgoodman@gitlab.com>
2019-08-19 15:48:27 -0400

    Return last deployment in environment dashboard
    Reuse DeploymentEntity class

--- ee/app/serializers/dashboard_environments_environment_entity.rb
+++ ee/app/serializers/dashboard_environments_environment_entity.rb
@@ -11,4 +11,18 @@ class DashboardEnvironmentsEnvironmentEntity < Grape::Entity
   expose :environment_path do |environment|
     project_environment_path(environment.project, environment.raw_environment)
   end
+  expose :last_deployment do |environment|
+    DeploymentEntity.represent(environment.last_deployment, options.merge(request: new_request))
+  end
+
+  private
+
+  alias_method :environment, :object
+
+  def new_request
+    EntityRequest.new(
+      current_user: request.current_user,
+      project: environment.project
+    )
+  end
 end
