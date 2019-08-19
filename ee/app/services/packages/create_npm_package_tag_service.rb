2dcc444a8d123e516192d88c69713e838a7fbe22
Sara Ahbabou <sahbabou@gitab.com>
2019-08-19 01:12:18 -0700

    Remove extra space

--- ee/app/services/packages/create_npm_package_tag_service.rb
+++ ee/app/services/packages/create_npm_package_tag_service.rb
@@ -2,23 +2,5 @@
 module Packages
   class CreateNpmPackageTagService < BaseService
     attr_reader :package, :tag
-
-    def initialize(package, tag)
-      @package = package
-      @tag = tag
-    end
-
-    def execute
-      packages = Packages::PackageTag.find_packages_by_package_tag(tag, package.name)
-      if !packages.empty?
-        update_tag(packages.last)
-      else
-        package.package_tags.create(name: tag)
-      end
-    end
-
-    def update_tag(package_tag)
-      package_tag.update!(package_id: package.id)
-    end
   end
 end
