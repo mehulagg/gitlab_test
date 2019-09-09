a11bf67760cc038acfc966c45e2c9d025c38b4e7
Mayra Cabrera <mcabrera@gitlab.com>
2019-09-09 14:19:37 -0500

    Moves related issues
    Duplicates related issues to the new issue when an issue is moved

--- ee/app/services/ee/issues/move_service.rb
+++ ee/app/services/ee/issues/move_service.rb
@@ -8,6 +8,8 @@ module EE
       override :update_old_entity
       def update_old_entity
         rewrite_epic_issue
+        duplicate_related_issues
+
         super
       end
 
@@ -19,6 +21,32 @@ module EE
 
         epic_issue.update(issue_id: new_entity.id)
         original_entity.reset
+      end
+
+      def duplicate_related_issues
+        source_issue_links = IssueLink.for_source_issue(original_entity)
+        target_issue_links = IssueLink.for_target_issue(original_entity)
+
+        issue_links_attributes = source_issue_links
+          .map { |issue_link| [new_entity.id, issue_link.target_id] }
+          .concat(
+            target_issue_links.map { |issue_link| [issue_link.source_id, new_entity.id] }
+          )
+
+        return if issue_links_attributes.empty?
+
+        connection.execute(insert_sql_query(issue_links_attributes))
+      end
+
+      def insert_sql_query(issue_links)
+        <<~SQL
+        INSERT INTO #{IssueLink.table_name} (source_id, target_id)
+        VALUES #{issue_links.map { |issue_link| "(#{issue_link.join(', ')})" }.join(', ')}
+        SQL
+      end
+
+      def connection
+        ActiveRecord::Base.connection
       end
     end
   end
