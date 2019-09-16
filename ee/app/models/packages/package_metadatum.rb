64f0e73b1ad61896b799de5e9f348756d36174ca
Sara Ahbabou <sahbabou@gitab.com>
2019-09-16 08:57:40 -0700

    Add metadata size limit

--- ee/app/models/packages/package_metadatum.rb
+++ ee/app/models/packages/package_metadatum.rb
@@ -5,14 +5,4 @@ class Packages::PackageMetadatum < ApplicationRecord
   validates :package, presence: true
   validates_size_of :metadata, { in: 0..10.kilobytes }
 
-  def self.map_metadata(metadata)
-    metadata.each_pair do |name, metadatum|
-      if metadatum.is_a?(Hash)
-        map_metadata(metadatum)
-      else
-        name
-      end
-    end
-  end
-
 end
