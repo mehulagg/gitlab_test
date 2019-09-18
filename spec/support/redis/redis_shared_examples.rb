ea1e541fcc5820c8cac8cd0e87663967a3dfd7a3
Fabian Zimmer <fzimmer@gitlab.com>
2019-09-18 17:44:56 +0200

    Renamed remaining found instances

--- spec/support/redis/redis_shared_examples.rb
+++ spec/support/redis/redis_shared_examples.rb
@@ -149,7 +149,7 @@ RSpec.shared_examples "redis_shared_examples" do
 
       it 'returns an array of hashes with host and port keys' do
         is_expected.to include(host: 'localhost', port: sentinel_port)
-        is_expected.to include(host: 'slave2', port: sentinel_port)
+        is_expected.to include(host: 'secondary2', port: sentinel_port)
       end
     end
 
