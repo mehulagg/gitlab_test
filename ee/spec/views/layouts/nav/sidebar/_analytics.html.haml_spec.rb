9f36c2b61515101ba0fd6c8d9657a41d84f92ea2
Brandon Labuschagne <blabuschagne@gitlab.com>
2019-10-08 12:08:17 +0200

    Add code analytics navlink

--- ee/spec/views/layouts/nav/sidebar/_analytics.html.haml_spec.rb
+++ ee/spec/views/layouts/nav/sidebar/_analytics.html.haml_spec.rb
@@ -38,6 +38,16 @@ describe 'layouts/nav/sidebar/_analytics' do
         expect(rendered).to include(analytics_cycle_analytics_path)
         expect(rendered).to match(/<use xlink:href=".+?icons-.+?#repeat">/)
       end
+
+      it 'has `Code Analytics` link' do
+        stub_feature_flags(Gitlab::Analytics::CODE_ANALYTICS_FEATURE_FLAG => true)
+
+        render
+
+        expect(rendered).to have_content('Code Analytics')
+        expect(rendered).to include(analytics_code_analytics_path)
+        expect(rendered).to match(/<use xlink:href=".+?icons-.+?#code">/)
+      end
     end
 
     context 'when feature flags are disabled' do
@@ -46,6 +56,7 @@ describe 'layouts/nav/sidebar/_analytics' do
 
         expect(rendered).not_to have_content('Productivity Analytics')
         expect(rendered).not_to have_content('Cycle Analytics')
+        expect(rendered).not_to have_content('Code Analytics')
       end
     end
   end
