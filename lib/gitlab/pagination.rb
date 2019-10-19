f70818fab905e94934510f64392056fd8f9401de
Andreas Brandl <abrandl@gitlab.com>
2019-10-19 12:12:55 +0200

    Refactor pagination
    This refactors the pagination helper and extracts the logic into
    Gitlab::Pagination. The goal here is to change
    Gitlab::Serializer::Pagination to *not* use Api::Helpers::Pagination
    anymore. Instead, it calls the extracted classes directly.
    
    This is a preparation to add more pagination strategies to the API only
    (like keyset pagination).

---
+++ lib/gitlab/pagination.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+module Gitlab
+  module Pagination
+  end
+end
---
+++ lib/gitlab/pagination/paginate.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module Gitlab
+    module Pagination
+        module Paginate
+            def paginate(resource)
+            end
+        end
+    end
+end
