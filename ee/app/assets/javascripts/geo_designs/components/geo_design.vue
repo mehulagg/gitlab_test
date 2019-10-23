55a79c503b5566c3440c40070f2e90930ec170f8
Zack Cuddy <zcuddy@gitlab.com>
2019-10-23 14:52:14 -0500

    Scaffold out Card and Card Header

---
+++ ee/app/assets/javascripts/geo_designs/components/geo_design.vue
@@ -0,0 +1,40 @@
+<script>
+  // Using this for now, but might need to wrap into a new GlCard.
+  // eslint-disable-next-line import/no-extraneous-dependencies
+  import { BCard, BCardHeader, BCardBody } from 'bootstrap-vue'
+  import { GlLink, GlButton } from '@gitlab/ui';
+
+  export default {
+    name: 'GeoDesign',
+    components: {
+      BCard,
+      BCardHeader,
+      BCardBody,
+      GlLink,
+      GlButton
+    },
+    props: {
+      design: {
+        type: Object,
+        required: true
+      }
+    },
+    data() {
+      return {
+      }
+    },
+  }
+</script>
+
+<template>
+  <b-card no-body>
+    <b-card-header class="d-flex align-center">
+      <gl-link class="font-weight-bold" :href="design.url" target="_blank">{{ design.name }}</gl-link>
+      <div class="ml-auto">
+        <gl-button>{{ __("Reverify") }}</gl-button>
+        <gl-button>{{ __("Resync") }}</gl-button>
+      </div>
+    </b-card-header>
+    <b-card-body>{{ __("This is the card body") }}</b-card-body>
+  </b-card>
+</template>
--- ee/app/assets/javascripts/geo_designs/components/geo_designs.vue
+++ ee/app/assets/javascripts/geo_designs/components/geo_designs.vue
@@ -1,15 +1,38 @@
 <script>
+  import GeoDesign from './geo_design.vue'
+  import { __ } from '~/locale';
+
   export default {
     name: 'GeoDesigns',
+    components: {
+      GeoDesign
+    },
     data() {
       return {
+        designs: [
+          {
+            id: 1,
+            name: __('Zack\'s Design Repo'),
+            url: 'http://localhost:3002'
+          },
+          {
+            id: 2,
+            name: __('Valery\'s Design Repo'),
+            url: 'http://localhost:3002'
+          },
+          {
+            id: 3,
+            name: __('Mike\'s Design Repo'),
+            url: 'http://localhost:3002'
+          },
+        ]
       }
     },
   }
 </script>
 
 <template>
-  <div>
-    <h3>{{ __("Geo Designs") }}</h3>
-  </div>
+  <section>
+    <geo-design v-for="design in designs" :key="design.id" :design="design" />
+  </section>
 </template>
