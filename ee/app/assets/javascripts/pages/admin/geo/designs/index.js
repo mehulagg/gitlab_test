484b09831097a6a8f62ce9d15d2d1c464f146bce
Zack Cuddy <zcuddy@gitlab.com>
2019-10-25 12:45:12 -0500

    Removed call from wrong file

--- ee/app/assets/javascripts/pages/admin/geo/designs/index.js
+++ ee/app/assets/javascripts/pages/admin/geo/designs/index.js
@@ -1,8 +1,3 @@
 import initGeoDesigns from 'ee/geo_designs';
-import PersistentUserCallout from '~/persistent_user_callout';
 
 document.addEventListener('DOMContentLoaded', initGeoDesigns);
-document.addEventListener('DOMContentLoaded', () => {
-  const callout = document.querySelector('.user-callout');
-  PersistentUserCallout.factory(callout);
-});
