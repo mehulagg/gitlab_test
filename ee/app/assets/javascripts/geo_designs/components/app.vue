3f1e7082c648870673de76f91a20f2ad3a90c1a0
Zack Cuddy <zcuddy@gitlab.com>
2019-10-28 08:05:41 -0500

    Add loading icon

--- ee/app/assets/javascripts/geo_designs/components/app.vue
+++ ee/app/assets/javascripts/geo_designs/components/app.vue
@@ -1,4 +1,5 @@
 <script>
+import { GlLoadingIcon } from '@gitlab/ui';
 import { mapActions, mapState } from 'vuex';
 import GeoDesignsFilterBar from './geo_designs_filter_bar.vue';
 import GeoDesigns from './geo_designs.vue';
@@ -6,6 +7,7 @@ import GeoDesigns from './geo_designs.vue';
 export default {
   name: 'GeoDesignsApp',
   components: {
+    GlLoadingIcon,
     GeoDesignsFilterBar,
     GeoDesigns,
   },
@@ -31,6 +33,9 @@ export default {
 <template>
   <article class="geo-designs-container">
     <geo-designs-filter-bar />
-    <geo-designs />
+    <section>
+      <gl-loading-icon v-if="isLoading" size="xl" />
+      <geo-designs v-else />
+    </section>
   </article>
 </template>
