2283e12eeb6a40371827f95c706eb6da926ab457
James Fargher <proglottis@gmail.com>
2019-11-11 16:40:39 +1300

    Force helm upgrade so that it can also downgrade

--- lib/gitlab/kubernetes/helm/client_command.rb
+++ lib/gitlab/kubernetes/helm/client_command.rb
@@ -9,7 +9,7 @@ module Gitlab
           # installing an app. We ensure the helm version stored in the
           # database is correct by also updating this after transition to
           # :installed,:updated in Clusters::Concerns::ApplicationStatus
-          'helm init --upgrade'
+          'helm init --upgrade --force-upgrade'
         end
 
         def wait_for_tiller_command
