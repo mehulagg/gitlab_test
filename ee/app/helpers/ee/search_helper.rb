4864e83e7d8b273e15cbff4f13a938defa5b7a33
Juan <jjramirez.u@gmail.com>
2019-11-19 16:43:17 -0600

    Code tag styling for search term
    add changelog
    
    locale add
    
    Fixing parenthesis method call error
    
    Fixing test for unquoted search term
    
    Fixing failed string tests
    
    Fixing failed tests for new term styling.
    
    Update changelog
    
    Add locale translations
    
    Fix for search_entries_info_template test
    
    Updates to locale
    
    Removing unused slash and adding locales
    
    Adding span and fixing spacing issues
    
    Changing html and locales in search term
    
    Update in test

--- ee/app/helpers/ee/search_helper.rb
+++ ee/app/helpers/ee/search_helper.rb
@@ -56,9 +56,11 @@ module EE
       return super unless gitlab_com_snippet_db_search?
 
       if collection.total_pages > 1
-        s_("SearchResults|Showing %{from} - %{to} of %{count} %{scope} for \"%{term}\" in your personal and project snippets")
+        term = "<span>&nbsp;<code>%{term}</code>&nbsp;</span>"
+        s_("SearchResults|Showing %{count} %{scope} for#{term} in your personal and project snippets").html_safe
       else
-        s_("SearchResults|Showing %{count} %{scope} for \"%{term}\" in your personal and project snippets")
+        term = "<span>&nbsp;<code>%{term}</code>&nbsp;</span>"
+        s_("SearchResults|Showing %{count} %{scope} for#{term} in your personal and project snippets").html_safe
       end
     end
 
