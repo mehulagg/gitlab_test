b83ce1da5344859a21372e6e7d437989d2137e80
drew cimino <dcimino@gitlab.com>
2019-11-20 19:44:04 +0100

    Cleaned up unecessary spec code

--- ee/spec/services/ci/create_cross_project_pipeline_service_spec.rb
+++ ee/spec/services/ci/create_cross_project_pipeline_service_spec.rb
@@ -191,10 +191,6 @@ describe Ci::CreateCrossProjectPipelineService, '#execute' do
     context 'when configured with bridge job rules' do
       before do
         stub_ci_pipeline_yaml_file(config)
-
-        upstream_project
-        downstream_project
-
         downstream_project.add_maintainer(upstream_project.owner)
       end
 
@@ -213,25 +209,12 @@ describe Ci::CreateCrossProjectPipelineService, '#execute' do
       end
 
       let(:primary_pipeline) do
-        params = { ref: 'master',
-                   before: '00000000',
-                   after: upstream_project.commit.id,
-                   commits: [{ message: 'Message' }],
-                   variables_attributes: nil,
-                   push_options: nil,
-                   source_sha: nil,
-                   target_sha: nil }
-
-        Ci::CreatePipelineService.new(upstream_project, upstream_project.owner, params).execute(:push,
-          save_on_errors: false,
-          trigger_request: nil,
-          merge_request: nil,
-          external_pull_request: nil
-        )
+        Ci::CreatePipelineService.new(upstream_project, upstream_project.owner, { ref: 'master' })
+          .execute(:push, save_on_errors: false)
       end
 
-      let(:bridge)             { primary_pipeline.processables.find_by(name: 'bridge-job') }
-      let(:service)            { described_class.new(upstream_project, upstream_project.owner) }
+      let(:bridge)  { primary_pipeline.processables.find_by(name: 'bridge-job') }
+      let(:service) { described_class.new(upstream_project, upstream_project.owner) }
 
       context 'that include the bridge job' do
         it 'creates the downstream pipeline' do
