2ff2b90f35e51e3537d9f04be406fce74cbcc42e
Tetiana Chupryna <tchupryna@gitlab.com>
2019-11-26 13:05:34 +0200

    Accept arg id to query vulnerability

--- ee/app/graphql/resolvers/vulnerabilities/occurrence_resolver.rb
+++ ee/app/graphql/resolvers/vulnerabilities/occurrence_resolver.rb
@@ -3,11 +3,13 @@
 module Resolvers
   module Vulnerabilities
     class OccurrenceResolver < BaseResolver
+      argument :id, GraphQL::ID_TYPE, required: true,
+               description: 'bla bla bla'
 
       type Types::Vulnerabilities::OccurrenceType, null: true
 
       def resolve(**args)
-        ::Vulnerabilities::Occurrence.last
+        ::Vulnerabilities::Occurrence.find(args[:id])
       end
     end
   end
