66362154f940fa646cafdd6c6a4397c2f7e0675c
Jason Goodman <jgoodman@gitlab.com>
2019-12-04 13:22:29 -0500

    Use bigint for iid

--- db/migrate/20191127175335_add_iid_to_operations_feature_flags.rb
+++ db/migrate/20191127175335_add_iid_to_operations_feature_flags.rb
@@ -6,7 +6,7 @@ class AddIidToOperationsFeatureFlags < ActiveRecord::Migration[5.2]
   DOWNTIME = false
 
   def up
-    add_column :operations_feature_flags, :iid, :integer
+    add_column :operations_feature_flags, :iid, :bigint
   end
 
   def down
