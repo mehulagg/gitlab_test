ba06f679438bd53e799965175f033b969e2a257e
manojmj <mmj@gitlab.com>
2019-12-06 17:16:19 +0530

    Use raw SQL Update to update `minimum_password_length`

--- db/post_migrate/20191205084057_update_minimum_password_length.rb
+++ db/post_migrate/20191205084057_update_minimum_password_length.rb
@@ -4,23 +4,17 @@ class UpdateMinimumPasswordLength < ActiveRecord::Migration[5.2]
   DOWNTIME = false
 
   def up
-    application_setting = ApplicationSetting.current_without_cache
-    return unless application_setting
-
     value_to_be_updated_to = [
       Devise.password_length.min,
       ApplicationSetting::DEFAULT_MINIMUM_PASSWORD_LENGTH
     ].max
 
-    application_setting.update(minimum_password_length: value_to_be_updated_to)
+    execute "UPDATE application_settings SET minimum_password_length = #{value_to_be_updated_to}"
   end
 
   def down
-    application_setting = ApplicationSetting.current_without_cache
-    return unless application_setting
-
     value_to_be_updated_to = ApplicationSetting::DEFAULT_MINIMUM_PASSWORD_LENGTH
 
-    application_setting.update(minimum_password_length: value_to_be_updated_to)
+    execute "UPDATE application_settings SET minimum_password_length = #{value_to_be_updated_to}"
   end
 end
