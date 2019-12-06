0d955a4c88e858b63eb1d568ba25ddf8bc0a712e
manojmj <mmj@gitlab.com>
2019-12-06 11:44:49 +0530

    Add post-deploy migration
    Adds a post-deploy migration to set the value of
    `minimum_password length`, if the user had already
    set a value different than 8, using the devise initializer.

---
+++ db/post_migrate/20191205084057_update_minimum_password_length.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+class UpdateMinimumPasswordLength < ActiveRecord::Migration[5.2]
+  DOWNTIME = false
+
+  def up
+    application_setting = ApplicationSetting.current_without_cache
+    return unless application_setting
+
+    value_to_be_updated_to = [
+      Devise.password_length.min,
+      ApplicationSetting::DEFAULT_MINIMUM_PASSWORD_LENGTH
+    ].max
+
+    application_setting.update(minimum_password_length: value_to_be_updated_to)
+  end
+
+  def down
+    application_setting = ApplicationSetting.current_without_cache
+    return unless application_setting
+
+    value_to_be_updated_to = ApplicationSetting::DEFAULT_MINIMUM_PASSWORD_LENGTH
+
+    application_setting.update(minimum_password_length: value_to_be_updated_to)
+  end
+end
