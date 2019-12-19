7256b9665ac65a953f21ae39692ce6296a976304
Tom Quirk <tquirk@gitlab.com>
2019-12-19 12:08:41 +1000

    Clean up cache_update

--- ee/app/assets/javascripts/design_management/utils/cache_update.js
+++ ee/app/assets/javascripts/design_management/utils/cache_update.js
@@ -1,49 +1,6 @@
 import createFlash from '~/flash';
 import { extractCurrentDiscussion, extractDesign } from './design_management_utils';
-import {
-  ADD_IMAGE_DIFF_NOTE_ERROR,
-  ADD_DISCUSSION_COMMENT_ERROR,
-  UPLOAD_DESIGN_ERROR,
-  designDeletionError,
-} from './error_messages';
-
-const deleteDesignsFromStore = (store, query, selectedDesigns) => {
-  const data = store.readQuery(query);
-
-  const changedDesigns = data.project.issue.designCollection.designs.edges.filter(
-    ({ node }) => !selectedDesigns.includes(node.filename),
-  );
-  data.project.issue.designCollection.designs.edges = [...changedDesigns];
-
-  store.writeQuery({
-    ...query,
-    data,
-  });
-};
-
-/**
- * Adds a new version of designs to store
- *
- * @param {Object} store
- * @param {Object} query
- * @param {Object} version
- */
-const addNewVersionToStore = (store, query, version) => {
-  if (!version) return;
-
-  const data = store.readQuery(query);
-  const newEdge = { node: version, __typename: 'DesignVersionEdge' };
-
-  data.project.issue.designCollection.versions.edges = [
-    newEdge,
-    ...data.project.issue.designCollection.versions.edges,
-  ];
-
-  store.writeQuery({
-    ...query,
-    data,
-  });
-};
+import { ADD_IMAGE_DIFF_NOTE_ERROR, ADD_DISCUSSION_COMMENT_ERROR } from './error_messages';
 
 const addDiscussionCommentToStore = (store, createNote, query, queryVariables, discussionId) => {
   const data = store.readQuery({
@@ -148,76 +105,9 @@ const addImageDiffNoteToStore = (store, createImageDiffNote, query, variables) =
   });
 };
 
-const addNewDesignToStore = (store, designManagementUpload, query) => {
-  const data = store.readQuery(query);
-
-  const newDesigns = data.project.issue.designCollection.designs.edges.reduce((acc, design) => {
-    if (!acc.find(d => d.filename === design.node.filename)) {
-      acc.push(design.node);
-    }
-
-    return acc;
-  }, designManagementUpload.designs);
-
-  let newVersionNode;
-  const findNewVersions = designManagementUpload.designs.find(design => design.versions);
-
-  if (findNewVersions) {
-    const findNewVersionsEdges = findNewVersions.versions.edges;
-
-    if (findNewVersionsEdges && findNewVersionsEdges.length) {
-      newVersionNode = [findNewVersionsEdges[0]];
-    }
-  }
-
-  const newVersions = [
-    ...(newVersionNode || []),
-    ...data.project.issue.designCollection.versions.edges,
-  ];
-
-  const updatedDesigns = {
-    __typename: 'DesignCollection',
-    designs: {
-      __typename: 'DesignConnection',
-      edges: newDesigns.map(design => ({
-        __typename: 'DesignEdge',
-        node: design,
-      })),
-    },
-    versions: {
-      __typename: 'DesignVersionConnection',
-      edges: newVersions,
-    },
-  };
-
-  data.project.issue.designCollection = updatedDesigns;
-
-  store.writeQuery({
-    ...query,
-    data,
-  });
-};
-
 const onError = (data, message) => {
   createFlash(message);
   throw new Error(data.errors);
-};
-
-/**
- * Updates a store after design deletion
- *
- * @param {Object} store
- * @param {Object} data
- * @param {Object} query
- * @param {Array} designs
- */
-export const updateStoreAfterDesignsDelete = (store, data, query, designs) => {
-  if (data.errors) {
-    onError(data, designDeletionError({ singular: designs.length === 1 }));
-  } else {
-    deleteDesignsFromStore(store, query, designs);
-    addNewVersionToStore(store, query, data.version);
-  }
 };
 
 export const updateStoreAfterAddDiscussionComment = (
@@ -239,13 +129,5 @@ export const updateStoreAfterAddImageDiffNote = (store, data, query, queryVariab
     onError(data, ADD_IMAGE_DIFF_NOTE_ERROR);
   } else {
     addImageDiffNoteToStore(store, data, query, queryVariables);
-  }
-};
-
-export const updateStoreAfterUploadDesign = (store, data, query) => {
-  if (data.errors) {
-    onError(data, UPLOAD_DESIGN_ERROR);
-  } else {
-    addNewDesignToStore(store, data, query);
   }
 };
