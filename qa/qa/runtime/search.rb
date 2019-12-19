04a137766e5db8a77aa4040b0eca7b99a6e5cb63
Mark Lapierre <mlapierre@gitlab.com>
2019-12-19 16:53:53 +1100

    Extend pre-test search to search a project

--- qa/qa/runtime/search.rb
+++ qa/qa/runtime/search.rb
@@ -15,7 +15,11 @@ module QA
 
         search_term = SecureRandom.hex(8)
         content = "Elasticsearch test commit #{search_term}"
+        project = Resource::Project.fabricate_via_api! do |project|
+          project.name = "project-to-search-#{search_term}1"
+        end
         Resource::Repository::Commit.fabricate_via_api! do |commit|
+          commit.project = project
           commit.commit_message = content
           commit.add_files(
             [
@@ -28,18 +32,26 @@ module QA
         end
 
         QA::Support::Retrier.retry_on_exception(max_attempts: 6, sleep_interval: 10) do
-          request = Runtime::API::Request.new(api_client, "/search?scope=commits&search=#{search_term}")
-          response = get(request.url)
-
-          unless response.code == singleton_class::HTTP_STATUS_OK
-            raise ElasticSearchServerError, "Search attempt failed. Request returned (#{response.code}): `#{response}`."
-          end
-
-          true
+          search('commits', "commit*#{search_term}") && search('projects', "to-search*#{search_term}1")
         end
       end
 
       private
+
+      def search(scope, term)
+        QA::Runtime::Logger.debug("Search scope '#{scope}' for '#{term}'...")
+        request = Runtime::API::Request.new(api_client, "/search?scope=#{scope}&search=#{term}")
+        response = get(request.url)
+
+        unless response.code == singleton_class::HTTP_STATUS_OK
+          msg = "Search attempt failed. Request returned (#{response.code}): `#{response}`."
+          QA::Runtime::Logger.debug(msg)
+          raise ElasticSearchServerError, msg
+        end
+
+        QA::Runtime::Logger.debug("Found '#{term}'...")
+        true
+      end
 
       def api_client
         @api_client ||= Runtime::API::Client.new(:gitlab)
