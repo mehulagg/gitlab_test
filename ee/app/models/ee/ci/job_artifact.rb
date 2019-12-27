1ded1877947c0284c353f3cfa9a4cee0a429c451
Tetiana Chupryna <tchupryna@gitlab.com>
2019-12-27 18:22:19 +0200

    Rename file constant
    License Management -> License Scanning

--- ee/app/models/ee/ci/job_artifact.rb
+++ ee/app/models/ee/ci/job_artifact.rb
@@ -12,7 +12,7 @@ module EE
       after_destroy :log_geo_deleted_event
 
       SECURITY_REPORT_FILE_TYPES = %w[sast dependency_scanning container_scanning dast].freeze
-      LICENSE_MANAGEMENT_REPORT_FILE_TYPES = %w[license_management license_scanning].freeze
+      LICENSE_SCANNING_REPORT_FILE_TYPES = %w[license_management license_scanning].freeze
       DEPENDENCY_LIST_REPORT_FILE_TYPES = %w[dependency_scanning].freeze
       METRICS_REPORT_FILE_TYPES = %w[metrics].freeze
       CONTAINER_SCANNING_REPORT_TYPES = %w[container_scanning].freeze
@@ -28,7 +28,7 @@ module EE
       end
 
       scope :license_management_reports, -> do
-        with_file_types(LICENSE_MANAGEMENT_REPORT_FILE_TYPES)
+        with_file_types(LICENSE_SCANNING_REPORT_FILE_TYPES)
       end
 
       scope :dependency_list_reports, -> do
