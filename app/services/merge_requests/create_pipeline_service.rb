510084998d3798ca2601917ba8bfe751bcd171ba
Jason Goodman <jgoodman@gitlab.com>
2020-01-08 17:18:31 -0500

    Do not persist merge request pipeline on push with an invalid yaml file
    Retain persistence for post to api

--- app/services/merge_requests/create_pipeline_service.rb
+++ app/services/merge_requests/create_pipeline_service.rb
@@ -2,10 +2,10 @@
 
 module MergeRequests
   class CreatePipelineService < MergeRequests::BaseService
-    def execute(merge_request)
+    def execute(merge_request, save_on_errors: true)
       return unless can_create_pipeline_for?(merge_request)
 
-      create_detached_merge_request_pipeline(merge_request)
+      create_detached_merge_request_pipeline(merge_request, save_on_errors)
     end
 
     def create_detached_merge_request_pipeline(merge_request)
