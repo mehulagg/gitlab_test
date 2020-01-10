3c6e5edb9aa3fb23945dd96adf86dce91c060db7
Jason Goodman <jgoodman@gitlab.com>
2020-01-09 22:02:07 -0500

    Do not save pipeline on config error for a push
    Add save_config_errors parameter

--- app/services/merge_requests/base_service.rb
+++ app/services/merge_requests/base_service.rb
@@ -85,7 +85,7 @@ module MergeRequests
     end
 
     def create_pipeline_for(merge_request, user)
-      MergeRequests::CreatePipelineService.new(project, user, params).execute(merge_request)
+      MergeRequests::CreatePipelineService.new(project, user, params).execute(merge_request, save_config_errors: false)
     end
 
     def can_use_merge_request_ref?(merge_request)
