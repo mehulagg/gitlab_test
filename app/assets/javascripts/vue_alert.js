44071b95d1049cfe0378321b8a85bb3e96437b80
Nathan Friend <nathan@gitlab.com>
2020-01-16 15:21:39 -0500

    Some suggestions from @nfriend

--- app/assets/javascripts/vue_alert.js
+++ app/assets/javascripts/vue_alert.js
@@ -1,11 +1,8 @@
 import Vue from 'vue';
-import Translate from '~/vue_shared/translate';
 import VueAlert from '~/vue_shared/components/alert.vue';
 
-Vue.use(Translate);
-
 export default () => {
-  const el = document.getElementById('js-vue-alert');
+  const el = document.querySelector('#js-vue-alert');
 
   if (!el) {
     return false;
@@ -16,43 +13,11 @@ export default () => {
     components: {
       VueAlert,
     },
-    data() {
-      const {
-        dataset: {
-          message,
-          variant,
-          title,
-          dismissible,
-          primaryButtonLink,
-          primaryButtonText,
-          secondaryButtonLink,
-          secondaryButtonText,
-        },
-      } = this.$options.el;
-
-      return {
-        message,
-        variant,
-        title,
-        dismissible,
-        primaryButtonLink,
-        primaryButtonText,
-        secondaryButtonLink,
-        secondaryButtonText,
-      };
-    },
-
     render(createElement) {
       return createElement('vue-alert', {
         props: {
-          message: this.message,
-          variant: this.variant,
-          title: this.title,
-          dismissible: this.dismissible,
-          primaryButtonLink: this.primaryButtonLink,
-          primaryButtonText: this.primaryButtonText,
-          secondaryButtonLink: this.secondaryButtonLink,
-          secondaryButtonText: this.secondaryButtonText,
+          ...el.dataset,
+          dismissable: el.dataset.dismissable !== 'false',
         },
       });
     },
--- app/assets/javascripts/vue_shared/components/alert.vue
+++ app/assets/javascripts/vue_shared/components/alert.vue
@@ -7,44 +7,13 @@ export default {
     GlAlert,
   },
   props: {
+    ...GlAlert.props,
+
+    // this prop must be explicitly added as the GlAlert's
+    // "message" is a slot (and not an actual Vue prop)
     message: {
       type: String,
       required: true,
-    },
-    variant: {
-      type: String,
-      required: false,
-      default: 'danger',
-    },
-    title: {
-      type: String,
-      required: false,
-      default: '',
-    },
-    dismissible: {
-      type: Boolean,
-      required: false,
-      default: true,
-    },
-    primaryButtonLink: {
-      type: String,
-      required: false,
-      default: '',
-    },
-    primaryButtonText: {
-      type: String,
-      required: false,
-      default: '',
-    },
-    secondaryButtonLink: {
-      type: String,
-      required: false,
-      default: '',
-    },
-    secondaryButtonText: {
-      type: String,
-      required: false,
-      default: '',
     },
   },
   data() {
@@ -56,16 +25,7 @@ export default {
 </script>
 
 <template>
-  <gl-alert
-    v-if="showAlert"
-    :title="title"
-    :variant="variant"
-    :dismissible="dismissible"
-    :primary-button-link="primaryButtonLink"
-    :primary-button-text="primaryButtonText"
-    :secondary-button-link="secondaryButtonLink"
-    :secondary-button-text="secondaryButtonText"
-    @dismiss="showAlert = false"
-    >{{ message }}</gl-alert
-  >
+  <gl-alert v-if="showAlert" v-bind="$props" @dismiss="showAlert = false">
+    {{ message }}
+  </gl-alert>
 </template>
