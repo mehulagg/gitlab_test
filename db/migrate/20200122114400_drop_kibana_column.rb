7668a264bb6c303f71d5e4907959bf109c55efee
Adrien Kohlbecker <adrien.kohlbecker@gmail.com>
2020-01-22 12:50:50 +0100

    Drop kibana_hostname column

---
+++ db/migrate/20200122114400_drop_kibana_column.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# See http://doc.gitlab.com/ce/development/migration_style_guide.html
+# for more information on how to write migrations for GitLab.
+
+class DropKibanaColumn < ActiveRecord::Migration[5.2]
+  include Gitlab::Database::MigrationHelpers
+
+  # Set this constant to true if this migration requires downtime.
+  DOWNTIME = false
+
+  def change
+    remove_column :clusters_applications_elastic_stacks, :kibana_hostname, :string, limit: 255
+  end
+end
