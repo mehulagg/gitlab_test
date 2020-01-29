c7a9c4880c745a57f01ee037d4bfe1b93629b162
Cameron Swords <cswords@gitlab.com>
2020-01-29 15:34:11 +1100

    Update security reports worker configuration

--- ee/app/workers/store_security_reports_worker.rb
+++ ee/app/workers/store_security_reports_worker.rb
@@ -6,6 +6,9 @@ class StoreSecurityReportsWorker
   include ApplicationWorker
   include PipelineQueue
 
+  feature_category :static_application_security_testing
+  latency_sensitive_worker!
+
   def perform(pipeline_id)
     Ci::Pipeline.find(pipeline_id).try do |pipeline|
       break unless pipeline.project.can_store_security_reports?
