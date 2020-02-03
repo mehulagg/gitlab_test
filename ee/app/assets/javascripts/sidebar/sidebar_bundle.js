b572de1eb56f44ea61e3f8fdd9af1e075ed4a4bb
sstern <sstern@gitlab.com>
2020-02-03 15:59:45 -0800

    Get dropdowns to update during real time

--- ee/app/assets/javascripts/sidebar/sidebar_bundle.js
+++ ee/app/assets/javascripts/sidebar/sidebar_bundle.js
@@ -1,10 +1,13 @@
 import { getSidebarOptions } from '~/sidebar/mount_sidebar';
 import Mediator from './sidebar_mediator';
 import mountSidebar from './mount_sidebar';
+import mountTest from '~/sidebar/components/assignees/mount_user_select_hidden_input';
 
 export default () => {
   const mediator = new Mediator(getSidebarOptions());
   mediator.fetch();
 
+  // i dont like needing this in 2 places ee/ce
+  mountTest();
   mountSidebar(mediator);
 };
