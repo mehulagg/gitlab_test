124741a038862b478d4b0c95c273da2c286d41ba
Tetiana Chupryna <tchupryna@gitlab.com>
2020-02-10 20:47:42 +0200

    Add update mutation

--- app/graphql/mutations/projects/base.rb
+++ app/graphql/mutations/projects/base.rb
@@ -3,6 +3,16 @@
 module Mutations
   module Projects
     class Base < BaseMutation
+      include Mutations::ResolvesProject
+
+      argument :full_path, GraphQL::ID_TYPE,
+               required: true,
+               description: 'The project path'
+
+      argument :description, GraphQL::STRING_TYPE,
+               required: true,
+               description: 'New desc'
+
       field :project,
             Types::ProjectType,
             null: false,
@@ -10,6 +20,9 @@ module Mutations
 
       private
 
+      def find_object(full_path:)
+        resolve_project(full_path: full_path)
+      end
     end
   end
 end
--- app/graphql/mutations/projects/update_description.rb
+++ app/graphql/mutations/projects/update_description.rb
@@ -1,11 +1,30 @@
 # frozen_string_literal: true
 
+# mutation {
+#   projectsUpdateDecription(input: {fullPath: "root/tanuki", description: "hehehe"}) {
+#     project {
+#       description
+#     } errors
+#   }
+# }
+
 module Mutations
   module Projects
     class UpdateDescription < Base
       graphql_name 'ProjectsUpdateDecription'
 
-      argument :full_path
+      authorize :read_project
+
+      def resolve(full_path:, description:)
+        project = authorized_find!(full_path: full_path)
+
+        project.update(description: description)
+
+        {
+          project: project,
+          errors: project.errors.full_messages
+        }
+      end
     end
   end
 end
