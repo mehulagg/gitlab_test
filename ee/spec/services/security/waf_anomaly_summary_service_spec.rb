f15bfaa5597633134a731c83bd7a84fd986e1553
Lucas Charles <me@lucascharles.me>
2020-02-10 17:19:31 -0800

    Add WAF anomaly aggregation queries
    Adds Elasticsearch query aggregating WAF anomalies by querying against
    deployment environment name and time window.
    
    See https://gitlab.com/gitlab-org/gitlab/issues/14707 for additional
    details

--- ee/spec/services/security/waf_anomaly_summary_service_spec.rb
+++ ee/spec/services/security/waf_anomaly_summary_service_spec.rb
@@ -25,6 +25,38 @@ describe Security::WafAnomalySummaryService do
     }
   end
 
+  let(:nginx_response) do
+    empty_response.deep_merge(
+      "hits" => { "total" => 3 },
+      "aggregations" => {
+        "counts" => {
+          "buckets" => [
+            { "key_as_string" => "2019-12-04T23:00:00.000Z", "key" => 1575500400000, "doc_count" => 1 },
+            { "key_as_string" => "2019-12-05T00:00:00.000Z", "key" => 1575504000000, "doc_count" => 0 },
+            { "key_as_string" => "2019-12-05T01:00:00.000Z", "key" => 1575507600000, "doc_count" => 0 },
+            { "key_as_string" => "2019-12-05T08:00:00.000Z", "key" => 1575532800000, "doc_count" => 2 }
+          ]
+        }
+      }
+    )
+  end
+
+  let(:modsec_response) do
+    empty_response.deep_merge(
+      "hits" => { "total" => 1 },
+      "aggregations" => {
+        "counts" => {
+          "buckets" => [
+            { "key_as_string" => "2019-12-04T23:00:00.000Z", "key" => 1575500400000, "doc_count" => 0 },
+            { "key_as_string" => "2019-12-05T00:00:00.000Z", "key" => 1575504000000, "doc_count" => 0 },
+            { "key_as_string" => "2019-12-05T01:00:00.000Z", "key" => 1575507600000, "doc_count" => 0 },
+            { "key_as_string" => "2019-12-05T08:00:00.000Z", "key" => 1575532800000, "doc_count" => 1 }
+          ]
+        }
+      }
+    )
+  end
+
   subject { described_class.new(environment: environment) }
 
   describe '#execute' do
@@ -49,13 +81,27 @@ describe Security::WafAnomalySummaryService do
         let(:nginx_results) { empty_response }
         let(:modsec_results) { empty_response }
 
-        it 'returns results' do
+        it 'returns results', :aggregate_failures do
           results = subject.execute
 
           expect(results.fetch(:status)).to eq :success
           expect(results.fetch(:interval)).to eq 'day'
           expect(results.fetch(:total_traffic)).to eq 0
           expect(results.fetch(:anomalous_traffic)).to eq 0.0
+        end
+      end
+
+      context 'with violations' do
+        let(:nginx_results) { nginx_response }
+        let(:modsec_results) { modsec_response }
+
+        it 'returns results', :aggregate_failures do
+          results = subject.execute
+
+          expect(results.fetch(:status)).to eq :success
+          expect(results.fetch(:interval)).to eq 'day'
+          expect(results.fetch(:total_traffic)).to eq 3
+          expect(results.fetch(:anomalous_traffic)).to eq 0.33
         end
       end
     end
