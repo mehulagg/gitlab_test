a28c277d483d7ae1526ad0b66e7f89ff127d09d8
ntepluhina <ntepluhina@gitlab.com>
2020-02-14 15:49:13 +0200

    Added type test

--- ee/spec/graphql/types/design_management/design_collection_type_spec.rb
+++ ee/spec/graphql/types/design_management/design_collection_type_spec.rb
@@ -10,4 +10,102 @@ describe GitlabSchema.types['DesignCollection'] do
 
     expect(described_class).to have_graphql_fields(*expected_fields)
   end
+
+  describe 'pagination and totalCount' do
+    let(:user) { create(:user) }
+    let(:namespace) { create(:namespace, owner: user) }
+    let(:project) { create(:project, :public, namespace: namespace) }
+    let!(:issue) { create(:issue, project: project) }
+    let!(:designs) { create_list(:design, 10, :with_versions, versions_count: 5, issue: issue) }
+    let(:query) do
+      <<~GRAPHQL
+        query project($fullPath: ID!, $iid: String!, $atVersion: ID, $first: Int, $after: String) {
+          project(fullPath: $fullPath) {
+            id
+            issue(iid: $iid) {
+              iid
+              designCollection {
+                designs(atVersion: $atVersion, first: $first, after: $after) {
+                  totalCount
+                  edges {
+                    node {
+                      id
+                    }
+                  }
+                  pageInfo {
+                    endCursor
+                    hasNextPage
+                  }
+                }
+              }
+            }
+          }
+        }
+      GRAPHQL
+    end
+
+    before do
+      allow(Ability).to receive(:allowed?).and_return(true)
+    end
+
+    let(:page_size) { 3 }
+
+    subject do
+      GitlabSchema.execute(
+        query,
+        context: { current_user: user },
+        variables: {
+          fullPath: project.full_path,
+          iid: issue.iid.to_s,
+          first: page_size
+        }
+      ).to_h
+    end
+
+    context 'totalCount' do
+      it "returns total count" do
+        path = %w(data project issue designCollection designs totalCount)
+        expect(subject.dig(*path)).to eq(designs.count)
+      end
+
+      it "total count does not change between pages" do
+        old_count = subject.dig(*%w(data project issue designCollection designs totalCount))
+        new_cursor = subject.dig(*%w(data project issue designCollection designs pageInfo endCursor))
+        new_page = GitlabSchema.execute(
+          query,
+          context: { current_user: user },
+          variables: {
+            fullPath: project.full_path,
+            iid: issue.iid.to_s,
+            first: page_size,
+            after: new_cursor
+          }
+        ).to_h
+        new_count = new_page.dig(*%w(data project issue designCollection designs totalCount))
+        expect(old_count).to eq(new_count)
+      end
+
+      context 'pagination' do
+        let(:page_size) { 9 }
+
+        it 'returns new ids during pagination' do
+          old_edges = subject.dig(*%w(data project issue designCollection designs edges))
+          new_cursor = subject.dig(*%w(data project issue designCollection designs pageInfo endCursor))
+          new_page = GitlabSchema.execute(
+            query,
+            context: { current_user: user },
+            variables: {
+              fullPath: project.full_path,
+              iid: issue.iid.to_s,
+              first: page_size,
+              after: new_cursor
+            }
+          ).to_h
+          new_edges = new_page.dig(*%w(data project issue designCollection designs edges))
+          expect(old_edges.count).to eq(9)
+          expect(new_edges.count).to eq(1)
+        end
+      end
+    end
+  end
 end
