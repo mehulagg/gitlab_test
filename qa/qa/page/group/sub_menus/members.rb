c8c3e87a1e3c8570d63157ae40fc2365d6f3e36f
ADD-USERNAME-HERE <ADD-EMAIL-HERE>
2020-02-19 10:11:04 +0100

    Commit on initial modification in order to update group access permissions

--- qa/qa/page/group/sub_menus/members.rb
+++ qa/qa/page/group/sub_menus/members.rb
@@ -6,6 +6,7 @@ module QA
       module SubMenus
         class Members < Page::Base
           include Page::Component::UsersSelect
+          include Page::Component::Select2
 
           view 'app/views/shared/members/_invite_member.html.haml' do
             element :member_select_field
@@ -17,6 +18,17 @@ module QA
             element :access_level_dropdown
             element :delete_member_button
             element :developer_access_level_link, 'qa_selector: "#{role.downcase}_access_level_link"' # rubocop:disable QA/ElementWithPattern, Lint/InterpolationCheck
+          end
+
+          #? for what is this needed ?
+          view 'app/views/shared/members/_invite_group.html.haml' do
+            element :group_select_field
+            element :invite_group_button
+          end
+
+          view 'app/views/shared/members/_group.html.haml' do
+            element :group_row
+            element :delete_group_access_link
           end
 
           def add_member(username)
@@ -35,6 +47,33 @@ module QA
             page.accept_confirm do
               within_element(:member_row, text: username) do
                 click_element :delete_member_button
+              end
+            end
+          end
+
+          # TODO: ask the QA team about this method, there is no selector in the haml file which I believe makes this method not possible for now. A change is probably required
+          def update_group_access_level(username, access_level)
+            within_element(:group_row, text: username) do
+              click_element :access_level_dropdown
+              click_element "#{access_level.downcase}_access_level_link"
+            end
+          end
+
+          def invite_group(group_name)
+            click_element :invite_group_tab
+            select_group(group_name)
+            click_element :invite_group_button
+          end
+
+          def select_group(group_name)
+            click_element :group_select_field
+            search_and_select(group_name)
+          end
+
+          def remove_group(username)
+            page.accept_confirm do
+              within_element(:group_row, text: username) do
+                click_element :delete_group_access_link
               end
             end
           end
