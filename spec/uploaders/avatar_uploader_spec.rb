ec6358f988f2a7a9e38303829c635ce6bf078376
Alex Pooley <apooley@gitlab.com>
2020-02-19 13:47:13 +0800

    Replace file type check with content whitelist
    File type check results in user inconvenience so we moved to a content
    whitelist check to complement existing extension checks.
    
    Created a ContentTypeWhitelist to bring forward the whitelist feature
    available in Carrierwave 2 but not in the current 1.3.1 version.
    
    Applied the ContentTypeWhitelist to the GitlabUploader base class.

--- spec/uploaders/avatar_uploader_spec.rb
+++ spec/uploaders/avatar_uploader_spec.rb
@@ -47,15 +47,32 @@ describe AvatarUploader do
     end
   end
 
-  context 'upload type check' do
-    AvatarUploader::SAFE_IMAGE_EXT.each do |ext|
-      context "#{ext} extension" do
-        it_behaves_like 'type checked uploads', filenames: "image.#{ext}"
-      end
-    end
+  context 'accept whitelist file content type' do
+    # We need to feed through a valid path, but we force the parsed mime type
+    # in a stub below so we can set any path.
+    let(:path) { File.join('spec', 'fixtures', 'video_sample.mp4') }
 
-    context 'skip image/svg+xml integrity check' do
-      it_behaves_like 'skipped type checked uploads', filenames: 'image.svg'
+    where(:mime_type) { described_class::MIME_WHITELIST }
+
+    with_them do
+      before do
+        allow_any_instance_of(described_class).to receive(:extension_whitelist).and_return(nil)
+        allow_any_instance_of(MimeMagic).to receive(:type).and_return(mime_type)
+      end
+
+      it_behaves_like 'accepted carrierwave upload'
     end
+  end
+
+  context 'upload non-whitelisted file content type' do
+    let(:path) { File.join('spec', 'fixtures', 'sanitized.svg') }
+
+    it_behaves_like 'denied carrierwave upload'
+  end
+
+  context 'upload misnamed non-whitelisted file content type' do
+    let(:path) { File.join('spec', 'fixtures', 'not_a_png.png') }
+
+    it_behaves_like 'denied carrierwave upload'
   end
 end
--- spec/uploaders/favicon_uploader_spec.rb
+++ spec/uploaders/favicon_uploader_spec.rb
@@ -6,19 +6,32 @@ describe FaviconUploader do
   let_it_be(:model) { build_stubbed(:user) }
   let_it_be(:uploader) { described_class.new(model, :favicon) }
 
-  context 'upload type check' do
-    FaviconUploader::EXTENSION_WHITELIST.each do |ext|
-      context "#{ext} extension" do
-        it_behaves_like 'type checked uploads', filenames: "image.#{ext}"
+  context 'accept whitelist file content type' do
+    # We need to feed through a valid path, but we force the parsed mime type
+    # in a stub below so we can set any path.
+    let(:path) { File.join('spec', 'fixtures', 'video_sample.mp4') }
+
+    where(:mime_type) { described_class::MIME_WHITELIST }
+
+    with_them do
+      before do
+        allow_any_instance_of(described_class).to receive(:extension_whitelist).and_return(nil)
+        allow_any_instance_of(MimeMagic).to receive(:type).and_return(mime_type)
       end
+
+      it_behaves_like 'accepted carrierwave upload'
     end
   end
 
-  context 'upload non-whitelisted file extensions' do
-    it 'will deny upload' do
-      path = File.join('spec', 'fixtures', 'banana_sample.gif')
-      fixture_file = fixture_file_upload(path)
-      expect { uploader.cache!(fixture_file) }.to raise_exception(CarrierWave::IntegrityError)
-    end
+  context 'upload non-whitelisted file content type' do
+    let(:path) { File.join('spec', 'fixtures', 'sanitized.svg') }
+
+    it_behaves_like 'denied carrierwave upload'
+  end
+
+  context 'upload misnamed non-whitelisted file content type' do
+    let(:path) { File.join('spec', 'fixtures', 'not_a_png.png') }
+
+    it_behaves_like 'denied carrierwave upload'
   end
 end
