e8ea9401e88997306dc61f0191ac9568a126e395
Peter Leitzen <pleitzen@gitlab.com>
2020-02-21 23:14:24 +0100

    Ignore overlapping CSS selectors errors

---
+++ spec/support/capybara_node_element_fix.rb
@@ -0,0 +1,10 @@
+module CapybaraNodeElementFix
+  # Override `click` to prevent overlapping CSS selectors:
+  #
+  # See https://github.com/machinio/cuprite/blob/426e4f5/lib/capybara/cuprite/errors.rb#L41
+  def click
+    trigger('click')
+  end
+end
+
+Capybara::Node::Element.prepend(CapybaraNodeElementFix)
