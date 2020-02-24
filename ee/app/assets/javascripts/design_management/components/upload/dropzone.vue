d95a851aecbac5d260903cd12bdfe5312724dab4
Tom Quirk <tquirk@gitlab.com>
2020-02-24 15:52:47 +0930

    i18n for dropzone component

--- ee/app/assets/javascripts/design_management/components/upload/dropzone.vue
+++ ee/app/assets/javascripts/design_management/components/upload/dropzone.vue
@@ -1,6 +1,8 @@
 <script>
+import createFlash from '~/flash';
 import uploadDesignMutation from '../../graphql/mutations/uploadDesign.mutation.graphql';
-import { GlIcon, GlLink } from '@gitlab/ui';
+import { UPLOAD_DESIGN_INVALID_FILETYPE_ERROR } from '../../utils/error_messages';
+import { GlIcon, GlLink, GlSprintf } from '@gitlab/ui';
 
 // WARNING: replace this with something
 // more sensical as per https://gitlab.com/gitlab-org/gitlab/issues/118611
@@ -9,10 +11,14 @@ const VALID_FILE_MIMETYPE = {
   regex: /image\/.+/,
 };
 
+// https://developer.mozilla.org/en-US/docs/Web/API/DataTransfer/types
+const VALID_DATA_TRANSFER_TYPE = 'Files';
+
 export default {
   components: {
     GlIcon,
     GlLink,
+    GlSprintf,
   },
   props: {
     maxFiles: {
@@ -29,32 +35,40 @@ export default {
   },
   computed: {
     dropzoneStyle() {
-      if (this.dragging) {
-        return {
-          background: 'rbga(255, 255, 255, 0.5)',
-        };
-      }
+      return this.dragging
+        ? {
+            background: 'rbga(255, 255, 255, 0.5)',
+          }
+        : {};
     },
   },
   methods: {
     validFileTypes(files) {
-      return !files.some(({ type }) => type.match(VALID_FILE_MIMETYPE.regex).length !== 1);
+      return !files.some(({ type }) => (type.match(VALID_FILE_MIMETYPE.regex) || []).length === 0);
     },
     validDragDataType(e) {
-      return !e.dataTransfer.types.some(t => t !== 'Files');
+      return !e.dataTransfer.types.some(t => t !== VALID_DATA_TRANSFER_TYPE);
     },
     ondrop(e) {
       this.dragging = false;
-
       const files = Array.from(e.dataTransfer.files);
 
-      if (files.length > this.maxFiles) {
-        console.error('too many files');
+      if (!files) {
         return;
       }
 
-      if (!this.isDragDataValid || !this.validFileTypes(files)) {
-        console.error('invalid drag data');
+      // TODO(tom) get clarification of UX
+      if (files.length > this.maxFiles) {
+        return;
+      }
+
+      // Do not createFlash as the user already has feedback when dropzone is active
+      if (!this.isDragDataValid) {
+        return;
+      }
+
+      if (!this.validFileTypes(files)) {
+        createFlash(UPLOAD_DESIGN_INVALID_FILETYPE_ERROR);
         return;
       }
 
@@ -93,13 +107,25 @@ export default {
     <div class="w-100 h-100 position-relative">
       <slot v-bind="{ dragging, isDragDataValid }">
         <div class="border-design-dropzone w-100 h-100 d-flex-center rounded-sm">
-          <div class="d-flex-center flex-column">
+          <div class="d-flex-center flex-column text-center">
             <gl-icon name="doc-new" :size="48" class="mb-4" />
-            <span>Drag and drop to upload your designs</span>
-            <span
-              >or
-              <gl-link class="h-100 w-100" @click="openFileUpload">click to upload</gl-link>.</span
-            >
+            <p>
+              <gl-sprintf
+                :message="
+                  __(
+                    '%{lineOneStart}Drag and drop to upload your designs%{lineOneEnd} or %{linkStart}click to upload%{linkEnd}.',
+                  )
+                "
+              >
+                <template #lineOne="{ content }"
+                  ><span class="d-block">{{ content }}</span>
+                </template>
+
+                <template #link="{ content }">
+                  <gl-link class="h-100 w-100" @click="openFileUpload">{{ content }}</gl-link>
+                </template>
+              </gl-sprintf>
+            </p>
           </div>
         </div>
         <input
@@ -112,18 +138,16 @@ export default {
         />
       </slot>
       <div
-        class="design-dropzone--overlay border-design-dropzone w-100 h-100 position-absolute d-flex-center"
         v-show="dragging"
+        class="design-dropzone--overlay border-design-dropzone w-100 h-100 position-absolute d-flex-center"
       >
-        <div class="mw-50 text-center">
-          <template v-show="isDragDataValid">
-            <h3>Oh no!</h3>
-            <span>You can only drop image files here.</span>
-          </template>
-          <template v-show="!isDragDataValid">
-            <h3>Incoming!</h3>
-            <span>Drop your designs to start your upload.</span>
-          </template>
+        <div v-show="!isDragDataValid" class="mw-50 text-center">
+          <h3>{{ __('Oh no!') }}</h3>
+          <span>{{ __('You can only drop image files here.') }}</span>
+        </div>
+        <div v-show="isDragDataValid" class="mw-50 text-center">
+          <h3>{{ __('Incoming!') }}</h3>
+          <span>{{ __('Drop your designs to start your upload.') }}</span>
         </div>
       </div>
     </div>
