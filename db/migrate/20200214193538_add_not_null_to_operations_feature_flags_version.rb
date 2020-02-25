e883892a726b32e3b96fdefe2137157498ed9547
Jason Goodman <jgoodman@gitlab.com>
2020-02-25 12:41:35 -0500

    Remove old migrations

--- db/migrate/20200214193538_add_not_null_to_operations_feature_flags_version.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class AddNotNullToOperationsFeatureFlagsVersion < ActiveRecord::Migration[6.0]
-  DOWNTIME = false
-
-  def change
-    change_column_null :operations_feature_flags, :version, false, 1
-  end
-end
