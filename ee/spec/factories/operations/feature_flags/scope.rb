cd6c7f5658e85abe3c09ba9d6ef046550f50931f
Jason Goodman <jgoodman@gitlab.com>
2020-02-25 17:36:14 -0500

    Fix feature flag strategy and scope factories

--- ee/spec/factories/operations/feature_flags/scope.rb
+++ ee/spec/factories/operations/feature_flags/scope.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :operations_scope, class: 'Operations::Scope' do
+  factory :operations_scope, class: 'Operations::FeatureFlags::Scope' do
     association :strategy, factory: :operations_strategy
     sequence(:environment_scope) { |n| "review/patch-#{n}" }
   end
--- ee/spec/factories/operations/feature_flags/strategy.rb
+++ ee/spec/factories/operations/feature_flags/strategy.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :operations_strategy, class: 'Operations::Strategy' do
+  factory :operations_strategy, class: 'Operations::FeatureFlags::Strategy' do
     association :feature_flag, factory: :operations_feature_flag
     name { "default" }
     parameters { {} }
