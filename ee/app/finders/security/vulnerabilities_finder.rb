b605af6242bfbab47c443d98155791fa2ae3b94e
Avielle Wolfe <awolfe@gitlab.com>
2020-02-26 22:11:04 -0500

    Filter vulnerabilities by report types
    * Adds report type filtering to VulnerabilitiesFinder
    * Adds Vulnerability.with_report_types

--- ee/app/finders/security/vulnerabilities_finder.rb
+++ ee/app/finders/security/vulnerabilities_finder.rb
@@ -16,17 +16,24 @@ module Security
 
     def execute
       filter_by_severities
+      filter_by_report_types
 
       vulnerabilities
     end
 
     private
 
-    attr_reader :project, :filters, :vulnerabilities
+    attr_reader :filters, :vulnerabilities
 
     def filter_by_severities
       if filters[:severities].present?
         @vulnerabilities = vulnerabilities.with_severities(filters[:severities])
+      end
+    end
+
+    def filter_by_report_types
+      if filters[:report_types].present?
+        @vulnerabilities = vulnerabilities.with_report_types(filters[:report_types])
       end
     end
   end
--- ee/spec/finders/security/vulnerabilities_finder_spec.rb
+++ ee/spec/finders/security/vulnerabilities_finder_spec.rb
@@ -4,9 +4,9 @@ require 'spec_helper'
 
 describe Security::VulnerabilitiesFinder do
   let_it_be(:project) { create(:project) }
-  let_it_be(:low_vulnerability) { create(:vulnerability, severity: :low, project: project) }
-  let_it_be(:medium_vulnerability) { create(:vulnerability, severity: :medium, project: project) }
-  let_it_be(:high_vulnerability) { create(:vulnerability, severity: :high, project: project) }
+  let_it_be(:vulnerability1) { create(:vulnerability, severity: :low, report_type: :sast, project: project) }
+  let_it_be(:vulnerability2) { create(:vulnerability, severity: :medium, report_type: :dast, project: project) }
+  let_it_be(:vulnerability3) { create(:vulnerability, severity: :high, report_type: :dependency_scanning, project: project) }
 
   let(:filters) { {} }
 
@@ -28,15 +28,23 @@ describe Security::VulnerabilitiesFinder do
     let(:filters) { { severities: %w[medium low] } }
 
     it 'only returns vulnerabilities matching the given severities' do
-      expect(subject).to contain_exactly(low_vulnerability, medium_vulnerability)
+      is_expected.to contain_exactly(vulnerability1, vulnerability2)
     end
   end
 
   context 'when filtered by report type' do
-    it 'only returns vulnerabilities matching the given report types'
+    let(:filters) { { report_types: %w[sast dast] } }
+
+    it 'only returns vulnerabilities matching the given report types' do
+      is_expected.to contain_exactly(vulnerability1, vulnerability2)
+    end
   end
 
-  context 'when filtered by severity and report type' do
-    it 'only returns vulnerabilities matching the given severities AND report types'
+  context 'when filtered by state' do
+    it 'only returns vulnerabilities matching the given states'
+  end
+
+  context 'when filtered by more than one property' do
+    it 'only returns vulnerabilities matching all of the given filters'
   end
 end
