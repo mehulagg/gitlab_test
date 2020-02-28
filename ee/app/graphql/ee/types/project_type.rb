cc846d5df492a008a68bd54b880ee5f7fbce0d0c
Avielle Wolfe <awolfe@gitlab.com>
2020-02-27 20:05:05 -0500

    Create VulnerabilityType and supporting types
    * Creates VulnerabilityType
    * Creates VulnerabilityReportTypeEnum
    * Creates VulnerabilitySeverityEnum
    * Creates VulnerabilityStateEnum
    * Creates VulnerabilityResolver
    * Adds vulnerabilities property to ProjectType

--- ee/app/graphql/ee/types/project_type.rb
+++ ee/app/graphql/ee/types/project_type.rb
@@ -11,6 +11,12 @@ module EE
 
         field :service_desk_address, GraphQL::STRING_TYPE, null: true,
           description: 'E-mail address of the service desk.'
+
+        field :vulnerabilities,
+              ::Types::VulnerabilityType.connection_type,
+              null: true,
+              description: 'Vulnerabilities reported on the project',
+              resolver: Resolvers::VulnerabilitiesResolver
       end
     end
   end
--- ee/spec/graphql/types/project_type_spec.rb
+++ ee/spec/graphql/types/project_type_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 
 describe GitlabSchema.types['Project'] do
   it 'includes the ee specific fields' do
-    expected_fields = %w[service_desk_enabled service_desk_address]
+    expected_fields = %w[service_desk_enabled service_desk_address vulnerabilities]
 
     is_expected.to include_graphql_fields(*expected_fields)
   end
