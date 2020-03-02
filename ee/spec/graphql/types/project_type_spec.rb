c6b11f76136144778a6907bfa457999d10fcc826
Brett Walker <bwalker@gitlab.com>
2020-03-02 15:29:17 -0600

    Fix how we call include_graphql_fields
    from described_class.new to described_class

--- ee/spec/graphql/types/project_type_spec.rb
+++ ee/spec/graphql/types/project_type_spec.rb
@@ -6,6 +6,6 @@ describe GitlabSchema.types['Project'] do
   it 'includes the ee specific fields' do
     expected_fields = %w[service_desk_enabled service_desk_address]
 
-    is_expected.to include_graphql_fields(*expected_fields)
+    expect(described_class).to include_graphql_fields(*expected_fields)
   end
 end
