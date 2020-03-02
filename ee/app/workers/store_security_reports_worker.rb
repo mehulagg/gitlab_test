d8a89620aa258501fa9a5fe4cd4d4b26f316f5ce
Seth Berger <4310743-sethgitlab@users.noreply.gitlab.com>
2020-03-02 14:17:11 -0600

    Updated sidekiq attribution

--- ee/app/workers/store_security_reports_worker.rb
+++ ee/app/workers/store_security_reports_worker.rb
@@ -6,6 +6,8 @@ class StoreSecurityReportsWorker # rubocop:disable Scalability/IdempotentWorker
   include ApplicationWorker
   include SecurityScansQueue
 
+  feature_category :static_application_security_testing
+
   def perform(pipeline_id)
     Ci::Pipeline.find(pipeline_id).try do |pipeline|
       break unless pipeline.project.can_store_security_reports?
@@ -14,3 +16,4 @@ class StoreSecurityReportsWorker # rubocop:disable Scalability/IdempotentWorker
     end
   end
 end
+
