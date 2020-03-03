35d7e2c3fca7ec4b8acd58dc9e05b05cab017fd7
Avielle Wolfe <awolfe@gitlab.com>
2020-03-03 18:33:03 -0500

    Add vulnerabilities_summary feature flag spec
    :)

--- ee/spec/graphql/types/project_type_spec.rb
+++ ee/spec/graphql/types/project_type_spec.rb
@@ -3,6 +3,14 @@
 require 'spec_helper'
 
 describe GitlabSchema.types['Project'] do
+  let_it_be(:project) { create(:project) }
+  let_it_be(:user) { create(:user) }
+  let_it_be(:vulnerability) { create(:vulnerability, project: project, severity: :high) }
+
+  before do
+    project.add_developer(user)
+  end
+
   it 'includes the ee specific fields' do
     expected_fields = %w[service_desk_enabled service_desk_address vulnerabilities vulnerabilities_summary]
 
@@ -10,15 +18,10 @@ describe GitlabSchema.types['Project'] do
   end
 
   describe 'vulnerabilities' do
-    let_it_be(:project) { create(:project) }
-    let_it_be(:user) { create(:user) }
-    let_it_be(:vulnerability) { create(:vulnerability, project: project) }
-
     let_it_be(:query) do
       %(
         query {
           project(fullPath:"#{project.full_path}") {
-            name
             vulnerabilities {
               nodes {
                 title
@@ -27,10 +30,6 @@ describe GitlabSchema.types['Project'] do
           }
         }
       )
-    end
-
-    before do
-      project.add_developer(user)
     end
 
     subject { GitlabSchema.execute(query, context: { current_user: user }).as_json }
@@ -57,6 +56,47 @@ describe GitlabSchema.types['Project'] do
         vulnerabilities = subject.dig('data', 'project', 'vulnerabilities', 'nodes')
 
         expect(vulnerabilities.count).to be(1)
+      end
+    end
+  end
+
+  describe 'vulnerabilities_summary' do
+    let_it_be(:query) do
+      %(
+        query {
+          project(fullPath:"#{project.full_path}") {
+            vulnerabilitiesSummary {
+              high
+            }
+          }
+        }
+      )
+    end
+
+    subject { GitlabSchema.execute(query, context: { current_user: user }).as_json }
+
+    context 'when first_class_vulnerabilities is disabled' do
+      before do
+        stub_feature_flags(first_class_vulnerabilities: false)
+      end
+
+      it 'is null' do
+        vulnerabilities = subject.dig('data', 'project', 'vulnerabilities')
+
+        expect(vulnerabilities).to be_nil
+      end
+    end
+
+    context 'when first_class_vulnerabilities is enabled' do
+      before do
+        stub_feature_flags(first_class_vulnerabilities: true)
+        stub_licensed_features(security_dashboard: true)
+      end
+
+      it "returns the project's vulnerabilities summary" do
+        high_count = subject.dig('data', 'project', 'vulnerabilitiesSummary', 'high')
+
+        expect(high_count).to be(1)
       end
     end
   end
