66f7715483cc53e6cf9e544d0ad4381e2a96c642
Avielle Wolfe <awolfe@gitlab.com>
2020-03-03 17:43:30 -0500

    Test feature flag on vulnerabilities field
    :)

--- ee/spec/graphql/types/project_type_spec.rb
+++ ee/spec/graphql/types/project_type_spec.rb
@@ -8,4 +8,56 @@ describe GitlabSchema.types['Project'] do
 
     is_expected.to include_graphql_fields(*expected_fields)
   end
+
+  describe 'vulnerabilities' do
+    let_it_be(:project) { create(:project) }
+    let_it_be(:user) { create(:user) }
+    let_it_be(:vulnerability) { create(:vulnerability, project: project) }
+
+    let_it_be(:query) do
+      %(
+        query {
+          project(fullPath:"#{project.full_path}") {
+            name
+            vulnerabilities {
+              nodes {
+                title
+              }
+            }
+          }
+        }
+      )
+    end
+
+    before do
+      project.add_developer(user)
+    end
+
+    subject { GitlabSchema.execute(query, context: { current_user: user }).as_json }
+
+    context 'when first_class_vulnerabilities is disabled' do
+      before do
+        stub_feature_flags(first_class_vulnerabilities: false)
+      end
+
+      it 'is null' do
+        vulnerabilities = subject.dig('data', 'project', 'vulnerabilities')
+
+        expect(vulnerabilities).to be_nil
+      end
+    end
+
+    context 'when first_class_vulnerabilities is enabled' do
+      before do
+        stub_feature_flags(first_class_vulnerabilities: true)
+        stub_licensed_features(security_dashboard: true)
+      end
+
+      it "returns the project's vulnerabilities" do
+        vulnerabilities = subject.dig('data', 'project', 'vulnerabilities', 'nodes')
+
+        expect(vulnerabilities.count).to be(1)
+      end
+    end
+  end
 end
