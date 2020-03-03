721bd17bf7ba2ac833fa06790ecce958889831b1
Avielle Wolfe <awolfe@gitlab.com>
2020-03-03 16:49:32 -0500

    Refactor the VulnerabilitiesResolver
    Hopefully it is now more clear what type of `object` is the parent.

--- ee/app/graphql/resolvers/vulnerabilities_resolver.rb
+++ ee/app/graphql/resolvers/vulnerabilities_resolver.rb
@@ -2,17 +2,27 @@
 
 module Resolvers
   class VulnerabilitiesResolver < BaseResolver
+    include Gitlab::Utils::StrongMemoize
+
     type Types::VulnerabilityType, null: true
 
     def resolve(**args)
-      # The project or group could have been loaded in batch by `BatchLoader`.
-      # At this point we need the `id` of the project/group to query for vulnerabilities, so
+      return Vulnerability.none unless vulnerable
+
+      vulnerable.vulnerabilities.with_findings.ordered
+    end
+
+    private
+
+    # `vulnerable` will be a Project, Group, or InstanceSecurityDashboard
+    def vulnerable
+      # A project or group could have been loaded in batch by `BatchLoader`.
+      # At this point we need the `id` of the project or group to query for vulnerabilities, so
       # make sure it's loaded and not `nil` before continuing.
-      parent = object.respond_to?(:sync) ? object.sync : object
 
-      return Vulnerability.none unless parent
-
-      parent.vulnerabilities.with_findings.ordered
+      strong_memoize(:vuln) do
+        object.respond_to?(:sync) ? object.sync : object
+      end
     end
   end
 end
