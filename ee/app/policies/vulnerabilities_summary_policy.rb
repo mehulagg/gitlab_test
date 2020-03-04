a014a50b5bfe9aa4be7a2fa95507204371df3866
Avielle Wolfe <awolfe@gitlab.com>
2020-03-03 19:31:00 -0500

    Add VulnerabilitiesSummaryPolicy specs
    They are essentially duplicates of the VulnerabilityPolicy specs.

--- ee/app/policies/vulnerabilities_summary_policy.rb
+++ ee/app/policies/vulnerabilities_summary_policy.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+# VulnerabilitiesSummary uses the same permissions as Vulnerability,
+# the :read_vulnerability ability defined in ProjectPolicy
+
 class VulnerabilitiesSummaryPolicy < BasePolicy
   delegate { @subject.vulnerable }
 end
