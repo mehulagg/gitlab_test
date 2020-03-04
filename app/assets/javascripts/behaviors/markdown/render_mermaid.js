d234bb5c8ab73e4ad63600a6e1e7482b68a7bcc7
Rajat Jain <rjain@gitlab.com>
2020-03-04 08:53:11 +0530

    Add functionality to render individual mermaids
    With 5000 chars as a hard limit, there are many cases where we'd want
    to render individual mermaid diagrams. With this we're adding a render
    button which will render the nearest mermaid svg.

--- app/assets/javascripts/behaviors/markdown/render_mermaid.js
+++ app/assets/javascripts/behaviors/markdown/render_mermaid.js
@@ -1,6 +1,7 @@
 import flash from '~/flash';
 import $ from 'jquery';
 import { sprintf, __ } from '../../locale';
+import { once } from 'lodash';
 
 // Renders diagrams and flowcharts from text using Mermaid in any element with the
 // `js-render-mermaid` class.
@@ -17,15 +18,11 @@ import { sprintf, __ } from '../../locale';
 //
 
 // This is an arbitrary number; Can be iterated upon when suitable.
-const MAX_CHAR_LIMIT = 5000;
+const MAX_CHAR_LIMIT = 50;
+let mermaidModule = {};
 
-function renderMermaids($els) {
-  if (!$els.length) return;
-
-  // A diagram may have been truncated in search results which will cause errors, so abort the render.
-  if (document.querySelector('body').dataset.page === 'search:show') return;
-
-  import(/* webpackChunkName: 'mermaid' */ 'mermaid')
+function importMermaidModule() {
+  return import(/* webpackChunkName: 'mermaid' */ 'mermaid')
     .then(mermaid => {
       mermaid.initialize({
         // mermaid core options
@@ -40,62 +37,121 @@ function renderMermaids($els) {
         securityLevel: 'strict',
       });
 
+      mermaidModule = mermaid;
+
+      return mermaid;
+    })
+    .catch(err => {
+      flash(`Can't load mermaid module: ${err}`);
+    });
+}
+
+function fixElementSource(el) {
+  // Mermaid doesn't like `<br />` tags, so collapse all like tags into `<br>`, which is parsed correctly.
+  const source = el.textContent.replace(/<br\s*\/>/g, '<br>');
+
+  // Remove any extra spans added by the backend syntax highlighting.
+  Object.assign(el, { textContent: source });
+
+  return { source };
+}
+
+function renderMermaidEl(el) {
+  mermaidModule.init(undefined, el, id => {
+    const source = el.textContent;
+    const svg = document.getElementById(id);
+
+    // As of https://github.com/knsv/mermaid/commit/57b780a0d,
+    // Mermaid will make two init callbacks:one to initialize the
+    // flow charts, and another to initialize the Gannt charts.
+    // Guard against an error caused by double initialization.
+    if (svg.classList.contains('mermaid')) {
+      return;
+    }
+
+    svg.classList.add('mermaid');
+
+    // pre > code > svg
+    svg.closest('pre').replaceWith(svg);
+
+    // We need to add the original source into the DOM to allow Copy-as-GFM
+    // to access it.
+    const sourceEl = document.createElement('text');
+    sourceEl.classList.add('source');
+    sourceEl.setAttribute('display', 'none');
+    sourceEl.textContent = source;
+
+    svg.appendChild(sourceEl);
+  });
+}
+
+function renderMermaids($els) {
+  if (!$els.length) return;
+
+  // A diagram may have been truncated in search results which will cause errors, so abort the render.
+  if (document.querySelector('body').dataset.page === 'search:show') return;
+
+  importMermaidModule()
+    .then(() => {
       let renderedChars = 0;
 
       $els.each((i, el) => {
-        // Mermaid doesn't like `<br />` tags, so collapse all like tags into `<br>`, which is parsed correctly.
-        const source = el.textContent.replace(/<br\s*\/>/g, '<br>');
-
+        const { source } = fixElementSource(el);
         /**
          * Restrict the rendering to a certain amount of character to
          * prevent mermaidjs from hanging up the entire thread and
          * causing a DoS.
          */
         if ((source && source.length > MAX_CHAR_LIMIT) || renderedChars > MAX_CHAR_LIMIT) {
-          el.textContent = sprintf(
+          const errorText = sprintf(
             __(
               'Cannot render the image. Maximum character count (%{charLimit}) has been exceeded.',
             ),
             { charLimit: MAX_CHAR_LIMIT },
           );
+
+          const html = `
+              <div class="js-lazy-render-mermaid-container d-flex-center">
+                <span class="margin-right-small mr-2">${errorText}</span>
+                <button class="btn btn-link js-lazy-render-mermaid">Render</button>
+              </div>
+          `;
+
+          const $parent = $(el).parent();
+
+          if (!$parent.find('.js-lazy-render-mermaid-container').length) {
+            $parent.find('.js-render-mermaid').hide();
+            $parent.append(html);
+          }
+
           return;
         }
 
         renderedChars += source.length;
-        // Remove any extra spans added by the backend syntax highlighting.
-        Object.assign(el, { textContent: source });
 
-        mermaid.init(undefined, el, id => {
-          const svg = document.getElementById(id);
-
-          // As of https://github.com/knsv/mermaid/commit/57b780a0d,
-          // Mermaid will make two init callbacks:one to initialize the
-          // flow charts, and another to initialize the Gannt charts.
-          // Guard against an error caused by double initialization.
-          if (svg.classList.contains('mermaid')) {
-            return;
-          }
-
-          svg.classList.add('mermaid');
-
-          // pre > code > svg
-          svg.closest('pre').replaceWith(svg);
-
-          // We need to add the original source into the DOM to allow Copy-as-GFM
-          // to access it.
-          const sourceEl = document.createElement('text');
-          sourceEl.classList.add('source');
-          sourceEl.setAttribute('display', 'none');
-          sourceEl.textContent = source;
-
-          svg.appendChild(sourceEl);
-        });
+        renderMermaidEl(el);
       });
     })
     .catch(err => {
-      flash(`Can't load mermaid module: ${err}`);
+      flash(`Encountered an error while rendering: ${err}`);
     });
 }
+
+const hookLazyRenderMermaidEvent = once(() => {
+  $(document.body).on('click', '.js-lazy-render-mermaid', function eventHandler() {
+    const el = $(this)
+      .closest('.mermaid')
+      .find('.js-render-mermaid');
+
+    $(el).show();
+
+    $(this)
+      .parent()
+      .remove();
+
+    renderMermaidEl(el);
+  });
+});
 
 export default function renderMermaid($els) {
   if (!$els.length) return;
@@ -111,4 +167,6 @@ export default function renderMermaid($els) {
       renderMermaids($(this).find('.js-render-mermaid'));
     }
   });
+
+  hookLazyRenderMermaidEvent();
 }
