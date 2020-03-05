b9a5fb992266fbf298981914b74522319e372e26
Sean Carroll <scarroll@gitlab.com>
2020-03-05 18:22:46 +0100

    Support multiple Evidences for a Release
    Part of https://gitlab.com/gitlab-org/gitlab/issues/199065
    
    See merge request https://gitlab.com/gitlab-org/gitlab/-/merge_requests/26509

--- spec/lib/api/entities/release_spec.rb
+++ spec/lib/api/entities/release_spec.rb
@@ -5,24 +5,29 @@ require 'spec_helper'
 describe API::Entities::Release do
   let_it_be(:project) { create(:project) }
   let_it_be(:release) { create(:release, :with_evidence, project: project) }
+  let(:evidence) { release.evidences.first }
   let(:user) { create(:user) }
   let(:entity) { described_class.new(release, current_user: user) }
+  let(:subject_evidence) { subject[:evidences].first }
 
   subject { entity.as_json }
 
-  describe 'evidence' do
+  describe 'evidences' do
     context 'when the current user can download code' do
       it 'exposes the evidence sha and the json path' do
         allow(Ability).to receive(:allowed?).and_call_original
         allow(Ability).to receive(:allowed?)
           .with(user, :download_code, project).and_return(true)
 
-        expect(subject[:evidence_sha]).to eq(release.evidence_sha)
-        expect(subject[:assets][:evidence_file_path]).to eq(
-          Gitlab::Routing.url_helpers.evidence_project_release_url(project,
-                                                                   release.tag,
-                                                                   format: :json)
-        )
+        expect(subject_evidence[:sha]).to eq(evidence.summary_sha)
+        expect(subject_evidence[:collected_at]).to eq(evidence.collected_at)
+        expect(subject_evidence[:filepath]).to eq(
+          Gitlab::Routing.url_helpers.namespace_project_release_evidence_url(
+            namespace_id: project.namespace_id,
+            project_id: project,
+            release_tag: release,
+            id: evidence.id,
+            format: :json))
       end
     end
 
@@ -32,8 +37,7 @@ describe API::Entities::Release do
         allow(Ability).to receive(:allowed?)
           .with(user, :download_code, project).and_return(false)
 
-        expect(subject.keys).not_to include(:evidence_sha)
-        expect(subject[:assets].keys).not_to include(:evidence_file_path)
+        expect(subject.keys).not_to include(:evidences)
       end
     end
   end
