8b900ac4f8cff425228dc197949480bb03aa9154
Martin Wortschack <mwortschack@gitlab.com>
2020-03-06 10:18:01 +0100

    Add group activity analytics skeleton

---
+++ ee/app/assets/javascripts/analytics/group_analytics/components/group_activity_card.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  name: 'GroupActivityCard',
+  props: {
+    groupFullPath: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<template>
+  <div>{{ __('Group analytics activity goes here') }}</div>
+</template>
