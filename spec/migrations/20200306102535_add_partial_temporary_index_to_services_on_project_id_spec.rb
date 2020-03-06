42ab10b570730977e9e48dafadcd6a4fa1e159e0
Mikolaj Wawrzyniak <mwawrzyniak@gitlab.com>
2020-03-06 12:13:48 +0100

    Add tmp index to services on project_id

---
+++ spec/migrations/20200306102535_add_partial_temporary_index_to_services_on_project_id_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+require Rails.root.join('db', 'post_migrate', '20200306102535_add_partial_temporary_index_to_services_on_project_id_spec.rb.rb')
+
+describe AddPartialTemporaryIndexToServicesOnProjectId, :migration do
+  let(:migration) { described_class.new }
+
+  describe '#up' do
+    it 'creates temporary partial index on type' do
+      expect { migration.up }.to change { migration.index_exists?(:services, :project_id, name: described_class::INDEX_NAME) }.from(false).to(true)
+    end
+  end
+
+  describe '#down' do
+    it 'removes temporary partial index on type' do
+      migration.up
+
+      expect { migration.down }.to change { migration.index_exists?(:services, :project_id, name: described_class::INDEX_NAME) }.from(true).to(false)
+    end
+  end
+end
