ca44c1f779603b65ee3f0187d66fddf0c86f792c
Heinrich Lee Yu <heinrich@gitlab.com>
2020-03-17 17:55:23 +0800

    Switch to @rails/ujs package
    Remove jquery-ujs and use rails-ujs instead

--- app/assets/javascripts/commons/jquery.js
+++ app/assets/javascripts/commons/jquery.js
@@ -1,7 +1,6 @@
 import 'jquery';
 
 // common jQuery plugins
-import 'jquery-ujs';
 import 'jquery.caret'; // must be imported before at.js
 import '@gitlab/at.js';
 import 'vendor/jquery.scrollTo';
