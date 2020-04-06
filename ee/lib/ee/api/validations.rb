9415f7d88f1909ec18f09c7ae600e617a6bf978b
Peter Leitzen <pleitzen@gitlab.com>
2020-04-06 15:06:58 +0000

    Apply suggestion to ee/lib/ee/api/validations.rb

--- ee/lib/ee/api/validations.rb
+++ ee/lib/ee/api/validations.rb
@@ -3,7 +3,7 @@
 module EE
   module API
     module Validations
-      # This empty module needed to resolve Mac OS X flakey autoloading issues as seen in
+      # This empty module is needed to resolve Mac OS X flakey autoloading issues as seen in
       # https://gitlab.com/gitlab-org/gitlab/-/merge_requests/28804
     end
   end
