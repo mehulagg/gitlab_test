d90bca9e20be14f5fbc1abc3b65a76a865615a22
Aishwarya Subramanian <asubramanian@gitlab.com>
2020-04-07 17:57:26 -0500

    Create action

--- app/controllers/projects/settings/access_token_controller.rb
+++ app/controllers/projects/settings/access_token_controller.rb
@@ -3,8 +3,20 @@
 module Projects
   module Settings
     class AccessTokenController < Projects::ApplicationController
+      def index
+
+      end
 
       def create
+        access_token = Users::CreateAccessTokenService.new(@project, current_user, params)
+
+        if access_token
+          @token = access_token.token
+
+          format.html do
+            redirect_to project_settings_access_token_controller_path, notice: _("Your new personal access token has been created.")
+          end
+        end
       end
     end
   end
