3a61a50ddb189c924f722ca94bdc338fa41d1a7a
Maxime Orefice <morefice@gitlab.com>
2020-04-16 15:00:56 -0400

    Add skeleton for accessibility report comparer entity

---
+++ spec/serializers/accessibility_reports_comparer_entity_spec.rb
@@ -0,0 +1,66 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe AccessibilityReportsComparerEntity do
+  let(:entity) { described_class.new(comparer) }
+  let(:comparer) { Gitlab::Ci::Reports::AccessibilityReportsComparer.new(base_report, head_report) }
+  let(:base_report) { Gitlab::Ci::Reports::AccessibilityReports.new }
+  let(:head_report) { Gitlab::Ci::Reports::AccessibilityReports.new }
+
+  describe '#as_json' do
+    subject { entity.as_json }
+
+    context 'when head report has a newly failed which does not exist in base' do
+      before do
+        base_report.errors = 1
+        base_report.urls = {
+          "https://gitlab.com" =>
+            [
+              {
+                "code" => "WCAG2AA.Principle4.Guideline4_1.4_1_2.H91.A.NoContent",
+                "type" => "error",
+                "typeCode" => 1,
+                "message" => "Anchor element found with a valid href attribute, but no link content has been supplied.",
+                "context" => "<a class=\"site-title active\" rel=\"author\" href=\"/\">\n        <svg version=\"1.0\" xml...</a>",
+                "selector" => "html > body > header > div > nav > a",
+                "runner" => "htmlcs",
+                "runnerExtras" => {}
+              }
+            ]
+          }
+
+        head_report.errors = 2
+        head_report.urls = {
+          "https://gitlab.com" =>
+            [
+              {
+                "code" => "WCAG2AA.Principle4.Guideline4_1.4_1_2.H91.A.NoContent",
+                "type" => "error",
+                "typeCode" => 1,
+                "message" => "Anchor element found with a valid href attribute, but no link content has been supplied.",
+                "context" => "<a class=\"site-title active\" rel=\"author\" href=\"/\">\n        <svg version=\"1.0\" xml...</a>",
+                "selector" => "html > body > header > div > nav > a",
+                "runner" => "htmlcs",
+                "runnerExtras" => {}
+              },
+              {
+                "code" => "WCAG2AA.Principle4.Guideline4_1.4_1_2.H91.A.NoContent",
+                "type" => "error",
+                "typeCode" => 1,
+                "message" => "Anchor element found with a valid href attribute, but no link content has been supplied.",
+                "context" => "<a class=\"social-icon\" target=\"_blank\" href=\"https://gitlab.com\" rel=\"nofollow noopener noreferrer\">\n        <svg xmlns=\"http://www...</a>",
+                "selector" => "html > body > header > div > nav > a",
+                "runner" => "htmlcs",
+                "runnerExtras" => {}
+              }
+            ]
+          }
+      end
+
+      it 'contains correct compared accessibility report details' do
+        expect(subject[:added]).to eq(1)
+      end
+    end
+  end
+end
