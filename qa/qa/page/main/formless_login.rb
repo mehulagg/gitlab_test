7d20044bbf276c0b2d3ad33b46b03bff0b0f9384
Walmyr Lima e Silva Filho <walmyr@gitlab.com>
2020-04-17 12:19:34 +0000

    Apply suggestion to qa/qa/page/main/formless_login.rb

--- qa/qa/page/main/formless_login.rb
+++ qa/qa/page/main/formless_login.rb
@@ -6,9 +6,7 @@ module QA
       class FormlessLogin < Page::Base
         # This workaround avoids failure in Test::Sanity::Selectors
         # as it requires a Page class to have views / elements defined. 
-        view 'app/views/layouts/devise.html.haml' do
-          element :login_page
-        end
+        view 'app/views/layouts/devise.html.haml'
 
         def self.path(user: Runtime::User)
           "/users/qa_sign_in?user[login]=#{user.username}&user[password]=#{user.password}&gitlab_qa_formless_login_token=#{Runtime::Env.gitlab_qa_token}"
