a52170fb9a49997ae8cc0fddfbe140427b49ce54
Rubén Dávila <ruben@gitlab.com>
2020-04-17 22:26:13 -0500

    Add helper class to handle the display of banner

---
+++ ee/app/helpers/seats_monitoring_helper.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+module SeatsMonitoringHelper
+  include Gitlab::Utils::StrongMemoize
+
+  def display_overage_warning?
+    license_is_over_capacity?
+  end
+
+  private
+
+  def license_is_over_capacity?
+    return if current_license.nil? || current_license&.trial?
+
+    # TODO: pass historical_max counter corresponding to the current period to #overage
+    current_license.overage > 0
+  end
+
+  def current_license
+    strong_memoize(:current_license) { License.current }
+  end
+end
