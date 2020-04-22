d544dacb2ff2247cf71917bcfe2782a60cfd2597
Simon Knox <simon@gitlab.com>
2020-04-22 13:24:58 +1000

    Only load embed_chart JS for markdown if it's required
    Should also add createStore but unsure how to handle non-default exports

--- app/assets/javascripts/behaviors/markdown/render_metrics.js
+++ app/assets/javascripts/behaviors/markdown/render_metrics.js
@@ -1,5 +1,4 @@
 import Vue from 'vue';
-import EmbedGroup from '~/monitoring/components/embeds/embed_group.vue';
 import { createStore } from '~/monitoring/stores/embed_group/';
 
 // TODO: Handle copy-pasting - https://gitlab.com/gitlab-org/gitlab-foss/issues/64369.
@@ -8,37 +7,43 @@ export default function renderMetrics(elements) {
     return;
   }
 
-  const EmbedGroupComponent = Vue.extend(EmbedGroup);
+  import(/* webpackChunkName: 'embed_group' */ '~/monitoring/components/embeds/embed_group.vue')
+    .then((EmbedGroup) => {
+      const EmbedGroupComponent = Vue.extend(EmbedGroup);
 
-  const wrapperList = [];
+      const wrapperList = [];
 
-  elements.forEach(element => {
-    let wrapper;
-    const { previousElementSibling } = element;
-    const isFirstElementInGroup = !previousElementSibling?.urls;
+      elements.forEach(element => {
+        let wrapper;
+        const { previousElementSibling } = element;
+        const isFirstElementInGroup = !previousElementSibling?.urls;
 
-    if (isFirstElementInGroup) {
-      wrapper = document.createElement('div');
-      wrapper.urls = [element.dataset.dashboardUrl];
-      element.parentNode.insertBefore(wrapper, element);
-      wrapperList.push(wrapper);
-    } else {
-      wrapper = previousElementSibling;
-      wrapper.urls.push(element.dataset.dashboardUrl);
-    }
+        if (isFirstElementInGroup) {
+          wrapper = document.createElement('div');
+          wrapper.urls = [element.dataset.dashboardUrl];
+          element.parentNode.insertBefore(wrapper, element);
+          wrapperList.push(wrapper);
+        } else {
+          wrapper = previousElementSibling;
+          wrapper.urls.push(element.dataset.dashboardUrl);
+        }
 
-    // Clean up processed element
-    element.parentNode.removeChild(element);
-  });
+        // Clean up processed element
+        element.parentNode.removeChild(element);
+      });
 
-  wrapperList.forEach(wrapper => {
-    // eslint-disable-next-line no-new
-    new EmbedGroupComponent({
-      el: wrapper,
-      store: createStore(),
-      propsData: {
-        urls: wrapper.urls,
-      },
+      wrapperList.forEach(wrapper => {
+        // eslint-disable-next-line no-new
+        new EmbedGroupComponent({
+          el: wrapper,
+          store: createStore(),
+          propsData: {
+            urls: wrapper.urls,
+          },
+        });
+      });
+    })
+    .catch((e) => {
+      throw e;
     });
-  });
 }
