362469becc75edde1988937d643f6743c219b7cf
Alex Kalderimis <alex.kalderimis@gmail.com>
2020-04-22 17:29:35 -0400

    [DSL] Move commit actions to new DSL

--- lib/gitlab/quick_actions/commit_actions.rb
+++ lib/gitlab/quick_actions/commit_actions.rb
@@ -3,34 +3,37 @@
 module Gitlab
   module QuickActions
     module CommitActions
-      include Gitlab::QuickActions::Dsl
+      include Gitlab::QuickActions::DslNew
 
-      # Commit only quick actions definitions
-      desc _('Tag this commit.')
-      explanation do |tag_name, message|
-        if message.present?
-          _("Tags this commit to %{tag_name} with \"%{message}\".") % { tag_name: tag_name, message: message }
-        else
-          _("Tags this commit to %{tag_name}.") % { tag_name: tag_name }
-        end
-      end
-      execution_message do |tag_name, message|
-        if message.present?
-          _("Tagged this commit to %{tag_name} with \"%{message}\".") % { tag_name: tag_name, message: message }
-        else
-          _("Tagged this commit to %{tag_name}.") % { tag_name: tag_name }
-        end
-      end
-      params 'v1.2.3 <message>'
-      parse_params do |tag_name_and_message|
-        tag_name_and_message.split(' ', 2)
-      end
       types Commit
-      condition do
-        current_user.can?(:push_code, project)
-      end
-      command :tag do |tag_name, message|
-        update(tag_name: tag_name, tag_message: message)
+
+      command :tag do
+        # Commit only quick actions definitions
+        desc _('Tag this commit.')
+        params 'v1.2.3 <message>'
+        explanation do |tag_name, message|
+          if message.present?
+            _("Tags this commit to %{tag_name} with \"%{message}\".") % { tag_name: tag_name, message: message }
+          else
+            _("Tags this commit to %{tag_name}.") % { tag_name: tag_name }
+          end
+        end
+        execution_message do |tag_name, message|
+          if message.present?
+            _("Tagged this commit to %{tag_name} with \"%{message}\".") % { tag_name: tag_name, message: message }
+          else
+            _("Tagged this commit to %{tag_name}.") % { tag_name: tag_name }
+          end
+        end
+        parse_params do |tag_name_and_message|
+          tag_name_and_message.split(' ', 2)
+        end
+        condition do
+          current_user.can?(:push_code, project)
+        end
+        action do |tag_name, message|
+          update(tag_name: tag_name, tag_message: message)
+        end
       end
     end
   end
