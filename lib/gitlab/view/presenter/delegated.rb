c7685768bee4ed7d497904dc9edb6eaab9ae5570
Peter Leitzen <pleitzen@gitlab.com>
2020-04-23 14:29:39 +0200

    Presenter delegated

--- lib/gitlab/view/presenter/delegated.rb
+++ lib/gitlab/view/presenter/delegated.rb
@@ -3,7 +3,7 @@
 module Gitlab
   module View
     module Presenter
-      class Delegated < SimpleDelegator
+      class Delegated
         include Gitlab::View::Presenter::Base
 
         def initialize(subject, **attributes)
@@ -13,12 +13,11 @@ module Gitlab
             if subject.respond_to?(key)
               raise CannotOverrideMethodError.new("#{subject} already respond to #{key}!")
             end
-
-            p define: key
-            define_singleton_method(key) { value }
           end
+        end
 
-          super(subject)
+        def method_missing(key, *args, &block)
+          @_attributes.has_key?(key) ? @_attributes[key] : @subject.__send__(key, *args, &block)
         end
       end
     end
