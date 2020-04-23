badd3c7f76c4b6ce2fc1f2d88d39543aa11d5706
Mike Kozono <mkozono@gmail.com>
2020-04-22 18:19:12 -0700

    Revert "Replicate new PackageFiles"
    Not yet backfill.

--- ee/spec/replicators/geo/package_file_replicator_spec.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'spec_helper'
-
-describe Geo::PackageFileReplicator do
-  let(:model_record) { build(:package_file, :npm) }
-
-  it_behaves_like 'a blob replicator'
-end
