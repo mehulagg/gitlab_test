28e4fc2598113e175dce9460f5874fd1d4494440
Alex Kalderimis <alex.kalderimis@gmail.com>
2020-04-23 11:00:00 -0400

    [DSL] Migrate relate actions to new DSL

--- ee/lib/ee/gitlab/quick_actions/relate_actions.rb
+++ ee/lib/ee/gitlab/quick_actions/relate_actions.rb
@@ -4,23 +4,24 @@ module EE
   module Gitlab
     module QuickActions
       module RelateActions
-        include ::Gitlab::QuickActions::Dsl
+        include ::Gitlab::QuickActions::DslNew
 
-        desc _('Mark this issue as related to another issue')
-        explanation do |related_reference|
-          _('Marks this issue as related to %{issue_ref}.') % { issue_ref: related_reference }
-        end
-        execution_message do |related_reference|
-          _('Marked this issue as related to %{issue_ref}.') % { issue_ref: related_reference }
-        end
-        params '#issue'
-        types Issue
-        condition do
-          quick_action_target.persisted? &&
-            current_user.can?(:"update_#{quick_action_target.to_ability_name}", quick_action_target)
-        end
-        command :relate do |related_param|
-          IssueLinks::CreateService.new(quick_action_target, current_user, { issuable_references: [related_param] }).execute
+        command :relate do
+          desc _('Mark this issue as related to another issue')
+          explanation do |related_reference|
+            _('Marks this issue as related to %{issue_ref}.') % { issue_ref: related_reference }
+          end
+          execution_message do |related_reference|
+            _('Marked this issue as related to %{issue_ref}.') % { issue_ref: related_reference }
+          end
+          params '#issue'
+          types Issue
+          condition do
+            quick_action_target.persisted? && can_ability?(:update)
+          end
+          action do |related_param|
+            IssueLinks::CreateService.new(quick_action_target, current_user, { issuable_references: [related_param] }).execute
+          end
         end
       end
     end
