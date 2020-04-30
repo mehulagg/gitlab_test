5dbd85fdd164b171c9441f93c66b6eb2b9726ae0
Tristan Read <tread@gitlab.com>
2020-04-30 16:26:05 +1200

    Add seed data to detail page

--- app/controllers/projects/alert_management_controller.rb
+++ app/controllers/projects/alert_management_controller.rb
@@ -2,6 +2,7 @@
 
 class Projects::AlertManagementController < Projects::ApplicationController
   before_action :ensure_feature_enabled
+  before_action :set_alert_id, only: :details
 
   def index
   end
@@ -13,5 +14,9 @@ class Projects::AlertManagementController < Projects::ApplicationController
 
   def ensure_feature_enabled
     render_404 unless Feature.enabled?(:alert_management_minimal, project)
+  end
+
+  def set_alert_id
+    @alert_id = params[:id]
   end
 end
--- app/helpers/projects/alert_management_helper.rb
+++ app/helpers/projects/alert_management_helper.rb
@@ -10,4 +10,11 @@ module Projects::AlertManagementHelper
       'alert-management-enabled' => Feature.enabled?(:alert_management_minimal, project).to_s
     }
   end
+
+  def alert_management_detail_data(project, alert_id)
+    {
+      'alert-id' => alert_id,
+      'project-path' => project.full_path,
+    }
+  end
 end
