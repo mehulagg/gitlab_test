add5f516b0a357fb82365f59d62348a754fac362
lauraMon <lmontemayor@gitlab.com>
2020-04-29 20:00:45 -0400

    Updates some styles and translations

--- app/assets/javascripts/alert_management/components/alert_details.vue
+++ app/assets/javascripts/alert_management/components/alert_details.vue
@@ -1,19 +1,19 @@
 <script>
-import { GlButton, GlDropdown, GlDropdownItem, GlTabs, GlTab } from '@gitlab/ui';
-import { __ } from '~/locale';
+import { GlNewDropdown, GlNewDropdownItem, GlTabs, GlTab } from '@gitlab/ui';
+import { s__ } from '~/locale';
 
+/* eslint-disable import/no-commonjs */
 const mock = require('./alerts.json');
 
 export default {
   statuses: {
-    triggered: __('Triggered'),
-    acknowledged: __('Acknowledged'),
-    ignored: __('Ignored'),
+    triggered: s__('AlertManagement|Triggered'),
+    acknowledged: s__('AlertManagement|Acknowledged'),
+    ignored: s__('AlertManagement|Ignored'),
   },
   components: {
-    GlButton,
-    GlDropdown,
-    GlDropdownItem,
+    GlNewDropdown,
+    GlNewDropdownItem,
     GlTab,
     GlTabs,
   },
@@ -26,39 +26,39 @@ export default {
 </script>
 <template>
   <div>
-    <div class="d-flex justify-content-between mt-4">
-      <h5>Reported {{ alertDetails[0].startedAt }}</h5>
-      <gl-button category="primary" variant="success">{{ __("Create Issue") }}</gl-button>
-    </div>
-    <hr />
-    <h1>{{ alertDetails[0].title }}</h1>
-    <div class="d-flex justify-content-between">
-      <div>
-        <gl-tabs>
-          <gl-tab title="Overview">
-            <ul>
-              <li>{{ __("Start time:") }}</li>
-              <li>{{ __("End time:") }}</li>
-              <li>{{ __("Events:") }}</li>
-            </ul>
-          </gl-tab>
-          <gl-tab title="Full Detail Alerts">
-            <ul>
-              <li>{{ __("description:") }}</li>
-            </ul>
-          </gl-tab>
-        </gl-tabs>
-      </div>
-      <div>
-        <gl-dropdown :text="alertDetails[0].status">
-          <gl-dropdown-item
+    <div class="d-flex justify-content-between mt-3">
+      <p class="gl-font-size-42">{{ alertDetails[0].title }}</p>
+      <div class="align-self-center">
+        <gl-new-dropdown :text="alertDetails[0].status" right>
+          <gl-new-dropdown-item
             v-for="(label, field) in $options.statuses"
             :key="field"
             class="align-middle"
             >{{ label }}
-          </gl-dropdown-item>
-        </gl-dropdown>
+          </gl-new-dropdown-item>
+        </gl-new-dropdown>
       </div>
+    </div>
+    <div class="d-flex justify-content-between">
+      <gl-tabs>
+        <gl-tab title="Overview">
+          <ul class="pl-3">
+            <li class="font-weight-bold mb-3 mt-2">
+              {{ s__('AlertManagement|Start time:') }}
+              <span class="font-weight-normal">{{ alertDetails[0].startedAt }}</span>
+            </li>
+            <li class="font-weight-bold my-3">
+              {{ s__('AlertManagement|End time:')
+              }}<span class="font-weight-normal"> {{ alertDetails[0].endedAt }}</span>
+            </li>
+            <li class="font-weight-bold my-3">
+              {{ s__('AlertManagement|Events:') }}
+              <span class="font-weight-normal"> {{ alertDetails[0].eventCount }}</span>
+            </li>
+          </ul>
+        </gl-tab>
+        <gl-tab title="Full Detail Alerts" />
+      </gl-tabs>
     </div>
   </div>
 </template>
