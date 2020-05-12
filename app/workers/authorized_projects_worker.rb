8633898983de2cacf2f72ce11b5db15260d13754
Imre Farkas <ifarkas@gitlab.com>
2020-05-12 13:10:43 +0200

    Remove rspec-mocks workaround for Ruby 2.3.7

--- app/workers/authorized_projects_worker.rb
+++ app/workers/authorized_projects_worker.rb
@@ -10,14 +10,6 @@ class AuthorizedProjectsWorker
 
   idempotent!
 
-  # This is a workaround for a Ruby 2.3.7 bug. rspec-mocks cannot restore the
-  # visibility of prepended modules. See https://github.com/rspec/rspec-mocks/issues/1231
-  # for more details.
-  if Rails.env.test?
-    def self.bulk_perform_and_wait(args_list, timeout: 10)
-    end
-  end
-
   # rubocop: disable CodeReuse/ActiveRecord
   def perform(user_id)
     user = User.find_by(id: user_id)
