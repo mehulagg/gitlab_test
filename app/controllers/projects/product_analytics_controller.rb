343555e20efb9090a883d59f6d2d4aa0c7292311
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2020-05-14 20:02:34 +0300

    A bit of cleanup
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- app/controllers/projects/product_analytics_controller.rb
+++ app/controllers/projects/product_analytics_controller.rb
@@ -2,17 +2,21 @@
 
 class Projects::ProductAnalyticsController < Projects::ApplicationController
   respond_to :html
+
+  before_action :javascript_vars, only: [:example, :test]
+
   # routes are defined in project.rb
   # Database is twitten to by https://gitlab.com/gitlab-org/snowplow-go-collector
 
   def index
-    @nr_of_events = ProductAnalyticsEvent.number(@project.id.to_s)
-    @events = pae(100)
+    @number_of_events = ProductAnalyticsEvent.number(@project.id.to_s)
+    @events = product_analytics_events(100)
   end
 
   def activity
     @timerange = 30
     @graphs = []
+
     %w(collector_tstamp dvce_created_tstamp).each do |graph|
       results = ProductAnalyticsEvent.by_day_and_graph(@project.id.to_s, graph.to_sym, @timerange.days)
       @graphs << { id: graph, keys: results.keys, values: results.values }
@@ -22,6 +26,7 @@ class Projects::ProductAnalyticsController < Projects::ApplicationController
   def users
     @timerange = 30
     @graphs = []
+
     %w(v_collector platform os_timezone br_lang doc_charset v_tracker br_cookies br_colordepth).each do |graph|
       results = ProductAnalyticsEvent.by_graph(@project.id.to_s, graph.to_sym, @timerange.days)
       # TODO Consider sanitizing the keys with https://api.rubyonrails.org/classes/ERB/Util.html#method-c-json_escape to prevent XSS
@@ -30,17 +35,15 @@ class Projects::ProductAnalyticsController < Projects::ApplicationController
   end
 
   def example
-    javascript_vars
   end
 
   def test
-    javascript_vars
-    @event = pae(1).try(:first)
+    @event = product_analytics_events(1).try(:first)
   end
 
   private
 
-  def pae(limit)
+  def product_analytics_events(limit)
     ProductAnalyticsEvent.get_some(@project.id.to_s, limit)
   end
 
