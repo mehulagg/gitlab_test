01e0ce2f56930e203f10a17b6310616ab2aca30c
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2020-06-03 13:00:18 +0300

    Reuse GitLab charts for PA
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

---
+++ app/assets/javascripts/analytics/product_analytics/activity_charts_bundle.js
@@ -0,0 +1,40 @@
+import Vue from 'vue';
+import { __ } from '~/locale';
+import { GlColumnChart } from '@gitlab/ui/dist/charts';
+
+export default () => {
+	const containers = document.querySelectorAll('.js-project-analytics-chart');
+
+	if (!containers) {
+		return false;
+	}  
+
+	return containers.forEach(container => {
+		return new Vue({
+			el: container,
+			components: {
+				GlColumnChart,
+			},
+			data() {
+				return {
+					chartData: JSON.parse(container.dataset.chartData),
+				};
+			},
+			computed: {
+				seriesData() {
+					return { full: this.chartData.keys.map((val, idx) => [val, this.chartData.values[idx]]) };
+				},
+			},
+			render(h) {
+				return h(GlColumnChart, {
+					props: {
+						data: this.seriesData,
+						xAxisTitle: __('Platform usage'),
+						yAxisTitle: __('y axis title'),
+						xAxisType: 'category',
+					},
+				});
+			},
+		});
+	});
+};
