9d1968528c666bff89aa4762ed4dc32036dd80d9
Dave Pisek <dpisek@gitlab.com>
2020-06-03 14:46:47 +1000

    POC: Add help link and icon to modal

--- ee/app/assets/javascripts/approvals/components/modal_license_compliance.vue
+++ ee/app/assets/javascripts/approvals/components/modal_license_compliance.vue
@@ -1,11 +1,16 @@
 <script>
 import { mapState } from 'vuex';
+import { GlButton, GlIcon, GlLink, GlSprintf } from '@gitlab/ui';
 import { __ } from '~/locale';
 import GlModalVuex from '~/vue_shared/components/gl_modal_vuex.vue';
 import RuleForm from './rule_form.vue';
 
 export default {
   components: {
+    GlButton,
+    GlIcon,
+    GlLink,
+    GlSprintf,
     GlModalVuex,
     RuleForm,
   },
@@ -44,10 +49,17 @@ export default {
     <rule-form ref="form" :init-rule="rule" locked-name="License-Check" />
     <template #modal-footer="{ ok, cancel }">
       <div class="d-flex w-100">
-        <p>Yo this is the message of the day</p>
+        <p>
+          <gl-icon name="question" :size="12" class="text-primary-600" />
+          <gl-sprintf :message="__('Learn more about %{licenseCheckHelpLink}')">
+            <template #licenseCheckHelpLink>
+              <gl-link href="http://example.com" target="_blank">{{ __('License-Check') }}</gl-link>
+            </template>
+          </gl-sprintf>
+        </p>
         <div class="ml-auto">
-          <button @click="cancel">{{ __('Cancel') }}</button>
-          <button @click="ok">{{ title }}</button>
+          <gl-button @click="cancel">{{ __('Cancel') }}</gl-button>
+          <gl-button variant="success" @click="ok">{{ title }}</gl-button>
         </div>
       </div>
     </template>
