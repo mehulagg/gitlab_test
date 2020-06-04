5763b15b712d952641ce6ae2db6939fddb85c38b
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2020-06-04 16:04:16 +0300

    Format activity_charts_bundle.js file
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- app/assets/javascripts/analytics/product_analytics/activity_charts_bundle.js
+++ app/assets/javascripts/analytics/product_analytics/activity_charts_bundle.js
@@ -3,38 +3,38 @@ import { __ } from '~/locale';
 import { GlColumnChart } from '@gitlab/ui/dist/charts';
 
 export default () => {
-	const containers = document.querySelectorAll('.js-project-analytics-chart');
+  const containers = document.querySelectorAll('.js-project-analytics-chart');
 
-	if (!containers) {
-		return false;
-	}  
+  if (!containers) {
+    return false;
+  }
 
-	return containers.forEach(container => {
-		return new Vue({
-			el: container,
-			components: {
-				GlColumnChart,
-			},
-			data() {
-				return {
-					chartData: JSON.parse(container.dataset.chartData),
-				};
-			},
-			computed: {
-				seriesData() {
-					return { full: this.chartData.keys.map((val, idx) => [val, this.chartData.values[idx]]) };
-				},
-			},
-			render(h) {
-				return h(GlColumnChart, {
-					props: {
-						data: this.seriesData,
-						xAxisTitle: __('Value'),
-						yAxisTitle: __('Number of events'),
-						xAxisType: 'category',
-					},
-				});
-			},
-		});
-	});
+  return containers.forEach(container => {
+    return new Vue({
+      el: container,
+      components: {
+        GlColumnChart,
+      },
+      data() {
+        return {
+          chartData: JSON.parse(container.dataset.chartData),
+        };
+      },
+      computed: {
+        seriesData() {
+          return { full: this.chartData.keys.map((val, idx) => [val, this.chartData.values[idx]]) };
+        },
+      },
+      render(h) {
+        return h(GlColumnChart, {
+          props: {
+            data: this.seriesData,
+            xAxisTitle: __('Value'),
+            yAxisTitle: __('Number of events'),
+            xAxisType: 'category',
+          },
+        });
+      },
+    });
+  });
 };
