4f6848391f906e030f2d03e19a06188713faa01c
Ben Bodenmiller <bbodenmiller@hotmail.com>
2020-06-06 22:56:34 +0000

    Update to be owner

--- app/models/concerns/select_for_project_authorization.rb
+++ app/models/concerns/select_for_project_authorization.rb
@@ -8,8 +8,8 @@ module SelectForProjectAuthorization
       select("projects.id AS project_id, members.access_level")
     end
 
-    def select_as_maintainer_for_project_authorization
-      select(["projects.id AS project_id", "#{Gitlab::Access::MAINTAINER} AS access_level"])
+    def select_as_owner_for_project_authorization
+      select(["projects.id AS project_id", "#{Gitlab::Access::OWNER} AS access_level"])
     end
   end
 end
