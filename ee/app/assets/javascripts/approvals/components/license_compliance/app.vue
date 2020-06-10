85b1438ab2bcde162ac8cc1da40790ea9ac9f39a
Dave Pisek <dpisek@gitlab.com>
2020-06-10 22:04:29 +1000

    Finish License-Approval component specs

--- ee/app/assets/javascripts/approvals/components/license_compliance/app.vue
+++ ee/app/assets/javascripts/approvals/components/license_compliance/app.vue
@@ -1,20 +1,31 @@
 <script>
 import { mapActions, mapState } from 'vuex';
-import LicenseComplianceApprovalAction from './approval_action.vue';
 import ModalLicenseCompliance from '../modal_license_compliance.vue';
+import { GlButton, GlIcon, GlLink, GlSkeletonLoading, GlSprintf } from '@gitlab/ui';
+import { __ } from '~/locale';
 
 export default {
   components: {
-    LicenseComplianceApprovalAction,
+    GlButton,
+    GlIcon,
+    GlLink,
+    GlSkeletonLoading,
+    GlSprintf,
     ModalLicenseCompliance,
   },
   computed: {
     ...mapState({
-      isLoading: state => state.approvals.isLoading,
-      rules: state => state.approvals.rules,
+      isLoading: ({ approvals }) => approvals.isLoading,
+      rules: ({ approvals }) => approvals.rules,
+      docsLink: ({ approvals }) => approvals.docsLink,
     }),
     licenseCheckRule() {
-      return this.rules.find(({ name }) => name === 'License-Check');
+      return this.rules?.find(({ name }) => name === 'License-Check');
+    },
+    licenseCheckStatusText() {
+      return this.isLicenseCheckActive
+        ? __('%{docLinkStart}License-Check%{docLinkEnd} is active')
+        : __('%{docLinkStart}License-Check%{docLinkEnd} is inactive');
     },
   },
   created() {
@@ -22,17 +33,32 @@ export default {
   },
   methods: {
     ...mapActions(['fetchRules']),
+    // @TODO - rename the modal action
     ...mapActions({ openCreateModal: 'createModal/open' }),
   },
 };
 </script>
-<template
-  ><span
-    ><license-compliance-approval-action
-      docs-link="http://foo.com"
-      :is-license-check-active="Boolean(licenseCheckRule)"
-      :is-loading="isLoading"
-      @approvalClick="openCreateModal(licenseCheckRule)"/>
-    <modal-license-compliance modal-id="yo"
-  /></span>
+<template>
+  <span class="gl-display-inline-flex gl-align-items-center">
+    <gl-button name="openModal" :loading="isLoading" @click="openCreateModal(licenseCheckRule)"
+      >{{ __('License Approval') }}
+    </gl-button>
+    <span class="gl-ml-3">
+      <gl-skeleton-loading
+        v-if="isLoading"
+        :aria-label="__('loading')"
+        :lines="1"
+        class="gl-display-inline-flex gl-h-auto gl-align-items-center"
+      />
+      <span v-else>
+        <gl-icon name="information" :size="12" class="gl-text-blue-600" />
+        <gl-sprintf :message="licenseCheckStatusText" class="gl-inline-flex">
+          <template #docLink="{ content }">
+            <gl-link :href="docsLink" target="_blank">{{ content }}</gl-link>
+          </template>
+        </gl-sprintf>
+      </span>
+    </span>
+    <modal-license-compliance modal-id="move-me-into-modal" />
+  </span>
 </template>
--- ee/spec/frontend/approvals/components/license_compliance/app_spec.js
+++ ee/spec/frontend/approvals/components/license_compliance/app_spec.js
@@ -1,6 +1,8 @@
 import Vuex from 'vuex';
-import { shallowMount, createLocalVue } from '@vue/test-utils';
-import component from 'ee/approvals/components/license_compliance/app.vue';
+import { mount, createLocalVue } from '@vue/test-utils';
+import { GlIcon } from '@gitlab/ui';
+import ApprovalsLicenseCompliance from 'ee/approvals/components/license_compliance/app.vue';
+import ModalLicenseCompliance from 'ee/approvals/components/modal_license_compliance.vue';
 
 const localVue = createLocalVue();
 localVue.use(Vuex);
@@ -9,12 +11,21 @@ describe('EE Approvals LicenseCompliance App', () => {
   let wrapper;
   let store;
 
+  const fetchRulesMock = jest.fn();
+  const openModalMock = jest.fn();
+
   const createStore = () => {
-    store = new Vuex.Store({
+    const storeOptions = {
       actions: {
-        fetchRules: () => {},
+        fetchRules: fetchRulesMock,
       },
       modules: {
+        createModal: {
+          namespaced: true,
+          actions: {
+            open: openModalMock,
+          },
+        },
         approvals: {
           state: {
             isLoading: true,
@@ -22,19 +33,23 @@ describe('EE Approvals LicenseCompliance App', () => {
           },
         },
       },
-    });
+    };
+
+    store = new Vuex.Store(storeOptions);
   };
 
-  const createComponent = () => {
-    wrapper = shallowMount(component, {
+  const createWrapper = () => {
+    wrapper = mount(ApprovalsLicenseCompliance, {
       localVue,
       store,
+      stubs: {
+        ModalLicenseCompliance,
+      },
     });
   };
 
   beforeEach(() => {
     createStore();
-    createComponent();
   });
 
   afterEach(() => {
@@ -42,7 +57,69 @@ describe('EE Approvals LicenseCompliance App', () => {
     wrapper = null;
   });
 
-  it('renders correctly', () => {
-    expect(wrapper.exists()).toBe(true);
+  const findByHrefAttribute = href => wrapper.find(`[href="${href}"]`);
+  const findOpenModalButton = () => wrapper.find('button[name="openModal"]');
+  const findLoadingIndicator = () => wrapper.find('[aria-label="loading"]');
+  const findInformationIcon = () => wrapper.find(GlIcon);
+
+  describe('when created', () => {
+    it('fetches approval rules', () => {
+      expect(fetchRulesMock).not.toHaveBeenCalled();
+      createWrapper();
+      expect(fetchRulesMock).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('when loading', () => {
+    beforeEach(() => {
+      createWrapper();
+    });
+
+    it('renders the open-modal button with an active loading state', () => {
+      expect(findOpenModalButton().props('loading')).toBe(true);
+    });
+
+    it('disables the open-modal button', () => {
+      expect(findOpenModalButton().attributes('disabled')).toBeTruthy();
+    });
+
+    it('renders a loading indicator', () => {
+      expect(findLoadingIndicator().exists()).toBe(true);
+    });
+  });
+
+  describe('when data has loaded', () => {
+    const docsLink = 'http://docs-link.com';
+
+    beforeEach(() => {
+      store.state.approvals.isLoading = false;
+      store.state.approvals.docsLink = docsLink;
+
+      createWrapper();
+    });
+
+    it('renders the open-modal button without an active loading state', () => {
+      expect(findOpenModalButton().props('loading')).toBe(false);
+    });
+
+    it('does not render a loading indicator', () => {
+      expect(findLoadingIndicator().exists()).toBe(false);
+    });
+
+    it('renders a information icon', () => {
+      expect(findInformationIcon().props('name')).toBe('information');
+    });
+
+    it('opens the link to the documentation page in a new tab', () => {
+      expect(findByHrefAttribute(docsLink).attributes('target')).toBe('_blank');
+    });
+
+    it('opens a model when the license-approval button is clicked', async () => {
+      expect(openModalMock).not.toHaveBeenCalled();
+
+      await findOpenModalButton().trigger('click');
+
+      expect(openModalMock).toHaveBeenCalled();
+    });
   });
 });
