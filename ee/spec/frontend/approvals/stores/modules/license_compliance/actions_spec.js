03d8900a8f42fb63f9d71646d55c60b53c02f3bb
Dave Pisek <dpisek@gitlab.com>
2020-06-15 14:03:57 +1000

    Add more action tests

--- ee/spec/frontend/approvals/stores/modules/license_compliance/actions_spec.js
+++ ee/spec/frontend/approvals/stores/modules/license_compliance/actions_spec.js
@@ -1,26 +1,37 @@
-import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
 import testAction from 'helpers/vuex_action_helper';
 import * as actions from 'ee/approvals/stores/modules/license_compliance/actions';
 import * as baseMutationTypes from 'ee/approvals/stores/modules/base/mutation_types';
-import createBaseState from 'ee/approvals/stores/modules/base/state';
+import { mapApprovalSettingsResponse } from 'ee/approvals/mappers';
+import axios from '~/lib/utils/axios_utils';
 
 describe('EE approvals license-compliance actions', () => {
   let state;
   let axiosMock;
 
+  const mocks = {
+    state: {
+      settingsPath: 'projects/9/approval_settings',
+    },
+  };
+
   beforeEach(() => {
-    state = createBaseState();
+    state = {
+      settings: {
+        settingsPath: mocks.state.settingsPath,
+      },
+    };
     axiosMock = new MockAdapter(axios);
   });
 
   describe('receiveRulesSuccess', () => {
     it('sets rules to given payload and loading to false', () => {
-      const approvalSetting = {};
-      testAction(actions.receiveRulesSuccess, approvalSetting, state, [
+      const payload = {};
+
+      return testAction(actions.receiveRulesSuccess, payload, state, [
         {
           type: baseMutationTypes.SET_APPROVAL_SETTINGS,
-          payload: approvalSetting,
+          payload,
         },
         {
           type: baseMutationTypes.SET_LOADING,
@@ -30,5 +41,52 @@ describe('EE approvals license-compliance actions', () => {
     });
   });
 
-  describe('fetchRules', () => {});
+  describe('fetchRules', () => {
+    it('sets loading state to be true and dispatches "receiveRuleSuccess"', () => {
+      const responseData = { rules: [] };
+      axiosMock.onGet(mocks.state.settingsPath).replyOnce(200, responseData);
+
+      return testAction(
+        actions.fetchRules,
+        null,
+        state,
+        [
+          {
+            type: baseMutationTypes.SET_LOADING,
+            payload: true,
+          },
+        ],
+        [
+          {
+            type: 'receiveRulesSuccess',
+            payload: mapApprovalSettingsResponse(responseData),
+          },
+        ],
+      );
+    });
+  });
+
+  describe('postRule', () => {
+    it('is a placeholder', () => {
+      expect(true).toBe(true);
+    });
+  });
+
+  describe('deleteRule', () => {
+    it('is a placeholder', () => {
+      expect(true).toBe(true);
+    });
+  });
+
+  describe('putRule', () => {
+    it('is a placeholder', () => {
+      expect(true).toBe(true);
+    });
+  });
+
+  describe('putFallbackRule', () => {
+    it('is a placeholder', () => {
+      expect(true).toBe(true);
+    });
+  });
 });
