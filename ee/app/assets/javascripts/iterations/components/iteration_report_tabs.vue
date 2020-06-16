7dc347ca7d25a16bae896972442daaf6d4714005
Simon Knox <simon@gitlab.com>
2020-06-17 08:29:52 +1000

    Replace columns with table view

--- ee/app/assets/javascripts/iterations/components/iteration_report_tabs.vue
+++ ee/app/assets/javascripts/iterations/components/iteration_report_tabs.vue
@@ -7,6 +7,7 @@ import {
   GlLink,
   GlTab,
   GlTabs,
+  GlTable,
   GlTooltipDirective,
 } from '@gitlab/ui';
 import { __ } from '~/locale';
@@ -15,6 +16,14 @@ import { getIdFromGraphQLId } from '~/graphql_shared/utils';
 import query from '../queries/iteration_issues.query.graphql';
 
 export default {
+  fields: [
+    {
+      key: 'title',
+      label: __('Title'),
+    },
+    { key: 'status', label: __('Status') },
+    { key: 'assignees', label: __('Assignees') },
+  ],
   isScopedLabel,
   components: {
     GlAvatar,
@@ -24,6 +33,7 @@ export default {
     GlLabel,
     GlTab,
     GlTabs,
+    GlTable,
   },
   directives: {
     GlTooltip: GlTooltipDirective,
@@ -107,51 +117,44 @@ export default {
 
 <template>
   <gl-tabs>
-    <gl-tab title="Issues" class="row milestone-content gl-display-flex!">
+    <gl-tab title="Issues">
       <template #title>
         <span>{{ __('Issues') }}</span
         ><gl-badge class="ml-2" variant="neutral">{{ totalIssues }}</gl-badge>
       </template>
-      <div v-for="g in groupedIssues" :key="g.title" class="col-sm-4">
-        <gl-card header-class="gl-line-height-normal" body-class="gl-p-0">
-          <template #header>
-            <span>{{ g.title }}</span>
-          </template>
-          <template #default>
-            <ul class="content-list milestone-issues-list">
-              <li v-for="issue in g.issues" :key="issue.title" class="gl-p-5!">
-                <gl-link :href="issue.webUrl">{{ issue.title }}</gl-link>
-                <div class="issuable-detail">
-                  <gl-link :href="issue.webUrl">#{{ issue.iid }}</gl-link>
-                  <gl-label
-                    v-for="label in issue.labels"
-                    :key="label.id"
-                    :background-color="label.color"
-                    :title="label.title"
-                    :description="label.description"
-                    :scoped="$options.isScopedLabel(label)"
-                    class="mr-2"
-                    size="sm"
-                  />
-                  <span class="assignee-icon">
-                    <span
-                      v-for="assignee in issue.assignees"
-                      :key="assignee.username"
-                      v-gl-tooltip="
-                        sprintf(__('Assigned to %{assigneeName}'), {
-                          assigneeName: assignee.name,
-                        })
-                      "
-                    >
-                      <gl-avatar :src="assignee.avatarUrl" :size="16" />
-                    </span>
-                  </span>
-                </div>
-              </li>
-            </ul>
-          </template>
-        </gl-card>
-      </div>
+
+      <gl-table :fields="$options.fields" :items="issues" stacked="sm" show-empty> </gl-table>
+      <ul class="content-list issuable-list">
+        <li v-for="issue in issues" :key="issue.id">
+          <gl-link :href="issue.webUrl">{{ issue.title }}</gl-link>
+          <div class="issuable-detail">
+            <gl-link :href="issue.webUrl">#{{ issue.iid }}</gl-link>
+            <gl-label
+              v-for="label in issue.labels"
+              :key="label.id"
+              :background-color="label.color"
+              :title="label.title"
+              :description="label.description"
+              :scoped="$options.isScopedLabel(label)"
+              class="mr-2"
+              size="sm"
+            />
+            <span class="assignee-icon">
+              <span
+                v-for="assignee in issue.assignees"
+                :key="assignee.username"
+                v-gl-tooltip="
+                  sprintf(__('Assigned to %{assigneeName}'), {
+                    assigneeName: assignee.name,
+                  })
+                "
+              >
+                <gl-avatar :src="assignee.avatarUrl" :size="16" />
+              </span>
+            </span>
+          </div>
+        </li>
+      </ul>
     </gl-tab>
   </gl-tabs>
 </template>
