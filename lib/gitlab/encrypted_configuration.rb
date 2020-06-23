7ebcc70d7ba4809736a75f357b82581c42141746
DJ Mountney <dj@gitlab.com>
2020-06-23 09:38:24 -0700

    Cleanup encrypted config initializer
    to just use what we need
    
    Drop the more generic encrypted command as we aren't using it currently

--- lib/gitlab/encrypted_configuration.rb
+++ lib/gitlab/encrypted_configuration.rb
@@ -2,22 +2,21 @@
 
 module Gitlab
   class EncryptedConfiguration < ActiveSupport::EncryptedConfiguration
-    def initialize(config_path: nil, key: nil, key_path: nil, env_key: nil, raise_if_missing_key: false)
+    def initialize(config_path: nil, key: nil)
       @content_path = Pathname.new(config_path).yield_self { |path| path.symlink? ? path.realpath : path } if config_path
-      @key_path = Pathname.new(key_path) if key_path
-      @key, @env_key, @raise_if_missing_key = key, env_key, raise_if_missing_key
+      @key = key
     end
 
     def key
-      @key || super
+      @key
     end
 
     def read_env_key
-      super if env_key
+      nil
     end
 
     def read_key_file
-      super if key_path
+      nil
     end
   end
 end
--- lib/tasks/gitlab/encrypted.rake
+++ lib/tasks/gitlab/encrypted.rake
@@ -1,15 +1,5 @@
 namespace :gitlab do
   namespace :encrypted do
-    desc 'GitLab | Encrypted | edit encrypted config files'
-    task :edit,  [:path] => ['gitlab:encrypted:safe_mode', :environment] do |t, args|
-      Gitlab::EncryptedCommand.edit(args[:path])
-    end
-
-    desc 'GitLab | Encrypted | show encrypted config files'
-    task :show, [:path] => ['gitlab:encrypted:safe_mode', :environment] do |t, args|
-      Gitlab::EncryptedCommand.show(args[:path])
-    end
-
     task :safe_mode do
       ENV['GITLAB_ENCRYPTED_SAFE_MODE'] = 'true'
     end
