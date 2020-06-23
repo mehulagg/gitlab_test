a8fbe10f719470eaacfeba0ff4459a803bdbf299
DJ Mountney <dj@gitlab.com>
2020-06-23 09:14:35 -0700

    Reduce encrypted method signature
    down to just what is used

--- config/settings.rb
+++ config/settings.rb
@@ -152,17 +152,12 @@ class Settings < Settingslogic
       Gitlab::Application.secrets.db_key_base
     end
 
-    def encrypted(path, key: nil, key_path: nil, env_key: nil, allow_in_safe_mode: false)
+    def encrypted(path, allow_in_safe_mode: false)
       return Gitlab::EncryptedConfiguration.new if ENV['GITLAB_ENCRYPTED_SAFE_MODE'] && !allow_in_safe_mode
-
-      # If no key defaults are provided, use our db_key_base
-      key = Settings.attr_encrypted_db_key_base_truncated unless key || key_path || env_key
 
       Gitlab::EncryptedConfiguration.new(
         config_path: Rails.root.join(path),
-        key: key,
-        key_path: key_path ? Rails.root.join(key_path) : nil,
-        env_key: env_key
+        key: Settings.attr_encrypted_db_key_base_truncated,
       )
     end
 
