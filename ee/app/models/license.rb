bcb883e57e252e706edb6ec988d64f4a85551ce3
serenafang <sfang@gitlab.com>
2020-06-24 10:36:11 -0500

    Add multiple_clusters back into ee

--- ee/app/models/license.rb
+++ ee/app/models/license.rb
@@ -90,6 +90,7 @@ class License < ApplicationRecord
     merge_trains
     metrics_reports
     multiple_approval_rules
+    multiple_clusters
     multiple_group_issue_boards
     object_storage
     operations_dashboard
