d98ab111fc866a9486b8ed8f4e8a681f94938f75
Mike Kozono <mkozono@gmail.com>
2020-06-26 09:20:57 -0700

    Expose graphql field name

--- ee/app/controllers/admin/geo/replicables_controller.rb
+++ ee/app/controllers/admin/geo/replicables_controller.rb
@@ -2,7 +2,16 @@
 
 class Admin::Geo::ReplicablesController < Admin::Geo::ApplicationController
   before_action :check_license!
+  before_action :set_replicator_class
 
   def index
+  end
+
+  private
+
+  def set_replicator_class
+    replicable_name = params[:plural_replicable_name].singularize
+
+    @replicator_class = Gitlab::Geo::Replicator.for_replicable_name(replicable_name)
   end
 end
