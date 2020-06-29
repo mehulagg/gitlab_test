7f7f2b203adb034af69e6c285794f3925c477eb6
Amy Troschinetz <atroschinetz@gitlab.com>
2020-06-29 15:35:26 -0700

    Code related to `unprotect` concern moved out of EE

--- app/policies/protected_branch_policy.rb
+++ app/policies/protected_branch_policy.rb
@@ -3,10 +3,24 @@
 class ProtectedBranchPolicy < BasePolicy
   delegate { @subject.project }
 
+  condition(:can_unprotect) do
+    @subject.can_unprotect?(@user)
+  end
+
+  condition(:unprotect_restrictions_enabled, scope: :subject) do
+    @subject.project.feature_available?(:unprotection_restrictions)
+  end
+
   rule { can?(:admin_project) }.policy do
     enable :create_protected_branch
     enable :update_protected_branch
     enable :destroy_protected_branch
+  end
+
+  rule { unprotect_restrictions_enabled & ~can_unprotect }.policy do
+    prevent :create_protected_branch # Prevent a user creating a rule they wouldn't be able to update or destroy
+    prevent :update_protected_branch
+    prevent :destroy_protected_branch
   end
 end
 
