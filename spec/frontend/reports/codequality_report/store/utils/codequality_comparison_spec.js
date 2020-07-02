0e8b76bad7fec4aec6df317eb9aa4f1b8cba73bd
mfluharty <mfluharty@gitlab.com>
2020-07-02 14:25:03 -0600

    Introduce new vuex store for code quality widget
    Copy existing functionality from mr widget store
    New store fetches the reports from the endpoints,
    passes them to the worker for comparison,
    worker passes back the diff of the issues to be displayed

---
+++ spec/frontend/reports/codequality_report/store/utils/codequality_comparison_spec.js
@@ -0,0 +1,64 @@
+import {
+  parseCodeclimateMetrics,
+  doCodeClimateComparison,
+} from '~/reports/codequality_report/store/utils/codequality_comparison';
+import mockFilterByKey from '~/reports/codequality_report/store/utils/filter_by_key';
+import { baseIssues, mockParsedHeadIssues, mockParsedBaseIssues } from '../../mock_data';
+
+jest.mock('~/reports/codequality_report/workers/codequality_comparison_worker', () => {
+  let mockPostMessageCallback;
+  return jest.fn().mockImplementation(() => {
+    return {
+      addEventListener: (_, callback) => {
+        mockPostMessageCallback = callback;
+      },
+      postMessage: data => {
+        if (!data.headIssues) throw new Error();
+        return mockPostMessageCallback({
+          data: {
+            newIssues: mockFilterByKey(data.headIssues, data.baseIssues, 'fingerprint'),
+            resolvedIssues: mockFilterByKey(data.baseIssues, data.headIssues, 'fingerprint'),
+          },
+        });
+      },
+    };
+  });
+});
+
+describe('Codequality report store utils', () => {
+  let result;
+
+  describe('parseCodeclimateMetrics', () => {
+    it('should parse the received issues', () => {
+      [result] = parseCodeclimateMetrics(baseIssues, 'path');
+
+      expect(result.name).toEqual(baseIssues[0].check_name);
+      expect(result.path).toEqual(baseIssues[0].location.path);
+      expect(result.line).toEqual(baseIssues[0].location.lines.begin);
+    });
+  });
+
+  describe('doCodeClimateComparison', () => {
+    describe('when the comparison worker finds changed issues', () => {
+      beforeEach(async () => {
+        result = await doCodeClimateComparison(mockParsedHeadIssues, mockParsedBaseIssues);
+      });
+
+      it('returns the new and resolved issues', () => {
+        expect(result.resolvedIssues[0]).toEqual(mockParsedBaseIssues[0]);
+        expect(result.newIssues[0]).toEqual(mockParsedHeadIssues[0]);
+      });
+    });
+
+    describe('when the comparison worker finds no changed issues', () => {
+      beforeEach(async () => {
+        result = await doCodeClimateComparison([], []);
+      });
+
+      it('returns the empty issue arrays', () => {
+        expect(result.newIssues).toEqual([]);
+        expect(result.resolvedIssues).toEqual([]);
+      });
+    });
+  });
+});
