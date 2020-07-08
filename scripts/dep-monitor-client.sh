b994266cd986d80f2a1cd5c5d7f5644aa82da5a3
dappelt <dappelt@gitlab.com>
2020-07-08 17:16:08 +0200

    squash me

--- scripts/dep-monitor-client.sh
+++ scripts/dep-monitor-client.sh
@@ -2,5 +2,14 @@
 apt-get update && apt-get install -y jq
 cd /builds/gitlab-org/gitlab
 du -h -d 1 . | sort -h
-tar -czvf gitlab.tgz --exclude=".git" --exclude="node_modules" --exclude="doc" . 
-curl -vvv -X POST -H "Authorization: Basic c29tZWJlcnQ6ajN2ZXBlNHlNQ21JKzY0bw==" -H "Content-Type: application/octet-stream" --data-binary @gitlab.tgz 46.101.173.169:3000/monitor/project | tee /tmp/jobid.txt | jq . && JOBID=$(jq .id /tmp/jobid.txt); JOBID=${JOBID:1:36}
+tar -czf gitlab.tgz --exclude=".git" --exclude="node_modules" --exclude="doc" .
+curl --silent -X POST -H "Authorization: Basic c29tZWJlcnQ6ajN2ZXBlNHlNQ21JKzY0bw==" -H "Content-Type: application/octet-stream" --data-binary @gitlab.tgz 46.101.173.169:3000/monitor/project | tee /tmp/jobid.txt | jq . && JOBID=$(jq .id /tmp/jobid.txt); JOBID=${JOBID:1:36}
+
+
+while ( $(curl --silent -G -d "id=$JOBID" 46.101.173.169:3000 | jq .status) == "pending" )
+do
+    echo .
+    sleep 1
+done
+
+curl --silent -G -d "id=$JOBID" 46.101.173.169:3000
