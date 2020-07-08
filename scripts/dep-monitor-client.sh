f3a5b082cc4a57bd81a39ec8e45d3a114703ac09
dappelt <dappelt@gitlab.com>
2020-07-08 15:26:02 +0200

    install jq

--- scripts/dep-monitor-client.sh
+++ scripts/dep-monitor-client.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+apt-get update && apt-get install jq
 cd /builds/gitlab-org/gitlab
-tar -czvf gitlab.tgz --exclude="./.git/" --exclude="node_modules/" --exclude="doc/" .
+tar -czvf gitlab.tgz . --exclude="./.git/" --exclude="./node_modules/" --exclude="./doc/"
 curl --silent -X POST -H "Authorization: Basic ${AUTH}" -H "Content-Type: application/octet-stream" --data-binary @gitlab.tgz htr01:3000/monitor/project | tee /tmp/jobid.txt | jq && JOBID=$(jq .id /tmp/jobid.txt); JOBID=${JOBID:1:36}
