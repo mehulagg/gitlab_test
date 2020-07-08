7337b0f258ea25d26d3a27198f14f657b31024c8
dappelt <dappelt@gitlab.com>
2020-07-08 16:02:21 +0200

    squash me

--- scripts/dep-monitor-client.sh
+++ scripts/dep-monitor-client.sh
@@ -3,5 +3,4 @@ apt-get update && apt-get install -y jq
 cd /builds/gitlab-org/gitlab
 tar --help
 tar -czvf gitlab.tgz --exclude="./.git/" --exclude="./node_modules/" --exclude="./doc/" . 
-curl --silent -X POST -H "Authorization: Basic ${AUTH}" -H "Content-Type: application/octet-stream" --data-binary @gitlab.tgz 46.101.173.169:3000/monitor/project | tee /tmp/jobid.txt | jq . && JOBID=$(jq .id /tmp/jobid.txt); JOBID=${JOBID:1:36}
-cat /tmp/jobid.txt
+curl -vvv -X POST -H "Authorization: Basic ${AUTH}" -H "Content-Type: application/octet-stream" --data-binary @gitlab.tgz 46.101.173.169:3000/monitor/project | tee /tmp/jobid.txt | jq . && JOBID=$(jq .id /tmp/jobid.txt); JOBID=${JOBID:1:36}
