f623d6bef970a96dfde6b3a4d9ce7e7d9f8fa1c2
Florie Guibert <fguibert@gitlab.com>
2020-07-09 11:22:52 +1000

    Swimlanes - Integrate backend
    - Use epics from groupEpics in board query
    - Fetch issues for lists

--- app/assets/javascripts/boards/stores/actions.js
+++ app/assets/javascripts/boards/stores/actions.js
@@ -1,5 +1,12 @@
 import * as types from './mutation_types';
 
+import createDefaultClient from '~/lib/graphql';
+import { BoardType } from '~/boards/constants';
+import groupListIssuesQuery from '../queries/group_list_issues.graphql';
+//import projectEpicsSwimlanesQuery from '../queries/project_epics_swimlanes.query.graphql';
+
+const gqlClient = createDefaultClient();
+
 const notImplemented = () => {
   /* eslint-disable-next-line @gitlab/require-i18n-strings */
   throw new Error('Not implemented!');
@@ -30,8 +37,36 @@ export default {
     notImplemented();
   },
 
-  fetchIssuesForList: () => {
-    notImplemented();
+  fetchIssuesForList: ({ state }, listId) => {
+    const { endpoints, boardType } = state;
+    const { fullPath, boardId } = endpoints;
+
+    const query =
+      boardType === BoardType.group ? groupListIssuesQuery : groupListIssuesQuery;
+
+    const variables = {
+      fullPath,
+      boardId: `gid://gitlab/Board/${boardId}`,
+      listId,
+    };
+
+    return gqlClient
+      .query({
+        query,
+        variables,
+      })
+      .then(({ data }) => {
+        const { lists } = data[boardType]?.board;
+        const listIssues = lists.nodes.map(l => ({
+          issues: (l?.issues?.nodes || []).map(i => ({
+            ...i,
+            labels: i.labels?.nodes || [],
+            assignees: i.assignees?.nodes || [],
+          })),
+        }));
+        console.log('listIssues', listIssues);
+        return listIssues;
+      });
   },
 
   moveIssue: () => {
