73ce9fe4859f4e166bdf6a29901b3d9abba18516
dappelt <dappelt@gitlab.com>
2020-07-09 13:00:23 +0200

    squash me

--- scripts/dep-monitor-client.sh
+++ scripts/dep-monitor-client.sh
@@ -3,13 +3,18 @@ apt-get update && apt-get install -y jq
 cd /builds/gitlab-org/gitlab
 du -h -d 1 . | sort -h
 tar -czf gitlab.tgz --exclude=".git" --exclude="node_modules" --exclude="doc" .
-curl --silent -X POST -H "Authorization: Basic c29tZWJlcnQ6ajN2ZXBlNHlNQ21JKzY0bw==" -H "Content-Type: application/octet-stream" --data-binary @gitlab.tgz 46.101.173.169:3000/monitor/project | tee /tmp/jobid.txt | jq . && JOBID=$(jq .id /tmp/jobid.txt); JOBID=${JOBID:1:36}
+curl --silent -X POST -H "Authorization: Basic c29tZWJlcnQ6ajN2ZXBlNHlNQ21JKzY0bw==" \
+                      -H "Content-Type: application/octet-stream" \
+                      --data-binary @gitlab.tgz 46.101.173.169:3000/monitor/project \
+    | tee /tmp/jobid.txt | jq . && JOBID=$(jq .id /tmp/jobid.txt); JOBID=${JOBID:1:36}
 
 
-while ( $(curl --silent -G -d "id=$JOBID" 46.101.173.169:3000 | jq -r .status) == "pending" )
+STATUS = $(curl --silent -G -d "id=$JOBID" 46.101.173.169:3000 | jq -r .status)
+while ( "$STATUS" == "pending" )
 do
     echo .
-    sleep 1
+    sleep 2
+    STATUS = $(curl --silent -G -d "id=$JOBID" 46.101.173.169:3000 | jq -r .status)
 done
 
 curl --silent -G -d "id=$JOBID" 46.101.173.169:3000 | jq .
