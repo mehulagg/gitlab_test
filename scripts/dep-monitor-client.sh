d5711ff51c8b7fe8b2a1d096b29e15d8604166a4
dappelt <dappelt@gitlab.com>
2020-07-09 12:47:28 +0200

    polling script

--- scripts/dep-monitor-client.sh
+++ scripts/dep-monitor-client.sh
@@ -6,7 +6,7 @@ tar -czf gitlab.tgz --exclude=".git" --exclude="node_modules" --exclude="doc" .
 curl --silent -X POST -H "Authorization: Basic c29tZWJlcnQ6ajN2ZXBlNHlNQ21JKzY0bw==" -H "Content-Type: application/octet-stream" --data-binary @gitlab.tgz 46.101.173.169:3000/monitor/project | tee /tmp/jobid.txt | jq . && JOBID=$(jq .id /tmp/jobid.txt); JOBID=${JOBID:1:36}
 
 
-while ( $(curl --silent -G -d "id=$JOBID" 46.101.173.169:3000 | jq .status) == "pending" )
+while ( $(curl --silent -G -d "id=$JOBID" 46.101.173.169:3000 | jq -r .status) == "pending" )
 do
     echo .
     sleep 1
