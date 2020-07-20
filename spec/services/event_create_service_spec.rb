e82fa60c35d299aa6122bf5d97fd32c63c449bb9
Marc Shaw <mshaw@gitlab.com>
2020-07-20 14:17:53 +0200

    WIP: Remove the feature flag for tracking action active monthly users

--- spec/services/event_create_service_spec.rb
+++ spec/services/event_create_service_spec.rb
@@ -187,7 +187,6 @@ RSpec.describe EventCreateService do
         end
 
         it 'records the event in the event counter' do
-          stub_feature_flags(Gitlab::UsageDataCounters::TrackUniqueActions::FEATURE_FLAG => true)
           counter_class = Gitlab::UsageDataCounters::TrackUniqueActions
           tracking_params = { event_action: counter_class::WIKI_ACTION, date_from: Date.yesterday, date_to: Date.today }
 
@@ -236,7 +235,6 @@ RSpec.describe EventCreateService do
     it_behaves_like 'service for creating a push event', PushEventPayloadService
 
     it 'records the event in the event counter' do
-      stub_feature_flags(Gitlab::UsageDataCounters::TrackUniqueActions::FEATURE_FLAG => true)
       counter_class = Gitlab::UsageDataCounters::TrackUniqueActions
       tracking_params = { event_action: counter_class::PUSH_ACTION, date_from: Date.yesterday, date_to: Date.today }
 
@@ -260,7 +258,6 @@ RSpec.describe EventCreateService do
     it_behaves_like 'service for creating a push event', BulkPushEventPayloadService
 
     it 'records the event in the event counter' do
-      stub_feature_flags(Gitlab::UsageDataCounters::TrackUniqueActions::FEATURE_FLAG => true)
       counter_class = Gitlab::UsageDataCounters::TrackUniqueActions
       tracking_params = { event_action: counter_class::PUSH_ACTION, date_from: Date.yesterday, date_to: Date.today }
 
@@ -329,7 +326,6 @@ RSpec.describe EventCreateService do
       it_behaves_like 'feature flag gated multiple event creation'
 
       it 'records the event in the event counter' do
-        stub_feature_flags(Gitlab::UsageDataCounters::TrackUniqueActions::FEATURE_FLAG => true)
         counter_class = Gitlab::UsageDataCounters::TrackUniqueActions
         tracking_params = { event_action: counter_class::DESIGN_ACTION, date_from: Date.yesterday, date_to: Date.today }
 
@@ -359,7 +355,6 @@ RSpec.describe EventCreateService do
       it_behaves_like 'feature flag gated multiple event creation'
 
       it 'records the event in the event counter' do
-        stub_feature_flags(Gitlab::UsageDataCounters::TrackUniqueActions::FEATURE_FLAG => true)
         counter_class = Gitlab::UsageDataCounters::TrackUniqueActions
         tracking_params = { event_action: counter_class::DESIGN_ACTION, date_from: Date.yesterday, date_to: Date.today }
 
