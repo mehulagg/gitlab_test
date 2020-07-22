875a114f3762db5081b8ddf4f3437c35948fb7a2
Aishwarya Subramanian <asubramanian@gitlab.com>
2020-07-22 17:15:14 -0500

    Service for merge commit csv export
    Adds a service for CSV export of Merge Commits.
    This service will be invoked in a controller action
    that lets Group owners and administrators download
    Merge Commits for a Group in the Compliance Dashboard.

---
+++ ee/app/services/ee/merge_commits/export_csv_service.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+module MergeCommits
+  class ExportCsvService
+    TARGET_FILESIZE = 15000000
+
+    def initialize(current_user, group)
+      @current_user = current_user
+      @group = group
+    end
+
+    def csv_data
+      csv_builder.render(TARGET_FILESIZE)
+    end
+
+    private
+
+    attr_reader :current_user, :group
+
+    def csv_builder
+      @csv_builder ||= CsvBuilder.new(data, header_to_value_hash)
+    end
+
+    def data
+      MergeRequestsFinder.new(current_user, finder_options).execute.merged
+    end
+
+    def finder_options
+      {
+        group_id: group.id
+      }
+    end
+
+    def header_to_value_hash
+      {
+        'Merge commit sha' => 'merge_commit_sha',
+        'Author' => -> (merge_request) { merge_request.author&.name },
+        'Merge Request' => 'id',
+        'Merged By' => -> (merge_request) { merge_request.metrics.merged_by&.name },
+        'Pipeline' => -> (merge_request) { merge_request.metrics.pipeline_id },
+        'Group' => -> (merge_request) { merge_request.source_project_namespace },
+        'Project' => -> (merge_request) { merge_request.project&.name },
+        'Approvers' => -> (merge_request) { merge_request.approved_by_users.map(&:name).join(" | ") }
+      }
+    end
+  end
+end
