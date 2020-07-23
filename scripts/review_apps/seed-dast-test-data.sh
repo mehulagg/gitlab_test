c572802555962321001b7d6dc88ac5f8f853fe5a
dappelt <dappelt@gitlab.com>
2020-07-23 12:03:04 +0200

    Squash me plz

--- scripts/review_apps/seed-dast-test-data.sh
+++ scripts/review_apps/seed-dast-test-data.sh
@@ -8,18 +8,22 @@ function create_user() {
         --data "name=${user}" \
         --data "username=${user}" \
         --data "password=${REVIEW_APPS_ROOT_PASSWORD}" \
+        --data "skip_confirmation=true" \
         "${CI_ENVIRONMENT_URL}/api/v4/users" > /tmp/user.json
+
+    [[ "$TRACE" ]] && cat /tmp/user.json
 
     jq .id /tmp/user.json
 }
 
-# function create_project_for_user () {
-#     local project="${2}"
+function create_project_for_user () {
+    local userid="${1}"
 
-#     curl --silent --show-error --header "PRIVATE-TOKEN: ${REVIEW_APPS_ROOT_TOKEN}" \
-#         --data "email=${user}@example.com" \
-#         --data "name=${user}" \
-#         --data "username=${user}" \
-#         --data "password=${REVIEW_APPS_ROOT_PASSWORD}" \
-#         "${CI_ENVIRONMENT_URL}/api/v4/users" > /tmp/user.json    
-# }
+    curl -vvv --show-error --header "PRIVATE-TOKEN: ${REVIEW_APPS_ROOT_TOKEN}" \
+        --data "user_id=${userid}" \
+        --data "name=awesome-test-project-${userid}" \
+        --data "visibility=private" \
+        "${CI_ENVIRONMENT_URL}/api/v4/projects/user/${userid}" > /tmp/project.json
+
+        [[ "$TRACE" ]] && cat /tmp/project.json
+}
