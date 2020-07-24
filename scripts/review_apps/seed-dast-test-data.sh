1f86178e7ec48b4465a5f48110d3d1efed83e383
dappelt <dappelt@gitlab.com>
2020-07-24 14:07:00 +0200

    Dont allow DAST user to create groups

--- scripts/review_apps/seed-dast-test-data.sh
+++ scripts/review_apps/seed-dast-test-data.sh
@@ -3,12 +3,20 @@
 function create_user() {
     local user="${1}"
 
+    # API details at https://docs.gitlab.com/ee/api/users.html#user-creation
+    #
+    # We set "can_create_group=false" because we don't want the DAST user to create groups.
+    # Otherwise, the DAST user likely creates a group and enables 2FA for all group members,
+    # which leads to the DAST scan getting "stuck" on the 2FA set up page.
+    # Once https://gitlab.com/gitlab-org/gitlab/-/issues/231447 is resolved, we can use 
+    # DAST_AUTH_EXCLUDE_URLS instead to prevent DAST from enabling 2FA.
     curl --silent --show-error --header "PRIVATE-TOKEN: ${REVIEW_APPS_ROOT_TOKEN}" \
         --data "email=${user}@example.com" \
         --data "name=${user}" \
         --data "username=${user}" \
         --data "password=${REVIEW_APPS_ROOT_PASSWORD}" \
         --data "skip_confirmation=true" \
+        --data "can_create_group=false" \
         "${CI_ENVIRONMENT_URL}/api/v4/users" > /tmp/user.json
 
     [[ "$TRACE" ]] && cat /tmp/user.json >&2
@@ -19,6 +27,7 @@ function create_user() {
 function create_project_for_user() {
     local userid="${1}"
 
+    # API details at https://docs.gitlab.com/ee/api/projects.html#create-project-for-user
     curl --silent --show-error --header "PRIVATE-TOKEN: ${REVIEW_APPS_ROOT_TOKEN}" \
         --data "user_id=${userid}" \
         --data "name=awesome-test-project-${userid}" \
