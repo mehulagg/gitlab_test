b732fef8c04d9e2c46f50ac725ff83af0959bd96
Frederic Caplette <fcaplette@gitlab.com>
2020-07-27 15:01:26 -0400

    Implement GraphQL for pipeline header
    Add GraphQL and Apollo in the pipeline
    header instead of REST services.

--- app/assets/javascripts/pipelines/pipeline_details_bundle.js
+++ app/assets/javascripts/pipelines/pipeline_details_bundle.js
@@ -1,16 +1,12 @@
 import Vue from 'vue';
-import { deprecatedCreateFlash as Flash } from '~/flash';
 import Translate from '~/vue_shared/translate';
-import { __ } from '~/locale';
-import { setUrlFragment, redirectTo } from '~/lib/utils/url_utility';
 import pipelineGraph from './components/graph/graph_component.vue';
 import createDagApp from './pipeline_details_dag';
 import GraphBundleMixin from './mixins/graph_pipeline_bundle_mixin';
 import PipelinesMediator from './pipeline_details_mediator';
-import pipelineHeader from './components/header_component.vue';
-import eventHub from './event_hub';
 import TestReports from './components/test_reports/test_reports.vue';
 import createTestReportsStore from './stores/test_reports';
+import { createPipelineHeaderApp } from './pipeline_details_header';
 
 Vue.use(Translate);
 
@@ -56,55 +52,6 @@ const createPipelinesDetailApp = mediator => {
   });
 };
 
-const createPipelineHeaderApp = mediator => {
-  if (!document.querySelector(SELECTORS.PIPELINE_HEADER)) {
-    return;
-  }
-  // eslint-disable-next-line no-new
-  new Vue({
-    el: SELECTORS.PIPELINE_HEADER,
-    components: {
-      pipelineHeader,
-    },
-    data() {
-      return {
-        mediator,
-      };
-    },
-    created() {
-      eventHub.$on('headerPostAction', this.postAction);
-      eventHub.$on('headerDeleteAction', this.deleteAction);
-    },
-    beforeDestroy() {
-      eventHub.$off('headerPostAction', this.postAction);
-      eventHub.$off('headerDeleteAction', this.deleteAction);
-    },
-    methods: {
-      postAction(path) {
-        this.mediator.service
-          .postAction(path)
-          .then(() => this.mediator.refreshPipeline())
-          .catch(() => Flash(__('An error occurred while making the request.')));
-      },
-      deleteAction(path) {
-        this.mediator.stopPipelinePoll();
-        this.mediator.service
-          .deleteAction(path)
-          .then(({ request }) => redirectTo(setUrlFragment(request.responseURL, 'delete_success')))
-          .catch(() => Flash(__('An error occurred while deleting the pipeline.')));
-      },
-    },
-    render(createElement) {
-      return createElement('pipeline-header', {
-        props: {
-          isLoading: this.mediator.state.isLoading,
-          pipeline: this.mediator.store.state.pipeline,
-        },
-      });
-    },
-  });
-};
-
 const createTestDetails = () => {
   const el = document.querySelector(SELECTORS.PIPELINE_TESTS);
   const { summaryEndpoint, suiteEndpoint } = el?.dataset || {};
@@ -132,7 +79,7 @@ export default () => {
   mediator.fetchPipeline();
 
   createPipelinesDetailApp(mediator);
-  createPipelineHeaderApp(mediator);
+  createPipelineHeaderApp();
   createTestDetails();
   createDagApp();
 };
