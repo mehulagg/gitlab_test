63fcc5c652aacf7958d58fd2bf139f8f8470f7b1
sstern <sstern@gitlab.com>
2020-08-04 16:57:49 -0700

    Add getter spec

--- spec/frontend/boards/stores/getters_spec.js
+++ spec/frontend/boards/stores/getters_spec.js
@@ -18,4 +18,22 @@ describe('Boards - Getters', () => {
       expect(getters.getLabelToggleState(state)).toBe('off');
     });
   });
+
+  describe('isSidebarOpen', () => {
+    it('returns true when activeId is not equal to 0', () => {
+      const state = {
+        activeId: 1,
+      };
+
+      expect(getters.isSidebarOpen(state)).toBe(true);
+    });
+
+    it('returns false when activeId is equal to 0', () => {
+      const state = {
+        activeId: 0,
+      };
+
+      expect(getters.isSidebarOpen(state)).toBe(false);
+    });
+  });
 });
