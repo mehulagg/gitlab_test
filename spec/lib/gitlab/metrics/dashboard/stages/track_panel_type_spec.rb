adb195b6f8889d0db32dc10d0a95f86ad2f7d67b
rpereira2 <rpereira@gitlab.com>
2020-08-19 14:44:19 +0530

    Flush snowplow in dashboard tracking spec

--- spec/lib/gitlab/metrics/dashboard/stages/track_panel_type_spec.rb
+++ spec/lib/gitlab/metrics/dashboard/stages/track_panel_type_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe Gitlab::Metrics::Dashboard::Stages::TrackPanelType do
     let(:dashboard) { load_sample_dashboard.deep_symbolize_keys }
 
     it 'creates tracking event' do
-      stub_application_setting(snowplow_enabled: true, snowplow_collector_hostname: 'localhost')
+      stub_application_setting(snowplow_enabled: true, snowplow_collector_hostname: 'fake_snowplow_collector')
       allow(Gitlab::Tracking).to receive(:event).and_call_original
 
       subject.transform!
@@ -22,6 +22,12 @@ RSpec.describe Gitlab::Metrics::Dashboard::Stages::TrackPanelType do
       expect(Gitlab::Tracking).to have_received(:event)
         .with('MetricsDashboard::Chart', 'chart_rendered', { label: 'area-chart' })
         .at_least(:once)
+
+      # Snowplow's `SnowplowTracker::AsyncEmitter` will automatically flush, and
+      # make a request once its buffer is full. Manually flush it now so that we
+      # don't interfere with any other spec that might be using snowplow.
+      stub_request(:get, /fake_snowplow_collector/).to_return(status: 200)
+      Gitlab::Tracking.send(:snowplow).flush
     end
   end
 end
