6047dc67efed91f0a516220224aaed24a9c8301e
James Ramsay <james@jramsay.com.au>
2020-08-20 16:14:02 +1000

    Commit author for suggestions is note author
    Git differentiates the author and committer so that the person who
    writes the change can be acknowledged even though someone else committed
    it. Suggestions may be proposed by a different person to who applies the
    change. If this information is lost, the provenance of the change can be
    hard to discern.

--- app/services/suggestions/apply_service.rb
+++ app/services/suggestions/apply_service.rb
@@ -32,12 +32,18 @@ module Suggestions
         .update_all(commit_id: result[:result], applied: true)
     end
 
+    def author
+      Gitlab::Git::User.from_gitlab(suggestion_set.authors.first) if suggestion_set.authors.one?
+    end
+
     def multi_service
       params = {
         commit_message: commit_message,
         branch_name: suggestion_set.branch,
         start_branch: suggestion_set.branch,
-        actions: suggestion_set.actions
+        actions: suggestion_set.actions,
+        author_name: author&.name,
+        author_email: author&.email
       }
 
       ::Files::MultiService.new(suggestion_set.project, current_user, params)
