60cd16682573e8889a6e07b5fdf083c888da87f9
Josianne Hyson <jhyson@gitlab.com>
2020-08-21 13:10:43 +0200

    Add endpoint for an export status callback

--- app/services/import_export/gitlab_client.rb
+++ app/services/import_export/gitlab_client.rb
@@ -9,12 +9,16 @@ module ImportExport
 
     def start_export(group_id:)
       Gitlab::HTTP.post(start_export_url(group_id),
-        # body: content,
+        body: { callback_url: callback_url },
         allow_local_requests: true,
         headers: headers)
     end
 
     private
+
+    def callback_url
+      "#{Gitlab.config.gitlab.url}/api/v4/groups/export_status"
+    end
 
     def start_export_url(group_id)
       "#{host}/api/v4/groups/#{group_id}/bulk_export"
--- lib/api/bulk_export.rb
+++ lib/api/bulk_export.rb
@@ -2,20 +2,17 @@
 
 module API
   class BulkExport < Grape::API::Instance
-    before do
-      authorize! :admin_group, user_group
-    end
-
-    params do
-      requires :id, type: String, desc: 'The ID of a group'
-      optional :callback_url, type: String, desc: 'An endpoint to notify export completion'
-    end
-
     resource :groups, requirements: { id: %r{[^/]+} } do
       desc 'Start export' do
         detail 'This feature was introduced in GitLab 12.5.'
       end
+      params do
+        requires :id, type: String, desc: 'The ID of a group'
+        optional :callback_url, type: String, desc: 'An endpoint to notify export completion'
+      end
       post ':id/bulk_export' do
+        authorize! :admin_group, user_group
+
         export_service = ImportExport::BulkExportService.new(
           group: user_group,
           user: current_user,
@@ -25,8 +22,18 @@ module API
         if export_service.async_execute
           accepted!
         else
-          render_api_error!(message: 'Something went wrong :(', status: 422)
+          render_api_error!('Something went wrong :(', 422)
         end
+      end
+
+      desc 'Notify Export Status' do
+        detail 'A webhook for notifying the destination instance of the status of an export'
+      end
+      params do
+
+      end
+      post 'export_status' do
+        render_api_error!('Something went wrong :(', 422)
       end
     end
   end
