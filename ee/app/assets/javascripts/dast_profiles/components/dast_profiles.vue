8da17d7e542600a30c019edee00cb9e9ca774ac7
Dave Pisek <dpisek@gitlab.com>
2020-08-21 10:16:58 +1000

    Move profile configs into file

--- ee/app/assets/javascripts/dast_profiles/components/dast_profiles.vue
+++ ee/app/assets/javascripts/dast_profiles/components/dast_profiles.vue
@@ -3,55 +3,11 @@ import * as Sentry from '@sentry/browser';
 import { GlButton, GlTab, GlTabs } from '@gitlab/ui';
 import { s__ } from '~/locale';
 import ProfilesList from './dast_profiles_list.vue';
-import dastSiteProfilesQuery from '../graphql/dast_site_profiles.query.graphql';
-import dastScannerProfilesQuery from '../graphql/dast_scanner_profiles.query.graphql';
-import dastSiteProfilesDelete from '../graphql/dast_site_profiles_delete.mutation.graphql';
-import dastScannerProfilesDelete from '../graphql/dast_scanner_profiles_delete.mutation.graphql';
 import * as cacheUtils from '../graphql/cache_utils';
-
-const configs = {
-  siteProfiles: {
-    profileType: 'siteProfiles',
-    query: dastSiteProfilesQuery,
-    deleteMutation: dastSiteProfilesDelete,
-    isEnabled: () => true, // feature flags can be passed in
-    fields: ['profileName', 'targetUrl'],
-    i18n: {
-      title: s__('DastProfiles|Site Profiles'),
-      errorMessages: {
-        fetchNetworkError: s__(
-          'DastProfiles|Could not fetch site profiles. Please refresh the page, or try again later.',
-        ),
-        deletionNetworkError: s__(
-          'DastProfiles|Could not delete site profile. Please refresh the page, or try again later.',
-        ),
-        deletionBackendError: s__('DastProfiles|Could not delete site profiles:'),
-      },
-    },
-  },
-  scannerProfiles: {
-    profileType: 'scannerProfiles',
-    query: dastScannerProfilesQuery,
-    deleteMutation: dastScannerProfilesDelete,
-    isEnabled: () => true, // feature flags can be passed in
-    fields: ['profileName', 'scannerType'],
-    i18n: {
-      title: s__('DastProfiles|Scanner Profiles'),
-      errorMessages: {
-        fetchNetworkError: s__(
-          'DastProfiles|Could not fetch scanner profiles. Please refresh the page, or try again later.',
-        ),
-        deletionNetworkError: s__(
-          'DastProfiles|Could not delete scanner profile. Please refresh the page, or try again later.',
-        ),
-        deletionBackendError: s__('DastProfiles|Could not delete scanner profiles:'),
-      },
-    },
-  },
-};
+import profileConfigs from '../constants/profile_configs';
 
 export default {
-  configs,
+  configs: profileConfigs,
   components: {
     GlButton,
     GlTab,
