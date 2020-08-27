6405a44328f2fe0e317d38be37a3b9a87f12e926
Scott Hampton <shampton@gitlab.com>
2020-08-27 11:51:19 -0700

    Move group repo analytics to Vue
    In preparation for adding more features to
    the group repository analytics page, we
    need to move the page to Vue.

---
+++ ee/app/assets/javascripts/analytics/repository_analytics/components/group_repository_analytics.vue
@@ -0,0 +1,33 @@
+<script>
+import { GlButton } from '@gitlab/ui';
+import { __ } from '~/locale';
+
+export default {
+  name: 'GroupRepositoryAnalytics',
+  components: {
+    GlButton,
+  },
+  props: {
+    groupAnalyticsCoverageReportsPath: {
+      type: String,
+      required: true,
+    },
+  },
+  text: {
+    codeCoverageHeader: __('Test Code Coverage'),
+    downloadCSVButton: __('Download historic test coverage data (.csv)'),
+  },
+};
+</script>
+
+<template>
+  <div class="d-flex justify-content-between align-items-center">
+    <h4 class="sub-header">{{ $options.text.codeCoverageHeader }}</h4>
+    <gl-button
+      :href="groupAnalyticsCoverageReportsPath"
+      rel="nofollow"
+      download
+      size="small"
+    >{{ $options.text.downloadCSVButton }}</gl-button>
+  </div>
+</template>
