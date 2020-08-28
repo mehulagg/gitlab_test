b18d3ac2acd17886f4f909c8d2683821f7096aaa
Mike Jang <mjang@gitlab.com>
2020-08-28 12:09:52 -0700

    Update another file due to prettier

--- app/assets/javascripts/notes/components/discussion_counter.vue
+++ app/assets/javascripts/notes/components/discussion_counter.vue
@@ -1,6 +1,6 @@
 <script>
 import { mapGetters, mapActions } from 'vuex';
-import { GlTooltipDirective, GlIcon } from '@gitlab/ui';
+import { GlButton, GlTooltipDirective, GlIcon } from '@gitlab/ui';
 import discussionNavigation from '../mixins/discussion_navigation';
 
 export default {
