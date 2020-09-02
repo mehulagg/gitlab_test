70cea5ff95a1953a2e6f7544071f2148bd0e2924
André Luís <aluis@gitlab.com>
2020-09-02 12:01:28 +0100

    Hook autosize to StartupCSS DOM event

--- app/assets/javascripts/behaviors/autosize.js
+++ app/assets/javascripts/behaviors/autosize.js
@@ -1,24 +1,11 @@
 import Autosize from 'autosize';
 
-const setupAutosize = () => {
-  const autosizeEls = document.querySelectorAll('.js-autosize');
+document.addEventListener(
+  window.gl && gl.startupcssEnabled ? 'startupcss-complete' : 'DOMContentLoaded',
+  () => {
+    const autosizeEls = document.querySelectorAll('.js-autosize');
 
-  Autosize(autosizeEls);
-  Autosize.update(autosizeEls);
-};
-
-const checkReadyForAutosize = ev => {
-  // only run when application css is set
-  // beware of other URLs in other envs (eg: prod)
-  if (ev.detail.startupcssHref?.match(/assets\/application/)) {
-    setupAutosize();
-  }
-};
-
-// Check whether to listen to StartupCSS or listening to DOMContentLoaded
-const startupcssLinksLoading = document.querySelectorAll('link[data-startupcss="loading"]');
-if (startupcssLinksLoading.length) {
-  document.addEventListener('startupcss-stylesheets-activated', checkReadyForAutosize);
-} else {
-  document.addEventListener('DOMContentLoaded', setupAutosize);
-}
+    Autosize(autosizeEls);
+    Autosize.update(autosizeEls);
+  },
+);
