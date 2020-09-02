7d60959c98ee9e1b2c8e9321c5ee96cbc9a332fc
Sebastian Arcila Valenzuela <sarcila@gitlab.com>
2020-09-02 15:15:43 +0200

    Change approach for membership removal
    Remove work lease and instead enqueue a job inmediatly for the
    removal, and display within the options to enable or disable ldap lock
    that users could be removed

--- ee/app/services/groups/schedule_non_ldap_membership_removal_service.rb
+++ ee/app/services/groups/schedule_non_ldap_membership_removal_service.rb
@@ -2,41 +2,16 @@
 
 module Groups
   class ScheduleNonLdapMembershipRemovalService < Groups::BaseService
-    include ExclusiveLeaseGuard
-
-    DEFAULT_LEASE_TIMEOUT = 1.hour.to_i
-
     def execute
-      group.update(params)
-
-      try_obtain_lease do
-        perform
+      group.update(params).tap do |updated|
+        perform if updated && !group.unlock_membership_to_ldap
       end
     end
 
     private
 
     def perform
-      Groups::RemoveNonLdapMembersWorker.perform_in(DEFAULT_LEASE_TIMEOUT, group.id, current_user.id)
-    end
-
-    # Used by ExclusiveLeaseGuard
-    def lease_timeout
-      DEFAULT_LEASE_TIMEOUT
-    end
-
-    # Used by ExclusiveLeaseGuard
-    def lease_key
-      "groups:remove_non_ldap_membership:#{group.id}"
-    end
-
-    # Used by ExclusiveLeaseGuard
-    # Overriding value as we never release the lease
-    # before the timeout in order to prevent multiple
-    # Groups::RemoveNonLdapMembersWorker to start in
-    # a short span of time
-    def lease_release?
-      false
+      Groups::RemoveNonLdapMembersWorker.perform_async(group.id, current_user.id)
     end
   end
 end
--- ee/spec/services/groups/schedule_non_ldap_membership_removal_service_spec.rb
+++ ee/spec/services/groups/schedule_non_ldap_membership_removal_service_spec.rb
@@ -4,32 +4,15 @@ require 'spec_helper'
 
 RSpec.describe Groups::ScheduleNonLdapMembershipRemovalService do
   describe '#execute', :clean_gitlab_redis_shared_state do
-    include ExclusiveLeaseHelpers
-
     let_it_be(:group) { create(:group) }
     let_it_be(:user) { create(:user) }
-    let(:lease_key) { "groups:remove_non_ldap_membership:#{group.id}" }
 
     subject { described_class.new(group, user) }
 
-    context 'when we can obtain the lease' do
-      it 'schedules the worker' do
-        stub_exclusive_lease(lease_key, timeout: described_class::DEFAULT_LEASE_TIMEOUT)
+    it 'schedules the worker' do
+      expect(::Groups::RemoveNonLdapMembersWorker).to receive(:perform_async).with(group.id, user.id).once
 
-        expect(::Groups::RemoveNonLdapMembersWorker).to receive(:perform_in).with(described_class::DEFAULT_LEASE_TIMEOUT, group.id, user.id).once
-
-        subject.execute
-      end
-    end
-
-    context "when we can't obtain the lease" do
-      it 'does not schedule the worker' do
-        stub_exclusive_lease_taken(lease_key, timeout: described_class::DEFAULT_LEASE_TIMEOUT)
-
-        expect(::Groups::RemoveNonLdapMembersWorker).not_to receive(:perform_in)
-
-        subject.execute
-      end
+      subject.execute
     end
   end
 end
