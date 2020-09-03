4efe99c0a8d0dbf9ca59af4602e33dd8e76618d8
André Luís <aluis@gitlab.com>
2020-09-03 01:57:11 +0100

    Fix stray prettier issue in main.js

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -53,7 +53,7 @@ window.$ = jQuery;
 jQuery.ajaxSetup({
   converters: {
     // eslint-disable-next-line @gitlab/require-i18n-strings, func-names
-    'text script': function (text) {
+    'text script': function(text) {
       jQuery.globalEval(text, { nonce: getCspNonceValue() });
       return text;
     },
