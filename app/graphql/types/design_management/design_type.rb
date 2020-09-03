minshi123/gitlab

Add pendingTodo for some todoables in GraphQL
This allows the client to query for any pending todos for the
current_user on types that implement the interface.

The interface has been added to DesignType, IssueType, MergeRequestType
and EpicType.

https://gitlab.com/gitlab-org/gitlab/-/issues/198439

app/graphql/types/design_management/design_type.rb
@@ -12,6 +12,7 @@ module Types
 
       implements(Types::Notes::NoteableType)
       implements(Types::DesignManagement::DesignFields)
+      implements(Types::PendingTodo)
 
       field :versions,
             Types::DesignManagement::VersionType.connection_type,

app/graphql/types/issue_type.rb
@@ -7,6 +7,7 @@ module Types
     connection_type_class(Types::CountableConnectionType)
 
     implements(Types::Notes::NoteableType)
+    implements(Types::PendingTodo)
 
     authorize :read_issue
 

app/graphql/types/merge_request_type.rb
@@ -7,6 +7,7 @@ module Types
     connection_type_class(Types::CountableConnectionType)
 
     implements(Types::Notes::NoteableType)
+    implements(Types::PendingTodo)
 
     authorize :read_merge_request
 

ee/app/graphql/types/epic_type.rb
@@ -14,6 +14,7 @@ module Types
     present_using EpicPresenter
 
     implements(Types::Notes::NoteableType)
+    implements(Types::PendingTodo)
 
     field :id, GraphQL::ID_TYPE, null: false,
           description: 'ID of the epic'

spec/graphql/types/design_management/design_type_spec.rb
@@ -3,8 +3,10 @@
 require 'spec_helper'
 
 RSpec.describe GitlabSchema.types['Design'] do
+  specify { expect(described_class.interfaces).to include(Types::PendingTodo) }
+
   it_behaves_like 'a GraphQL type with design fields' do
-    let(:extra_design_fields) { %i[notes discussions versions] }
+    let(:extra_design_fields) { %i[notes pending_todo discussions versions] }
     let_it_be(:design) { create(:design, :with_versions) }
     let(:object_id) { GitlabSchema.id_from_object(design) }
     let_it_be(:object_id_b) { GitlabSchema.id_from_object(create(:design, :with_versions)) }

spec/graphql/types/issue_type_spec.rb
@@ -11,11 +11,13 @@ RSpec.describe GitlabSchema.types['Issue'] do
 
   specify { expect(described_class.interfaces).to include(Types::Notes::NoteableType) }
 
+  specify { expect(described_class.interfaces).to include(Types::PendingTodo) }
+
   it 'has specific fields' do
     fields = %i[id iid title description state reference author assignees participants labels milestone due_date
                 confidential discussion_locked upvotes downvotes user_notes_count web_path web_url relative_position
                 subscribed time_estimate total_time_spent closed_at created_at updated_at task_completion_status
-                designs design_collection alert_management_alert]
+                designs design_collection alert_management_alert pending_todo]
 
     fields.each do |field_name|
       expect(described_class).to have_graphql_field(field_name)

spec/graphql/types/merge_request_type_spec.rb
@@ -9,6 +9,8 @@ RSpec.describe GitlabSchema.types['MergeRequest'] do
 
   specify { expect(described_class.interfaces).to include(Types::Notes::NoteableType) }
 
+  specify { expect(described_class.interfaces).to include(Types::PendingTodo) }
+
   it 'has the expected fields' do
     expected_fields = %w[
       notes discussions user_permissions id iid title title_html description
@@ -24,7 +26,7 @@ RSpec.describe GitlabSchema.types['MergeRequest'] do
       source_branch_exists target_branch_exists
       upvotes downvotes head_pipeline pipelines task_completion_status
       milestone assignees participants subscribed labels discussion_locked time_estimate
-      total_time_spent reference author merged_at commit_count
+      total_time_spent reference author merged_at commit_count pending_todo
     ]
 
     expected_fields << 'approved_by' if Gitlab.ee?

mehulagg/gitlab_test

app/graphql/types/design_management/design_type.rb
@@ -12,6 +12,7 @@ module Types
 
       implements(Types::Notes::NoteableType)
       implements(Types::DesignManagement::DesignFields)
+      implements(Types::CurrentUserTodos)
 
       field :versions,
             Types::DesignManagement::VersionType.connection_type,

app/graphql/types/issue_type.rb
@@ -7,6 +7,7 @@ module Types
     connection_type_class(Types::CountableConnectionType)
 
     implements(Types::Notes::NoteableType)
+    implements(Types::CurrentUserTodos)
 
     authorize :read_issue
 

app/graphql/types/merge_request_type.rb
@@ -7,6 +7,7 @@ module Types
     connection_type_class(Types::CountableConnectionType)
 
     implements(Types::Notes::NoteableType)
+    implements(Types::CurrentUserTodos)
 
     authorize :read_merge_request
 

ee/app/graphql/types/epic_type.rb
@@ -14,6 +14,7 @@ module Types
     present_using EpicPresenter
 
     implements(Types::Notes::NoteableType)
+    implements(Types::CurrentUserTodos)
 
     field :id, GraphQL::ID_TYPE, null: false,
           description: 'ID of the epic'

spec/graphql/types/design_management/design_type_spec.rb
@@ -3,8 +3,10 @@
 require 'spec_helper'
 
 RSpec.describe GitlabSchema.types['Design'] do
+  specify { expect(described_class.interfaces).to include(Types::CurrentUserTodos) }
+
   it_behaves_like 'a GraphQL type with design fields' do
-    let(:extra_design_fields) { %i[notes discussions versions] }
+    let(:extra_design_fields) { %i[notes current_user_todos discussions versions] }
     let_it_be(:design) { create(:design, :with_versions) }
     let(:object_id) { GitlabSchema.id_from_object(design) }
     let_it_be(:object_id_b) { GitlabSchema.id_from_object(create(:design, :with_versions)) }

spec/graphql/types/issue_type_spec.rb
@@ -11,11 +11,13 @@ RSpec.describe GitlabSchema.types['Issue'] do
 
   specify { expect(described_class.interfaces).to include(Types::Notes::NoteableType) }
 
+  specify { expect(described_class.interfaces).to include(Types::CurrentUserTodos) }
+
   it 'has specific fields' do
     fields = %i[id iid title description state reference author assignees participants labels milestone due_date
                 confidential discussion_locked upvotes downvotes user_notes_count web_path web_url relative_position
                 subscribed time_estimate total_time_spent closed_at created_at updated_at task_completion_status
-                designs design_collection alert_management_alert]
+                designs design_collection alert_management_alert current_user_todos]
 
     fields.each do |field_name|
       expect(described_class).to have_graphql_field(field_name)

spec/graphql/types/merge_request_type_spec.rb
@@ -9,6 +9,8 @@ RSpec.describe GitlabSchema.types['MergeRequest'] do
 
   specify { expect(described_class.interfaces).to include(Types::Notes::NoteableType) }
 
+  specify { expect(described_class.interfaces).to include(Types::CurrentUserTodos) }
+
   it 'has the expected fields' do
     expected_fields = %w[
       notes discussions user_permissions id iid title title_html description
@@ -24,7 +26,7 @@ RSpec.describe GitlabSchema.types['MergeRequest'] do
       source_branch_exists target_branch_exists
       upvotes downvotes head_pipeline pipelines task_completion_status
       milestone assignees participants subscribed labels discussion_locked time_estimate
-      total_time_spent reference author merged_at commit_count
+      total_time_spent reference author merged_at commit_count current_user_todos
     ]
 
     if Gitlab.ee?