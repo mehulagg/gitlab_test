02eb0a8009afd2df7e45b537bd6493dab1037f2c
Jennie Louie <jlouie@gitlab.com>
2020-09-04 13:07:10 -0700

    WIP add before hook and Geo example

--- qa/qa/specs/features/browser_ui/3_create/repository/push_over_http_spec.rb
+++ qa/qa/specs/features/browser_ui/3_create/repository/push_over_http_spec.rb
@@ -3,41 +3,83 @@
 module QA
   RSpec.describe 'Create' do
     describe 'Git push over HTTP', :ldap_no_tls do
-      it 'user pushes code to the repository', :smoke, testcase: 'https://gitlab.com/gitlab-org/quality/testcases/-/issues/426' do
-        Flow::Login.sign_in
+      context 'when project not using specific Praefect repository storage' do
+        shared_context 'A git push via HTTP' do
+          before do
+            @project = Resource::Project.fabricate! do |project|
+              project.name = 'http-push-project'
+            end
 
-        Resource::Repository::ProjectPush.fabricate! do |push|
-          push.file_name = 'README.md'
-          push.file_content = '# This is a test project'
-          push.commit_message = 'Add README.md'
-        end.project.visit!
+            Resource::Repository::ProjectPush.fabricate! do |push|
+              push.project = @project
+              push.file_name = 'README.md'
+              push.file_content = '# This is a test project'
+              push.commit_message = 'Add README.md'
+            end
+          end
+        end
 
-        Page::Project::Show.perform do |project|
-          expect(project).to have_file('README.md')
-          expect(project).to have_readme_content('This is a test project')
+        include_context 'A git push via HTTP'
+
+        it 'pushes code to the repository', :smoke, testcase: 'https://gitlab.com/gitlab-org/quality/testcases/-/issues/426' do
+          Flow::Login.sign_in
+
+          @project.visit!
+
+          Page::Project::Show.perform do |project|
+            expect(project).to have_file('README.md')
+            expect(project).to have_readme_content('This is a test project')
+          end
+        end
+
+        it 'replicates pushed code to the Geo secondary site', :orchestrated, :geo do
+          QA::Runtime::Logger.debug('Visiting the secondary geo node')
+
+          QA::Flow::Login.while_signed_in(address: :geo_secondary) do
+            EE::Page::Main::Banner.perform do |banner|
+              expect(banner).to have_secondary_read_only_banner
+            end
+
+            Page::Main::Menu.perform { |menu| menu.go_to_projects }
+
+            Page::Dashboard::Projects.perform do |dashboard|
+              dashboard.wait_for_project_replication(@project.name)
+              dashboard.go_to_project(@project.name)
+            end
+
+            # Validate the content has been sync'd from the primary
+            Page::Project::Show.perform do |show|
+              show.wait_for_repository_replication_with('README.md')
+
+              expect(page).to have_content('README.md')
+              expect(page).to have_content('This is a test project')
+            end
+          end
         end
       end
 
-      it 'pushes to a project using a specific Praefect repository storage', :smoke, :requires_admin, :requires_praefect, testcase: 'https://gitlab.com/gitlab-org/quality/testcases/-/issues/742' do
-        Flow::Login.sign_in_as_admin
+      context 'when project uses a specific Praefect repository storage' do
+        it 'pushes to a project using a specific Praefect repository storage', :smoke, :requires_admin, :requires_praefect, testcase: 'https://gitlab.com/gitlab-org/quality/testcases/-/issues/742' do
+          Flow::Login.sign_in_as_admin
 
-        project = Resource::Project.fabricate_via_api! do |storage_project|
-          storage_project.name = 'specific-repository-storage'
-          storage_project.repository_storage = QA::Runtime::Env.praefect_repository_storage
+          project = Resource::Project.fabricate_via_api! do |storage_project|
+            storage_project.name = 'specific-repository-storage'
+            storage_project.repository_storage = QA::Runtime::Env.praefect_repository_storage
+          end
+
+          Resource::Repository::Push.fabricate! do |push|
+            push.repository_http_uri = project.repository_http_location.uri
+            push.file_name = 'README.md'
+            push.file_content = "# This is a test project named #{project.name}"
+            push.commit_message = 'Add README.md'
+            push.new_branch = true
+          end
+
+          project.visit!
+
+          expect(page).to have_content('README.md')
+          expect(page).to have_content("This is a test project named #{project.name}")
         end
-
-        Resource::Repository::Push.fabricate! do |push|
-          push.repository_http_uri = project.repository_http_location.uri
-          push.file_name = 'README.md'
-          push.file_content = "# This is a test project named #{project.name}"
-          push.commit_message = 'Add README.md'
-          push.new_branch = true
-        end
-
-        project.visit!
-
-        expect(page).to have_content('README.md')
-        expect(page).to have_content("This is a test project named #{project.name}")
       end
     end
   end
