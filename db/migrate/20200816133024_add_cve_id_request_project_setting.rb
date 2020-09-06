4709fdc9389a7658d7832e06b640885b42550aeb
James Johnson <jjohnson@gitlab.com>
2020-09-06 06:25:12 -0700

    Changes new column to the security_setting table

--- db/migrate/20200816133024_add_cve_id_request_project_setting.rb
+++ db/migrate/20200816133024_add_cve_id_request_project_setting.rb
@@ -4,10 +4,10 @@ class AddCveIdRequestProjectSetting < ActiveRecord::Migration[6.0]
   DOWNTIME = false
 
   def up
-    add_column :projects, :cve_id_request_enabled, :boolean, default: true # rubocop:disable Migration/AddColumnsToWideTables
+    add_column :project_security_settings, :cve_id_request_enabled, :boolean, default: true, null: false
   end
 
   def down
-    remove_column :projects, :cve_id_request_enabled
+    remove_column :project_security_settings, :cve_id_request_enabled
   end
 end
