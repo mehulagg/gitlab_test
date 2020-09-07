1777e0b6f2777c30fdc013daee663270dd959bdf
Grant Young <gyoung@gitlab.com>
2020-09-07 15:29:33 +0100

    Add demo tab to templates page

---
+++ app/assets/javascripts/projects/default_demo_templates.js
@@ -0,0 +1,8 @@
+import { s__ } from '~/locale';
+
+export default {
+  demo_template: {
+    text: s__('ProjectTemplates|Demo Template (test)'),
+    icon: '.template-option .icon-gitlab-tanuki',
+  },
+};
