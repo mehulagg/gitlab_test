f7df66b4054678be7fcc88679cb148b90b0bf3fa
Luke Duncalfe <lduncalfe@eml.cc>
2020-09-07 16:08:15 +1200

    BaseEnum raises if value is lowercase string
    Our GraphQL styleguide for enums says the values should be uppercase.
    
    https://docs.gitlab.com/ee/development/api_graphql_styleguide.html#enums
    
    However a number of enums have previously been defined with lowercase
    values (See
    https://gitlab.com/gitlab-org/gitlab/-/merge_requests/41635).
    
    Our `BaseEnum.value` method now validates that the value passed to it
    doesn't contain a lowercase string.
    
    It's impossible to enforce this through Rubocop, as we often define
    enum values dynamically.

--- app/graphql/types/base_enum.rb
+++ app/graphql/types/base_enum.rb
@@ -4,9 +4,17 @@ module Types
   class BaseEnum < GraphQL::Schema::Enum
     extend GitlabStyleDeprecations
 
+    LOWERCASE_VALUE_ERROR = 'GitLab enum values must be uppercase. ' \
+                            'See https://docs.gitlab.com/ee/development/api_graphql_styleguide.html#enums' \
+
     class << self
       def value(*args, **kwargs, &block)
-        enum[args[0].downcase] = kwargs[:value] || args[0]
+        value = args[0]
+
+        raise ArgumentError, LOWERCASE_VALUE_ERROR if value.match(/\p{Lower}/) && !kwargs[:deprecated]
+
+        enum[value.downcase] = kwargs[:value] || value
+
         kwargs = gitlab_deprecation(kwargs)
 
         super(*args, **kwargs, &block)
--- spec/graphql/types/base_enum_spec.rb
+++ spec/graphql/types/base_enum_spec.rb
@@ -3,21 +3,51 @@
 require 'spec_helper'
 
 RSpec.describe Types::BaseEnum do
+  using RSpec::Parameterized::TableSyntax
+
+  describe '#value' do
+    where(:arg, :raises) do
+      'f'    | true
+      'FOo'  | true
+      'FOO'  | false
+      'F_OO' | false
+      '123a' | true
+      '123'  | false
+    end
+
+    with_them do
+      it 'raises an error when value contains a lowercase string' do
+        test = -> { described_class.value(arg) }
+
+        if raises
+          expect(&test).to raise_error(ArgumentError, /must be uppercase/)
+        else
+          expect(&test).not_to raise_error
+        end
+      end
+    end
+
+    it 'does not raise if `deprecated` keyword is present' do
+      expect do
+        described_class.value('foo', deprecated: { reason: 'reason', milestone: '1' })
+      end.not_to raise_error
+    end
+  end
+
   describe '#enum' do
     let(:enum) do
       Class.new(described_class) do
         value 'TEST', value: 3
-        value 'other'
         value 'NORMAL'
       end
     end
 
     it 'adds all enum values to #enum' do
-      expect(enum.enum.keys).to contain_exactly('test', 'other', 'normal')
-      expect(enum.enum.values).to contain_exactly(3, 'other', 'NORMAL')
+      expect(enum.enum.keys).to contain_exactly('test', 'normal')
+      expect(enum.enum.values).to contain_exactly(3, 'NORMAL')
     end
 
-    it 'is a HashWithIndefferentAccess' do
+    it 'is a HashWithIndifferentAccess' do
       expect(enum.enum).to be_a(HashWithIndifferentAccess)
     end
   end
