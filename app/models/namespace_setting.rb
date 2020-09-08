83dc08433c174699131f48681bbc6284cbe38c1f
Małgorzata Ksionek <mksionek@gitlab.com>
2020-09-08 16:48:27 +0200

    WIP

--- app/models/namespace_setting.rb
+++ app/models/namespace_setting.rb
@@ -4,6 +4,15 @@ class NamespaceSetting < ApplicationRecord
   belongs_to :namespace, inverse_of: :namespace_settings
 
   self.primary_key = :namespace_id
+
+  validate :allow_mfa_for_group
+
+
+  def allow_mfa_for_group
+    if namespace.parent_id
+      errors.add(:allow_mfa_for_subgroups, "allow MFA setting is not allowed since group is not top-level group.")
+    end
+  end
 end
 
 NamespaceSetting.prepend_if_ee('EE::NamespaceSetting')
--- spec/models/namespace_setting_spec.rb
+++ spec/models/namespace_setting_spec.rb
@@ -4,4 +4,7 @@ require 'spec_helper'
 
 RSpec.describe NamespaceSetting, type: :model do
   it { is_expected.to belong_to(:namespace) }
+
+  describe "Validation" do
+    subject { build(:namespace_setting, namespace: group)
 end
