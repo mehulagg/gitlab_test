b92252ee0106a0036187bc2f7a4f5f7fdaea4db0
Grant Young <gyoung@gitlab.com>
2020-09-08 15:05:10 +0100

    Switch demo_template to inherit from project

--- lib/gitlab/demo_template.rb
+++ lib/gitlab/demo_template.rb
@@ -1,41 +1,7 @@
 # frozen_string_literal: true
 
 module Gitlab
-  class DemoTemplate
-    attr_reader :title, :name, :description, :preview, :logo
-
-    def initialize(name, title, description, preview, logo = 'illustrations/gitlab_logo.svg')
-      @name, @title, @description, @preview, @logo = name, title, description, preview, logo
-    end
-
-    def file
-      archive_path.open
-    end
-
-    def archive_path
-      self.class.archive_directory.join(archive_filename)
-    end
-
-    def archive_filename
-      "#{name}.tar.gz"
-    end
-
-    def clone_url
-      "#{preview}.git"
-    end
-
-    def project_path
-      URI.parse(preview).path.sub(%r{\A/}, '')
-    end
-
-    def uri_encoded_project_path
-      ERB::Util.url_encode(project_path)
-    end
-
-    def ==(other)
-      name == other.name && title == other.title
-    end
-
+  class DemoTemplate < ProjectTemplate
     def self.localized_templates_table
       [
         DemoTemplate.new('demo_template', 'Demo Template (test)', _('Test template for Demo Templates.'), 'https://gitlab.com/gitlab-org/project-templates', 'illustrations/gitlab_logo.svg'),
@@ -43,14 +9,6 @@ module Gitlab
     end
 
     class << self
-      def all
-        localized_templates_table
-      end
-
-      def find(name)
-        all.find { |template| template.name == name.to_s }
-      end
-
       def archive_directory
         Rails.root.join("vendor/demo_templates")
       end
