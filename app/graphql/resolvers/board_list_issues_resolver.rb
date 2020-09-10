69a7e595432f49eed736c8cf2443e3ef02c1c37b
Jan Provaznik <jprovaznik@gitlab.com>
2020-09-10 18:12:20 +0200

    Filter board issues by epic ids

--- app/graphql/resolvers/board_list_issues_resolver.rb
+++ app/graphql/resolvers/board_list_issues_resolver.rb
@@ -8,12 +8,16 @@ module Resolvers
              required: false,
              description: 'Filters applied when selecting issues in the board list'
 
+    argument :epic_ids, [GraphQL::ID_TYPE],
+             required: false,
+             description: 'Include issues only in selected epics'
+
     type Types::IssueType, null: true
 
     alias_method :list, :object
 
     def resolve(**args)
-      filter_params = issue_filters(args[:filters]).merge(board_id: list.board.id, id: list.id)
+      filter_params = issue_filters(args[:filters]).merge(board_id: list.board.id, id: list.id, epic_ids: epic_ids(args))
       service = Boards::Issues::ListService.new(list.board.resource_parent, context[:current_user], filter_params)
 
       Gitlab::Graphql::Pagination::OffsetActiveRecordRelationConnection.new(service.execute)
@@ -22,6 +26,12 @@ module Resolvers
     # https://gitlab.com/gitlab-org/gitlab/-/issues/235681
     def self.complexity_multiplier(args)
       0.005
+    end
+
+    def epic_ids(args)
+      return unless args[:epic_ids]
+
+      args[:epic_ids].map { |gid| GitlabSchema.parse_gid(gid, expected_type: ::Epic).model_id }
     end
   end
 end
