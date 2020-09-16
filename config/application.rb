c071cde622ac2b85182b4df85ff1f00ad7a27ecf
Lukas Eipert <leipert@gitlab.com>
2020-09-16 15:23:21 +0200

    Extract cycle analytics page specific CSS
    In order to show how one would extract a page specific CSS bundle with a
    separate EE version, we have done the exercise for Value Stream
    Analytics. Caveat: Some colors have been changed.

--- config/application.rb
+++ config/application.rb
@@ -209,6 +209,7 @@ module Gitlab
     config.assets.paths << "#{config.root}/ee/app/assets/stylesheets/page_bundles" if Gitlab.ee?
     config.assets.paths << "#{config.root}app/assets/stylesheets/page_bundles"
     config.assets.precompile << "_mixins_and_variables_and_functions.css"
+    config.assets.precompile << "cycle_analytics.css"
     config.assets.precompile << "ide.css"
     config.assets.precompile << "jira_connect.css"
     config.assets.precompile << "todos.css"
