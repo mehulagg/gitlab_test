9b8f5aa78713f25f43a8781c647cffc1fe28ee52
Mehmet Emin INAC <minac@gitlab.com>
2020-09-16 22:17:12 +0300

    Implement Security::StoreScansService to store feature enabled reports

--- ee/spec/services/security/store_scans_service_spec.rb
+++ ee/spec/services/security/store_scans_service_spec.rb
@@ -21,4 +21,27 @@ RSpec.describe Security::StoreScansService do
       expect(mock_service_object).to have_received(:execute)
     end
   end
+
+  describe '#execute' do
+    let(:service_object) { described_class.new(pipeline) }
+
+    let_it_be(:sast_build) { create(:ee_ci_build, pipeline: pipeline) }
+    let_it_be(:dast_build) { create(:ee_ci_build, pipeline: pipeline) }
+    let_it_be(:sast_artifact) { create(:ee_ci_job_artifact, :sast, job: sast_build) }
+    let_it_be(:dast_artifact) { create(:ee_ci_job_artifact, :dast, job: dast_build) }
+
+    subject(:store_group_of_artifacts) { service_object.execute }
+
+    before do
+      allow(Security::StoreScansGroupService).to receive(:execute)
+      stub_licensed_features(sast: true, dast: false)
+    end
+
+    it 'executes Security::StoreScansGroupService for each group of artifacts if the feature is available' do
+      store_group_of_artifacts
+
+      expect(Security::StoreScansGroupService).to have_received(:execute).with([sast_artifact])
+      expect(Security::StoreScansGroupService).not_to have_received(:execute).with([dast_artifact])
+    end
+  end
 end
