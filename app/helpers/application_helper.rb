d1cb61843cfab92ffe3547cf8935129b2ff79335
Lukas Eipert <leipert@gitlab.com>
2020-09-16 15:16:09 +0200

    Prepare page specific bundles for EE versions
    Unfortunately we need to refactor our page specific bundles a little bit
    in order to ensure that EE versions of page specific CSS bundles are
    preferred over the CE versions while still allowing a fallback if not in
    EE.
    
    We introduce a little helper method as well in order to be a bit more
    verbose and show where the CSS files are coming from.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -245,6 +245,10 @@ module ApplicationHelper
     end
   end
 
+  def page_specific_stylesheet(path)
+    stylesheet_link_tag_defer(path.gsub('page_bundles/', ''))
+  end
+
   def outdated_browser?
     browser.ie?
   end
--- config/application.rb
+++ config/application.rb
@@ -177,11 +177,6 @@ module Gitlab
     config.assets.precompile << "mailer_client_specific.css"
     config.assets.precompile << "notify.css"
     config.assets.precompile << "mailers/*.css"
-    config.assets.precompile << "page_bundles/_mixins_and_variables_and_functions.css"
-    config.assets.precompile << "page_bundles/ide.css"
-    config.assets.precompile << "page_bundles/jira_connect.css"
-    config.assets.precompile << "page_bundles/todos.css"
-    config.assets.precompile << "page_bundles/xterm.css"
     config.assets.precompile << "performance_bar.css"
     config.assets.precompile << "lib/ace.js"
     config.assets.precompile << "disable_animations.css"
@@ -208,6 +203,15 @@ module Gitlab
 
     # Import css for xterm
     config.assets.paths << "#{config.root}/node_modules/xterm/src/"
+    config.assets.precompile << "xterm.css"
+
+    # Page specific bundles
+    config.assets.paths << "#{config.root}/ee/app/assets/stylesheets/page_bundles" if Gitlab.ee?
+    config.assets.paths << "#{config.root}app/assets/stylesheets/page_bundles"
+    config.assets.precompile << "_mixins_and_variables_and_functions.css"
+    config.assets.precompile << "ide.css"
+    config.assets.precompile << "jira_connect.css"
+    config.assets.precompile << "todos.css"
     config.assets.precompile << "xterm.css"
 
     # Add EE assets
