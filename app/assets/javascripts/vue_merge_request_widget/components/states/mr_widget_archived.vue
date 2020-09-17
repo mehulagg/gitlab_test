09e64b19ae10d697c1c35d3db3a074cfa59ed9c2
Russell Dickenson <rdickenson@gitlab.com>
2020-09-17 12:44:53 +1000

    Initial button changes

--- app/assets/javascripts/vue_merge_request_widget/components/states/mr_widget_archived.vue
+++ app/assets/javascripts/vue_merge_request_widget/components/states/mr_widget_archived.vue
@@ -1,10 +1,12 @@
 <script>
 import statusIcon from '../mr_widget_status_icon.vue';
+import { GlButton } from '@gitlab/ui';
 
 export default {
   name: 'MRWidgetArchived',
   components: {
     statusIcon,
+    GlButton,
   },
 };
 </script>
@@ -12,9 +14,9 @@ export default {
   <div class="mr-widget-body media">
     <div class="space-children">
       <status-icon status="warning" />
-      <button type="button" class="btn btn-success btn-sm" disabled="true">
+      <gl-button type="button" class="btn btn-success btn-sm" disabled="true">
         {{ s__('mrWidget|Merge') }}
-      </button>
+      </gl-button>
     </div>
     <div class="media-body">
       <span class="bold">
