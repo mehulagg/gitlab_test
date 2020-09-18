d5ac7925aa7ffcd7347a47fcc75e0a89edad1562
Mark Lapierre <mlapierre@gitlab.com>
2020-09-18 13:51:41 +1000

    Allow scoped feature flag toggling
    - Allows feature flags to be set for a user, project, group, or feature group
    - I'll move this commit to its own MR, along with tests.
      It's here because I added it while working on the test in this MR

--- qa/qa/runtime/feature.rb
+++ qa/qa/runtime/feature.rb
@@ -1,22 +1,27 @@
 # frozen_string_literal: true
 
+require 'active_support/core_ext/object/blank'
+
 module QA
   module Runtime
     module Feature
+      # Documentation: https://docs.gitlab.com/ee/api/features.html
+
       extend self
       extend Support::Api
 
       SetFeatureError = Class.new(RuntimeError)
       AuthorizationError = Class.new(RuntimeError)
+      UnknownScopeError = Class.new(RuntimeError)
 
-      def enable(key)
+      def enable(key, **scopes)
         QA::Runtime::Logger.info("Enabling feature: #{key}")
-        set_feature(key, true)
+        set_feature(key, true, scopes)
       end
 
-      def disable(key)
+      def disable(key, **scopes)
         QA::Runtime::Logger.info("Disabling feature: #{key}")
-        set_feature(key, false)
+        set_feature(key, false, scopes)
       end
 
       def remove(key)
@@ -27,21 +32,36 @@ module QA
         end
       end
 
-      def enable_and_verify(key)
-        set_and_verify(key, enable: true)
+      def enable_and_verify(key, **scopes)
+        set_and_verify(key, enable: true, **scopes)
       end
 
-      def disable_and_verify(key)
-        set_and_verify(key, enable: false)
+      def disable_and_verify(key, **scopes)
+        set_and_verify(key, enable: false, **scopes)
       end
 
-      def enabled?(key)
-        feature = JSON.parse(get_features).find { |flag| flag["name"] == key }
-        feature && feature["state"] == "on"
+      def enabled?(key, **scopes)
+        feature = JSON.parse(get_features).find { |flag| flag['name'] == key }
+        feature && feature['state'] == 'on' || feature['state'] == 'conditional' && scopes.present? && enabled_scope?(feature['gates'], scopes)
+      end
+
+      def enabled_scope?(gates, scopes)
+        scopes.each do |key, value|
+          case key
+          when :project, :group, :user
+            actors = gates.filter { |i| i['key'] == 'actors' }.first['value']
+            break actors.include?("#{key.to_s.capitalize}:#{value.id}")
+          when :feature_group
+            groups = gates.filter { |i| i['key'] == 'groups' }.first['value']
+            break groups.include?(value)
+          else
+            raise UnknownScopeError, "Unknown scope: #{key}"
+          end
+        end
       end
 
       def get_features
-        request = Runtime::API::Request.new(api_client, "/features")
+        request = Runtime::API::Request.new(api_client, '/features')
         response = get(request.url)
         response.body
       end
@@ -71,14 +91,14 @@ module QA
       # Arguments:
       #   key: The feature flag to set (as a string)
       #   enable: `true` to enable the flag, `false` to disable it
-      def set_and_verify(key, enable:)
+      def set_and_verify(key, enable:, **scopes)
         Support::Retrier.retry_on_exception(sleep_interval: 2) do
-          enable ? enable(key) : disable(key)
+          enable ? enable(key, scopes) : disable(key, scopes)
 
           is_enabled = nil
 
           QA::Support::Waiter.wait_until(sleep_interval: 1) do
-            is_enabled = enabled?(key)
+            is_enabled = enabled?(key, scopes)
             is_enabled == enable
           end
 
@@ -88,9 +108,12 @@ module QA
         end
       end
 
-      def set_feature(key, value)
+      def set_feature(key, value, **scopes)
         request = Runtime::API::Request.new(api_client, "/features/#{key}")
-        response = post(request.url, { value: value })
+        scopes[:project] = scopes[:project].full_path if scopes.key?(:project)
+        scopes[:group] = scopes[:group].full_path if scopes.key?(:group)
+        scopes[:user] = scopes[:user].username if scopes.key?(:user)
+        response = post(request.url, scopes.merge({ value: value }))
         unless response.code == QA::Support::Api::HTTP_STATUS_CREATED
           raise SetFeatureError, "Setting feature flag #{key} to #{value} failed with `#{response}`."
         end
