26a4c97f04c0b42a73f14c9bd5d2d123f44cfe41
James Johnson <jjohnson@gitlab.com>
2020-09-18 06:01:14 -0700

    Reorders database-dependent checks to be last

--- ee/app/helpers/ee/cve_request_helper.rb
+++ ee/app/helpers/ee/cve_request_helper.rb
@@ -3,19 +3,15 @@
 module EE
   module CveRequestHelper
     def request_cve_enabled_for_issue_and_user?(issue, user)
-      request_cve_enabled?(issue.project) &&
-        issue.confidential &&
       issue.confidential &&
         can?(user, :admin_project, issue.project) &&
         request_cve_enabled?(issue.project)
     end
 
     def request_cve_enabled?(project)
-      security_setting = ProjectSecuritySetting.safe_find_or_create_for(project)
-
-      request_cve_available?(project) &&
-        security_setting.cve_id_request_enabled == true &&
-        project.visibility_level == ::Gitlab::VisibilityLevel::PUBLIC
+      project.visibility_level == ::Gitlab::VisibilityLevel::PUBLIC &&
+        request_cve_available?(project) &&
+        ProjectSecuritySetting.safe_find_or_create_for(project).cve_id_request_enabled == true
     end
 
     def request_cve_available?(project)
