8b132d519e74e5c7d729b75e3b1109520b5615e4
Nicolò Maria Mezzopera <nicolo.mezzopera@gmail.com>
2020-09-21 10:24:48 +0200

    Add new package_path component

---
+++ app/assets/javascripts/packages/shared/components/package_path.vue
@@ -0,0 +1,65 @@
+<script>
+import { GlIcon, GlLink, GlTooltipDirective } from '@gitlab/ui';
+
+export default {
+  name: 'PackagePath',
+  components: {
+    GlIcon,
+    GlLink,
+  },
+  directives: {
+    GlTooltip: GlTooltipDirective,
+  },
+  props: {
+    path: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    pathPieces() {
+      return this.path.split('/');
+    },
+    root() {
+      return this.pathPieces[0];
+    },
+    leaf() {
+      return this.pathPieces[this.pathPieces.length - 1];
+    },
+    deeplyNested() {
+      return this.pathPieces.length > 2;
+    },
+    hasGroup() {
+      return this.root !== this.leaf;
+    },
+  },
+};
+</script>
+
+<template>
+  <div
+    v-gl-tooltip="{ title: path, disabled: !deeplyNested }"
+    class="gl-display-flex gl-align-items-center"
+  >
+    <gl-icon name="project" class="gl-mx-3 gl-min-w-0" />
+
+    <gl-link class="gl-text-gray-500 gl-min-w-0" :href="`/${root}`">
+      {{ root }}
+    </gl-link>
+
+    <template v-if="hasGroup">
+      <gl-icon name="chevron-right" class="gl-mx-2 gl-min-w-0" />
+
+      <template v-if="deeplyNested">
+        <span class="gl-inset-border-1-gray-200 gl-rounded-base gl-px-2 gl-min-w-0">
+          <gl-icon name="ellipsis_h" class="" />
+        </span>
+        <gl-icon name="chevron-right" class="gl-mx-2 gl-min-w-0" />
+      </template>
+
+      <gl-link class="gl-text-gray-500 gl-min-w-0" :href="`/${path}`">
+        {{ leaf }}
+      </gl-link>
+    </template>
+  </div>
+</template>
