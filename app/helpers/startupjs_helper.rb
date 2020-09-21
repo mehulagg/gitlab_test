28d2b8eefd3c8515ca955c756105e86a9a959c8e
John Hope <jhope@gitlab.com>
2020-09-21 15:35:40 +0100

    Use same strategy as REST API startup.js view helpers

--- app/helpers/startupjs_helper.rb
+++ app/helpers/startupjs_helper.rb
@@ -1,15 +1,13 @@
 # frozen_string_literal: true
 
 module StartupjsHelper
-  def embed_graphql(query, variables = {})
+  def page_startup_graphql_calls
+    @graphql_startup_calls
+  end
+
+  def add_page_startup_graphql_call(query, variables = {})
+    @graphql_startup_calls ||= []
     queryStr = File.read(File.join(Rails.root, "app/assets/javascripts/#{query}.query.graphql"))
-
-    content = <<~JAVASCRIPT
-
-      window.embedded_queries = window.embedded_queries || [];
-      window.embedded_queries.push({ "query": #{queryStr.to_json}, "variables": #{variables.to_json} });
-    JAVASCRIPT
-
-    content_tag(:script, content.html_safe)
+    @graphql_startup_calls << { query: queryStr, variables: {} }
   end
 end
