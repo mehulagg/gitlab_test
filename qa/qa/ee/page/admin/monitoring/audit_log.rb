8421bc5ba14e0b82c026e8922177be12b30f6f80
Robert Hunt <rhunt@gitlab.com>
2020-09-21 12:32:17 +0100

    Replace dynamic QA selectors with generic hard-coded values
    The existing QA selectors were in-place due to the process of migrating
    the audit events from HAML templates to a generic Vue app. This removes
    these selectors and replaces them with hard-coded generic values as the
    dynamic values are no longer needed. We can use the generic values very
    simply for all the QA testing requirements.
    
    We can also remove the audit_log_filter QA selector as it isn't used

--- qa/qa/ee/page/admin/monitoring/audit_log.rb
+++ qa/qa/ee/page/admin/monitoring/audit_log.rb
@@ -7,7 +7,7 @@ module QA
         module Monitoring
           class AuditLog < QA::Page::Base
             view 'ee/app/views/admin/audit_logs/index.html.haml' do
-              element :admin_audit_log_table
+              element :audit_log_table
             end
 
             def has_audit_log_table_with_text?(text)
@@ -16,7 +16,7 @@ module QA
               # https://gitlab.com/gitlab-org/gitlab/issues/119203
               # TODO: https://gitlab.com/gitlab-org/gitlab/issues/195424
               wait_until(reload: true) do
-                has_element?(:admin_audit_log_table, text: text)
+                has_element?(:audit_log_table, text: text)
               end
             end
           end
