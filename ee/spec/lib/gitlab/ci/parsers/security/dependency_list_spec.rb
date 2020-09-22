5f28270507555fe05d833c9b15339873555ae1c7
Tetiana Chupryna <tchupryna@gitlab.com>
2020-09-22 13:16:25 +0300

    Fix location tests

--- ee/spec/lib/gitlab/ci/parsers/security/dependency_list_spec.rb
+++ ee/spec/lib/gitlab/ci/parsers/security/dependency_list_spec.rb
@@ -25,14 +25,15 @@ RSpec.describe Gitlab::Ci::Parsers::Security::DependencyList do
         expect(report.dependencies[0][:name]).to eq('mini_portile2')
         expect(report.dependencies[0][:version]).to eq('2.2.0')
         expect(report.dependencies[0][:packager]).to eq('Ruby (Bundler)')
-        expect(report.dependencies[12][:packager]).to eq('JavaScript (Yarn)')
         expect(report.dependencies[0][:location][:path]).to eq('rails/Gemfile.lock')
-        expect(report.dependencies[12][:location][:blob_path]).to eq(blob_path)
         expect(report.dependencies[0][:iid]).to be_nil
+        expect(report.dependencies[12][:packager]).to eq('JavaScript (Yarn)')
+        expect(report.dependencies[12][:location][:blob_path]).to eq(blob_path)
         expect(report.dependencies[12][:iid]).to eq(11)
-        expect(report.dependencies[13][:location][:ancestors]).not_to be_empty
-        expect(report.dependencies[12][:location][:ancestors]).to be_empty
-        expect(report.dependencies[12][:location][:top_level]).to be_truthy
+        expect(report.dependencies[12][:location][:top_level]).to be_falsey
+        expect(report.dependencies[12][:location][:ancestors]).not_to be_empty
+        expect(report.dependencies[13][:location][:top_level]).to be_truthy
+        expect(report.dependencies[13][:location][:ancestors]).to be_nil
       end
 
       it 'merge vulnerabilities data' do
