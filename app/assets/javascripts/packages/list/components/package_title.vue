4a808e1ec5f3c2f8395eff90110a2bf526fe212c
Nicolò Maria Mezzopera <nicolo.mezzopera@gmail.com>
2020-09-22 13:55:49 +0200

    Add PackageTitle component

---
+++ app/assets/javascripts/packages/list/components/package_title.vue
@@ -0,0 +1,35 @@
+<script>
+import { mapState } from 'vuex';
+import { n__ } from '~/locale';
+import TitleArea from '~/vue_shared/components/registry/title_area.vue';
+import MetadataItem from '~/vue_shared/components/registry/metadata_item.vue';
+import { LIST_INTRO_TEXT, LIST_TITLE_TEXT } from '../constants';
+
+export default {
+  name: 'PackageTitle',
+  components: {
+    TitleArea,
+    MetadataItem,
+  },
+  computed: {
+    ...mapState(['pagination', 'config']),
+    packageAmountText() {
+      return n__(`%d Package`, `%d Packages`, this.pagination?.total);
+    },
+    infoMessages() {
+      return [{ text: LIST_INTRO_TEXT, link: this.config.packageHelpUrl }];
+    },
+  },
+  i18n: {
+    LIST_TITLE_TEXT,
+  },
+};
+</script>
+
+<template>
+  <title-area :title="$options.i18n.LIST_TITLE_TEXT" :info-messages="infoMessages">
+    <template #metadata_amount>
+      <metadata-item icon="package" :text="packageAmountText" />
+    </template>
+  </title-area>
+</template>
