7533d9db553464f352464f025df0ac88f2d69699
Miguel Rincon <mrincon@gitlab.com>
2020-09-23 11:47:41 +0800

    Refactor pipelines table css to be page specific
    In this change we wrap the CSS of the pipeline table that only appears
    in 4 pages, inside a `[data-page]` selector to scope where it is used.
    
    Additionally some unused/abandoned styles are removed.

--- app/assets/javascripts/commit/pipelines/pipelines_table.vue
+++ app/assets/javascripts/commit/pipelines/pipelines_table.vue
@@ -193,7 +193,7 @@ export default {
       "
     />
 
-    <div v-else-if="shouldRenderTable" class="table-holder">
+    <div v-else-if="shouldRenderTable">
       <gl-button
         v-if="canRenderPipelineButton"
         class="gl-mt-3 gl-mb-0 gl-display-md-none"
